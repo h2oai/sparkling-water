5d8b49e9f8c6e9ef72d30aa3e80ce47ff8deae0c
mmalohlava <michal.malohlava@gmail.com>
2014-10-28 12:28:47 -0700

    Make distribution package to be uploaded into S3.

---
+++ dist/README.txt
@@ -0,0 +1,10 @@
+
+This package contains:
+
+    README.txt               The file you are reading right now.
+    LICENSE.txt              This software is released under the Apache v2.0
+                             open source license.
+Thanks,
+The H2O team
+www.0xdata.com
+
--- make-distrib.sh
+++ make-distrib.sh
@@ -1,35 +1,74 @@
 #!/usr/bin/env bash
 
+#
+# Prepares distribution package which can be uploaded into s3
+#
+# Expects prepared assembly.
+#
+
+set -e
+set -u
+
 # Current dir
 TOPDIR=$(cd `dirname $0` &&  pwd)
 source $TOPDIR/bin/sparkling-env.sh
+DIST_DIR="$TOPDIR/dist/"
+DIST_BUILD_DIR="${DIST_DIR}build/"
+[ -d "$DIST_BUILD_DIR" ] && rm -rf "$DIST_BUILD_DIR"
+mkdir "$DIST_BUILD_DIR" 2> /dev/null
 
-cat > demofiles.list <<EOF
-sparkling-water/bin/launch-spark-cloud.sh
-sparkling-water/bin/run-example.sh
-sparkling-water/bin/sparkling-shell
-sparkling-water/bin/sparkling-env.sh
-sparkling-water/assembly/build/libs/sparkling-water-assembly-$VERSION-all.jar
-sparkling-water/examples/README.md
-sparkling-water/examples/scripts/dlDemo.script
-sparkling-water/examples/smalldata/allyears2k_headers.csv.gz
-sparkling-water/examples/smalldata/Chicago_Ohare_2010_2013.csv
-sparkling-water/examples/smalldata/Chicago_Ohare_International_Airport.csv
-sparkling-water/examples/smalldata/Chicago_Ohare_International_Airport_2010_2013.csv
-sparkling-water/examples/smalldata/prostate.csv
-sparkling-water/LICENSE
-sparkling-water/README.md
-sparkling-water/gradle.properties
+cat > "$TOPDIR/demofiles.list" <<EOF
+bin/
+bin/launch-spark-cloud.sh
+bin/run-example.sh
+bin/sparkling-shell
+bin/sparkling-env.sh
+assembly/build/libs/sparkling-water-assembly-$VERSION-all.jar
+examples/README.md
+examples/scripts/dlDemo.script
+examples/smalldata/allyears2k_headers.csv.gz
+examples/smalldata/Chicago_Ohare_2010_2013.csv
+examples/smalldata/Chicago_Ohare_International_Airport.csv
+examples/smalldata/Chicago_Ohare_International_Airport_2010_2013.csv
+examples/smalldata/prostate.csv
+LICENSE
+README.md
+gradle.properties
 EOF
 
-ZIPNAME="sparkling-water-$VERSION.zip" 
-[ -f "$ZIPNAME" ] && rm $ZIPNAME
+# Destination folder for ziping
+DEST_DIRNAME="sparkling-water-$VERSION"
+DEST_DIR="$TOPDIR/private/$DEST_DIRNAME"
+[ -d "$DEST_DIR" ] && rm -rf "$DEST_DIR"
 
-(
-  cd ../ 
-  cat sparkling-water/demofiles.list | zip -@ "sparkling-water/$ZIPNAME"
+# Source and destination folders for Scaladoc
+SCALADOC_SRC_DIR="$TOPDIR/core/build/docs/scaladoc/"
+SCALADOC_DST_DIR="$DIST_BUILD_DIR/scaladoc/"
+
+# Resulting zip file
+ZIP_NAME="sparkling-water-$VERSION.zip" 
+ZIP_FILE="$DIST_BUILD_DIR/$ZIP_NAME"
+[ -f "$ZIP_FILE" ] && rm "$ZIP_FILE"
+
+# Make distribution package and put it into dist directory
+rsync -rtvW --files-from "$TOPDIR/demofiles.list" "$TOPDIR/" "$DEST_DIR/"
+( 
+ cd private
+ zip -r "$ZIP_FILE" "$DEST_DIRNAME"
 )
+# Copy scaladoc
+rsync -rtvW "$SCALADOC_SRC_DIR" "$SCALADOC_DST_DIR"
 
+# Copy dist dir files
+cat "$DIST_DIR/index.html" | sed -e "s/SUBST_PROJECT_VERSION/$VERSION/g" > "$DIST_BUILD_DIR/index.html"
+
+exit 0
+
+# Cleanup
+rm $TOPDIR/demofiles.list
+rm -rf $DEST_DIR
+
+# Prepare a zip file with Spark distribution
 ZIPNAME_WITH_SPARK=$(echo $ZIPNAME | sed "s/water/water-with-spark/")
 cp $ZIPNAME $ZIPNAME_WITH_SPARK
 
@@ -41,4 +80,3 @@ SPARK_EXT="tgz"
  zip -r -u ../$ZIPNAME_WITH_SPARK $SPARK_DIST
 )
 
-rm demofiles.list
