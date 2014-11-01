2e4e82a361ef09d2fd2f51f4b8bc6392142e94fd
mmalohlava <michal.malohlava@gmail.com>
2014-10-31 17:34:46 -0700

    Shell script refactoring
    Changes:
      - common code moved to shared sparkling-env definition
      - removed code containing explicit number of workers

--- bin/run-example.sh
+++ bin/run-example.sh
@@ -4,12 +4,6 @@
 TOPDIR=$(cd `dirname $0`/.. &&  pwd)
 source $TOPDIR/bin/sparkling-env.sh
 
-FAT_JAR="sparkling-water-assembly-$VERSION-all.jar"
-# Example class prefix
-if [ ! -d "$SPARK_HOME" ]; then
-  echo "Please setup SPARK_HOME variable to your Spark installation!"
-  exit -1
-fi
 # Example prefix
 PREFIX=org.apache.spark.examples.h2o
 # Name of default example
@@ -24,7 +18,6 @@ fi
 EXAMPLE_MASTER=${MASTER:-"local-cluster[3,2,1024]"}
 EXAMPLE_DEPLOY_MODE="cluster"
 EXAMPLE_DEPLOY_MODE=${DEPLOY_MODE:-"client"} 
-EXAMPLE_NUM_OF_H2O_WORKERS=${NUM_WORKERS:-3} # 2 real workers + 1 in driver
 EXAMPLE_DRIVER_MEMORY=${DRIVER_MEMORY:-1G}
 EXAMPLE_H2O_SYS_OPS=${H2O_SYS_OPS:-""}
 tmpdir=${TMPDIR:-"/tmp/"}
@@ -36,7 +29,6 @@ echo "---------"
 echo "  Using example                  : $EXAMPLE"
 echo "  Using master    (MASTER)       : $EXAMPLE_MASTER"
 echo "  Deploy mode     (DEPLOY_MODE)  : $EXAMPLE_DEPLOY_MODE"
-echo "  Exp. workers    (NUM_WORKERS)  : $EXAMPLE_NUM_OF_H2O_WORKERS"
 echo "  Driver memory   (DRIVER_MEMORY): $EXAMPLE_DRIVER_MEMORY"
 echo "  H2O JVM options (H2O_SYS_OPS)  : $EXAMPLE_H2O_SYS_OPS"
 echo "---------"
@@ -45,5 +37,5 @@ VERBOSE=--verbose
 VERBOSE=
 (
  cd $TOPDIR
- $SPARK_HOME/bin/spark-submit $VERBOSE --driver-memory $EXAMPLE_DRIVER_MEMORY --driver-java-options "$EXAMPLE_H2O_SYS_OPS -Dspark.h2o.workers=$EXAMPLE_NUM_OF_H2O_WORKERS" --master $EXAMPLE_MASTER --deploy-mode $EXAMPLE_DEPLOY_MODE --class $EXAMPLE $TOPDIR/assembly/build/libs/$FAT_JAR
+ $SPARK_HOME/bin/spark-submit $VERBOSE --driver-memory $EXAMPLE_DRIVER_MEMORY --driver-java-options "$EXAMPLE_H2O_SYS_OPS" --master $EXAMPLE_MASTER --deploy-mode $EXAMPLE_DEPLOY_MODE --class $EXAMPLE $TOPDIR/assembly/build/libs/$FAT_JAR
 )
--- bin/sparkling-env.sh
+++ bin/sparkling-env.sh
@@ -1,6 +1,21 @@
+function checkSparkHome() {
+  # Example class prefix
+  if [ ! -d "$SPARK_HOME" ]; then
+    echo "Please setup SPARK_HOME variable to your Spark installation!"
+    exit -1
+  fi
+}
+
 if [ -z $TOPDIR ]; then
   echo "Caller has to setup TOPDIR variable!"
   exit -1
 fi
 
+# Version of this distribution
 VERSION=$( cat $TOPDIR/gradle.properties | grep version | sed -e "s/.*=//" )
+# Fat jar for this distribution
+FAT_JAR="sparkling-water-assembly-$VERSION-all.jar"
+FAT_JAR_FILE="$TOPDIR/assembly/build/libs/$FAT_JAR"
+# Verify there is Spark installation
+checkSparkHome
+
