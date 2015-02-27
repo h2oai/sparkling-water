2be3018d35b07e895707bee5326bb858f1f0e136
mmalohlava <michal.malohlava@gmail.com>
2015-02-26 17:27:25 -0800

    Fix meetup scripts.

--- examples/scripts/Meetup20150226.script.scala
+++ examples/scripts/Meetup20150226.script.scala
@@ -2,38 +2,40 @@
 // Meetup script 2015-02-26
 //
 
-// Common imports
+// Location of download data
+val DIR_PREFIX = "/Users/michal/Devel/projects/h2o/repos/h2o2/bigdata/laptop/citibike-nyc/"
 import org.apache.spark.h2o._
 import org.apache.spark.examples.h2o._
+import org.apache.spark.examples.h2o.DemoUtils._
 import org.apache.spark.sql.{SQLContext, SchemaRDD}
-import org.joda.time.MutableDateTime
 import water.fvec._
-import water.util.Timer
 import hex.tree.gbm.GBMModel
+import hex.tree.gbm.GBM
+import hex.tree.gbm.GBMModel.GBMParameters
 
-// Create H2OContext to execute H2O code on Spark cluster
-implicit val h2oContext = new H2OContext(sc).start()
-import h2oContext._
-
+// Initialize SQLConcept
 implicit val sqlContext = new SQLContext(sc)
 import sqlContext._
 
+// Create H2O context and start H2O services around the Spark cluster
+implicit val h2oContext = new H2OContext(sc).start()
+import h2oContext._
+
 //
-// Load data into H2O by using H2O parser
+// Load and parse bike data (year 2013) into H2O by using H2O parser
 //
 
-val DIR_PREFIX = "/Users/michal/Devel/projects/h2o/repos/h2o2/bigdata/laptop/citibike-nyc/"
 val dataFiles = Array[String](
-      "2013-07.csv", "2013-08.csv", "2013-09.csv", "2013-10.csv",
-      "2013-11.csv", "2013-12.csv",
-      "2014-01.csv", "2014-02.csv", "2014-03.csv", "2014-04.csv",
-      "2014-05.csv", "2014-06.csv", "2014-07.csv", "2014-08.csv").map(f => new java.io.File(DIR_PREFIX, f))
+    "2013-07.csv", "2013-08.csv", "2013-09.csv", "2013-10.csv",
+    "2013-11.csv", "2013-12.csv").map(f => new java.io.File(DIR_PREFIX, f))
+
 // Load and parse data
 val bikesDF = new DataFrame(dataFiles:_*)
+
 // Rename columns and remove all spaces in header
 val colNames = bikesDF.names().map( n => n.replace(' ', '_'))
 bikesDF._names = colNames
-bikesDF.update(null)
+bikesDF.update(null) // Update Frame in DKV
 
 //
 // Transform start time to days from Epoch
@@ -73,62 +75,49 @@ val daysVec = bikesPerDayDF('Days)
 // Run transformation TimeTransform
 val finalBikeDF = bikesPerDayDF.add(new TimeTransform().doIt(daysVec))
 
-//
-// Define function to build a model
-//
-def r2(model: GBMModel, fr: Frame) =  hex.ModelMetrics.getFromDKV(model, fr).asInstanceOf[hex.ModelMetricsSupervised].r2()
-
-def buildModel(df: DataFrame)(implicit h2oContext: H2OContext) = {
+def buildModel(df: DataFrame, modelName: String = "GBM model", trees: Int = 200, depth: Int = 6):R2 = {
     import hex.splitframe.ShuffleSplitFrame
     import water.Key
-    import h2oContext._
-
     //
     // Split into train and test parts
     //
     val keys = Array[String]("train.hex", "test.hex", "hold.hex").map(Key.make(_))
     val ratios = Array[Double](0.6, 0.3, 0.1)
     val frs = ShuffleSplitFrame.shuffleSplitFrame(df, keys, ratios, 1234567689L)
-    val train = frs(0)
-    val test = frs(1)
-    val hold = frs(2)
+    val (train, test, hold) = (frs(0), frs(1), frs(2))
 
     //
     // Launch GBM prediction
     //
     import hex.tree.gbm.GBM
     import hex.tree.gbm.GBMModel.GBMParameters
-
+    // Specify parameters
     val gbmParams = new GBMParameters()
     gbmParams._train = train
     gbmParams._valid = test
     gbmParams._response_column = 'bikes
-    gbmParams._ntrees = 500
-    gbmParams._max_depth = 6
+    gbmParams._ntrees = trees
+    gbmParams._max_depth = depth
+    // Launch computation
+    val gbmModel = new GBM(gbmParams).trainModel.get
 
-    val gbm = new GBM(gbmParams)
-    val gbmModel = gbm.trainModel.get
+    // Run scoring (i like Scala!)
+    Seq(train,test,hold).foreach(gbmModel.score(_).delete)
 
-    gbmModel.score(train).remove()
-    gbmModel.score(test).remove()
-    gbmModel.score(hold).remove()
-
-    println(
-      s"""
-         |r2 on train: ${r2(gbmModel, train)}
-          |r2 on test:  ${r2(gbmModel, test)}
-          |r2 on hold:  ${r2(gbmModel, hold)}"""".stripMargin)
-
+    // Collect R2 metrics
+    val result = R2(s"Model GBM($modelName, trees: $trees, depth: $depth)", r2(gbmModel, train), r2(gbmModel, test), r2(gbmModel, hold))
     // Perform clean-up
-    train.delete()
-    test.delete()
-    hold.delete()
-
-    gbmModel
+    Seq(train, test, hold).foreach(_.delete())
+    result
 }
 
-buildModel(finalBikeDF)
+// Build model
+val result1 = buildModel(finalBikeDF, "bike data")
 
+// Grid search over different parameters
+// Increase number of trees, but decrease trees depth
+val gridSearch = Seq( (10, 8), (100, 4), (200,2))
+val gridSearchResult1 = gridSearch.map( x => buildModel(finalBikeDF, "bike data", trees = x._1, depth = x._2) ) ++ Seq(result1)
 
 // Load weather data in NY 2013
 val weatherData = sc.textFile(DIR_PREFIX + "31081_New_York_City__Hourly_2013.csv")
@@ -153,7 +142,8 @@ val bikesWeatherRdd = sql(
     """.stripMargin)
 
 // And make prediction again but now on RDD
-buildModel(bikesWeatherRdd)
+val result2 = buildModel(bikesWeatherRdd)
+val gridSearchResult2 = gridSearch.map( x => buildModel(bikesWeatherRdd, "bike+weather data", trees = x._1, depth = x._2) ) ++ Seq(result2)
 
 // Kill the cloud
 sc.stop()
