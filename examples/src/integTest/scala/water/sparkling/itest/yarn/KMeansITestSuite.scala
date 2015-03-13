9e03901dc4775803ec6db184b55280159b533802
Amy Wang <amy@0xdata.com>
2015-03-13 16:34:07 -0700

    update to kmeans integration test, added test for accuracy against h2o kmeans

--- examples/src/integTest/scala/water/sparkling/itest/yarn/KMeansITestSuite.scala
+++ examples/src/integTest/scala/water/sparkling/itest/yarn/KMeansITestSuite.scala
@@ -2,15 +2,18 @@ package water.sparkling.itest.yarn
 
 import java.io.File
 
-import org.apache.spark.h2o.H2OContext
+import org.apache.spark.h2o._
 import org.apache.spark.mllib.clustering.KMeans
 import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.sql.SchemaRDD
 import org.apache.spark.{SparkContext, SparkConf}
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 import water.fvec.DataFrame
 import water.sparkling.itest.{SparkITest, YarnTest}
+import hex.kmeans.KMeansModel.KMeansParameters
+import water.util.Timer
 
 /**
  * Created by michal on 2/9/15.
@@ -52,28 +55,63 @@ object KMeansITest {
     val airlinesData = new DataFrame(d)
     val timeToParse = timer1.time
 
-    // Transfer data from H2O to Spark RDD
+    // Run Kmeans in H2O
+    val ignore_columns = airlinesData.names diff Array("Month", "DayofMonth", "DayOfWeek")
+    val H2OKMTimer = new Timer
+    val kmeansParams = new KMeansParameters()
+    kmeansParams._k = 5
+    kmeansParams._max_iterations = 10
+    kmeansParams._train = airlinesData
+    kmeansParams._ignored_columns = ignore_columns
+    kmeansParams._standardize = false
+    val KmeansModel = new hex.kmeans.KMeans(kmeansParams).trainModel().get()
+    val H2OKMBuildTime = H2OKMTimer.time
 
+    // Score in H2O
+    val pred = KmeansModel.score(airlinesData)
+    val predRDD = asSchemaRDD(pred)
+    val clusterCounts = predRDD.countByValue()
+
+    // Run Kmeans in Spark
+    val sqlQueryTimer = new water.util.Timer
     import org.apache.spark.sql.SQLContext
-
-    val timer2 = new water.util.Timer
     implicit val sqlContext = new SQLContext(sc)
     val airlinesRDD = asSchemaRDD(airlinesData)(sqlContext)
-    val timeToTransfer = timer2.time
+    airlinesRDD.registerTempTable("airlinesRDD")
+    val airlinesTable = sqlContext.sql(
+      """SELECT Month, DayofMonth, DayOfWeek FROM airlinesRDD"""
+    )
+    val sqlQueryTime = sqlQueryTimer.time
 
-    assert (airlinesData.numRows == airlinesRDD.count, "Transfer of H2ORDD to SparkRDD completed!")
+    assert (airlinesData.numRows == airlinesTable.count, "Transfer of H2ORDD to SparkRDD completed!")
 
     // Run Kmeans in Spark  on indices 10,19,26 (FlightNo, Distance, WeatherDelay)
 
-    val airlinesVectorRDD = airlinesRDD.map(row => Vectors.dense(row.getByte(1) * 1.0, row.getByte(2) * 1.0, row.getByte(3) * 1.0))
-
-    val timer3 = new water.util.Timer
+    val airlinesVectorRDD = airlinesTable.map(row => Vectors.dense(row.getByte(0) * 1.0, row.getByte(1) * 1.0, row.getByte(2) * 1.0))
+    val SparkKMTimer = new water.util.Timer
     val clusters = KMeans.train(airlinesVectorRDD, 5, 10)
-    val timeForKMModel = timer3.time
+    val SparkKMBuildTime = SparkKMTimer.time
 
-    // Evaluate clustering by computing within set sum of squared errors
-    val WSSSE = clusters.computeCost(airlinesVectorRDD)
-    println("Within Set Sum of Squared Errors = " + WSSSE)
+    // Predict on Spark's Kmeans
+    val sparkPredRDD = clusters.predict(airlinesVectorRDD)
+    val srdd : SchemaRDD = sparkPredRDD.map(v => IntHolder(Option(v)))
+    val df : DataFrame = srdd
+    val sparkClusterCounts = sparkPredRDD.countByValue()
+
+    // Get Within Set Sum of Squared Errors
+    val sparkWSSSE = clusters.computeCost(airlinesVectorRDD)
+    val h2oWMSE = KmeansModel._output._within_mse
+    val h2oSize = KmeansModel._output._size
+    val h2oWSSSE = (for(i <- 0 until h2oSize.length) yield h2oSize(i)*h2oWMSE(i)).fold(0.0) { (a,b) => a + b}
+
+    println("Spark: Within Set Sum of Squared Errors = " + sparkWSSSE)
+    println("Spark: Time to Build (s) = " + SparkKMBuildTime)
+    println("H2O: Within Set Sum of Squared Errors = " + h2oWSSSE)
+    println("H2O: Time to Build (s) = " + H2OKMBuildTime)
+
+    val relativeMeanDiff = (sparkWSSSE - h2oWSSSE)/sparkWSSSE
+    assert (relativeMeanDiff < 0.01, "Within Set Sum of Squared Errors matches!")
+
     // Shutdown Spark
     sc.stop()
     // Shutdown H2O explicitly
