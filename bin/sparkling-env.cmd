615b7cbf2c71c6e4fc56e301e5fa91a5da0280a3
Mark Chan <mkchan@gmail.com>
2015-11-05 18:06:46 -0800

    Fix for windows script

--- bin/sparkling-env.cmd
+++ bin/sparkling-env.cmd
@@ -13,12 +13,12 @@ for /f "tokens=2 delims==" %%i in ('findstr /r "^h2oMajorName=.*$" %TOPDIR%\grad
 for /f "tokens=2 delims==" %%i in ('findstr /r "^sparkVersion=.*$" %TOPDIR%\gradle.properties') do (@set SPARK_VERSION=%%i)
 
 rem Fat jar for this distribution
-set FAT_JAR="sparkling-water-assembly-%VERSION%-all.jar"
-set FAT_JAR_FILE="%TOPDIR%\assembly\build\libs\%FAT_JAR%"
+set FAT_JAR=sparkling-water-assembly-%VERSION%-all.jar
+set FAT_JAR_FILE=%TOPDIR%\assembly\build\libs\%FAT_JAR%
 
 rem Setup loging and outputs
 
 set tmpdir=%TMP%
-set SPARK_LOG_DIR="%tmpdir%\spark\logs"
-set SPARK_WORKER_DIR="%tmpdir%\spark\work"
-set SPARK_LOCAL_DIRS="%tmpdir%\spark\work"
+set SPARK_LOG_DIR=%tmpdir%\spark\logs
+set SPARK_WORKER_DIR=%tmpdir%\spark\work
+set SPARK_LOCAL_DIRS=%tmpdir%\spark\work
