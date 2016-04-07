4849ad33355d1387fa6280613498572931f583ce
Jakub Hava <jakub.hava@gmail.com>
2016-04-07 10:49:18 +0200

    [SW-37][SW-50] Reject incorrect Spark version.
    Warn and stop when we try to build or run Sparkling-Water with a different Spark version then is current Sparkling Water version intended to run on

--- bin/run-python-script.sh
+++ bin/run-python-script.sh
@@ -5,6 +5,8 @@ TOPDIR=$(cd `dirname $0`/.. &&  pwd)
 source $TOPDIR/bin/sparkling-env.sh
 # Verify there is Spark installation
 checkSparkHome
+# Verify if correct Spark version is used
+checkSparkVersion
 
 
 if [ $1 ] && [[ ${1} != "--"* ]]; then
