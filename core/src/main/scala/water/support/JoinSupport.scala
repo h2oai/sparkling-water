444b16ef2be72dc4d5da5a55a907fe41afdab33e
mmalohlava <michal.malohlava@gmail.com>
2016-06-05 15:30:44 -0700

    [SW-150] Introduce Scala API for Frame join operations
    The change introduces Support class which encapsulates
    Matt's join (aka merge).
    
    The test verifies possible combination of join methods (sort v. hash) and types (left, rigth, inner, outer)
    Note: outer seems that it is not implemented.

---
+++ core/src/main/scala/water/support/JoinSupport.scala
@@ -0,0 +1,77 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package water.support
+
+import org.apache.spark.h2o.Frame
+import water.munging.JoinMethod
+import water.rapids.Exec
+
+/**
+  * A wrapper to call H2O Merge operation.
+  */
+trait JoinSupport {
+
+  private val MERGE_RAPIDS: String = "(merge %s %s %s %s [] [] \"%s\")"
+
+  def join(left: Frame,
+           right: Frame,
+           allX: Boolean = false,
+           allY: Boolean = false,
+           byX: Array[Int] = Array.empty[Int],
+           byY: Array[Int] = Array.empty[Int],
+           method: JoinMethod = JoinMethod.AUTO
+          ): Frame = {
+    val rCode = String.format(MERGE_RAPIDS,
+                              left._key,
+                              right._key,
+                              toStr(allX),
+                              toStr(allY),
+                              method.name)
+    val session = new water.rapids.Session()
+    val ret = Exec.exec(rCode, session)
+    ret.getFrame
+  }
+
+  def leftJoin(left: Frame,
+               right: Frame,
+               method: JoinMethod = JoinMethod.AUTO): Frame = {
+    join(left, right, allX = true, allY = false, method = method)
+  }
+
+  def rightJoin(left: Frame,
+               right: Frame,
+               method: JoinMethod = JoinMethod.AUTO): Frame = {
+    join(left, right, allX = false, allY = true, method = method)
+  }
+
+  def innerJoin(left: Frame,
+                right: Frame,
+                method: JoinMethod = JoinMethod.AUTO): Frame = {
+    join(left, right, allX = false, allY = false, method = method)
+  }
+
+  def outerJoin(left: Frame,
+                right: Frame,
+                method: JoinMethod = JoinMethod.AUTO): Frame = {
+    join(left, right, allX = true, allY = true, method = method)
+  }
+
+  private def toStr(b: Boolean): String = if (b) "1" else "0"
+}
+
+object JoinSupport extends JoinSupport {
+}
