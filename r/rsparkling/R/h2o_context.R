7bc9ea660da39cf9915ab107928585b3ce739cf8
Erin LeDell <erin@h2o.ai>
2016-09-25 18:50:20 -0700

    [SW-182] RSparkling R package (#84)
    This commit provide implementation of RSparkling package. The implementation is based
    on the contribution from @jjallaire published originally in https://github.com/jjallaire/sparklingwater.
     
    This commit includes:
      - Transferred JJ's sparklyr SW code to the new sparkling-water/r folder
      - Updated PySparkling docs: fixed typos and naming conventions
      - Created folder and README for RSparkling
      - The method call `h2o_context` now initializes also R h2o client
      - Additional method to transfer data from H2O to Spark
      - Added H2O / Spark data conversion functions
      - Update devtools install in README.md
      - Fixed bugs that were causing problems with CRAN check
      - Suppress H2O cluster info output
      - Cleaned up RSparkling DESCRIPTION file and imports
      - Adding spark_install to example_rsparkling.R
      - Re-ordered args in `rsparkling::as_spark_dataframe` method
      - Added `sc` arg to `rsparkling::as_h2o_frame` for API consistency
      - Updated rsparkling README to include h2o install

---
+++ r/rsparkling/R/h2o_context.R
@@ -0,0 +1,72 @@
+#' Get the H2OContext. Will create the context if it has not been previously created.
+#'
+#' @param x Object of type \code{spark_connection} or \code{spark_jobj}.
+#'
+#' @export
+h2o_context <- function(x) {
+  UseMethod("h2o_context")
+}
+
+#' @export
+h2o_context.spark_connection <- function(x) {
+  hc <- sparklyr::invoke_static(x, "org.apache.spark.h2o.H2OContext", "getOrCreate", sparklyr::spark_context(x))
+  ip <- sparklyr::invoke(hc, "h2oLocalClientIp")
+  port <- sparklyr::invoke(hc, "h2oLocalClientPort")
+  invisible(utils::capture.output(h2o::h2o.init(ip = ip, port = port, strict_version_check = FALSE)))  #should update strict_version_check to TRUE
+  hc
+}
+
+#' @export
+h2o_context.spark_jobj <- function(x) {
+  h2o_context(sparklyr::spark_connection(x))
+}
+
+
+#' Open the H2O Flow UI in a browser
+#'
+#' @inheritParams h2o_context
+#'
+#' @param sc Object of type \code{spark_connection}.
+#' @export
+h2o_flow <- function(sc) {
+  flow <- sparklyr::invoke(h2o_context(sc), "h2oLocalClient")
+  utils::browseURL(paste0("http://", flow))
+}
+
+#' Convert a Spark DataFrame to an H2O Frame
+#'
+#' @param sc Object of type \code{spark_connection}.
+#' @param x A \code{spark_dataframe}.
+#'
+#' @export
+as_h2o_frame <- function(sc, x) {
+  # sc is not actually required since the sc is monkey-patched into the Spark DataFrame
+  # it is kept as an argument for API consistency
+  
+  # Ensure we are dealing with a Spark DataFrame (might be e.g. a tbl)
+  x <- sparklyr::spark_dataframe(x)
+
+  # Convert the Spark DataFrame to an H2OFrame
+  jhf <- sparklyr::invoke(h2o_context(x), "asH2OFrame", x)
+  key <- sparklyr::invoke(sparklyr::invoke(jhf, "key"), "toString")
+  h2o::h2o.getFrame(key)
+}
+
+#' Convert an H2O Frame to a Spark DataFrame
+#'
+#' @param sc Object of type \code{spark_connection}.
+#' @param x An \code{H2OFrame}.
+#'
+#' @export
+as_spark_dataframe <- function(sc, x) {
+  # TO DO: ensure we are dealing with a H2OFrame
+  
+  # Get SQLContext
+  sqlContext <- sparklyr::invoke_static(sc, "org.apache.spark.sql.SQLContext", "getOrCreate", sparklyr::spark_context(sc))
+  # Get H2OContext
+  hc <- h2o_context(sc)
+  # Invoke H2OContext#asDataFrame method on the backend
+  spark_df <- sparklyr::invoke(hc, "asDataFrame", h2o::h2o.getId(x), TRUE, sqlContext)
+  # Register returned spark_jobj as a table for dplyr
+  sparklyr::sdf_register(spark_df)
+}
---
+++ r/rsparkling/R/package.R
@@ -0,0 +1,17 @@
+
+#' @import sparklyr h2o utils
+NULL
+
+# define required spark packages
+spark_dependencies <- function(spark_version, scala_version, ...) {
+  sparklyr::spark_dependency(packages = c(
+    sprintf("ai.h2o:sparkling-water-core_%s:1.6.7", scala_version),
+    sprintf("ai.h2o:sparkling-water-ml_%s:1.6.7", scala_version),
+    sprintf("ai.h2o:sparkling-water-repl_%s:1.6.7", scala_version)
+  ))
+}
+
+.onLoad <- function(libname, pkgname) {
+  sparklyr::register_extension(pkgname)
+}
+
