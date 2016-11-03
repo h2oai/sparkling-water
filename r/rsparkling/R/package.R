6fdf2aec7edfab82d3d37e4e70ebd87f81e58cde
Jan Gorecki <jangorecki@users.noreply.github.com>
2016-11-03 20:16:02 +0000

    add missing suggested pkgs, strict pkgs import, closes SW-238 (#113)

--- r/rsparkling/R/package.R
+++ r/rsparkling/R/package.R
@@ -1,10 +1,6 @@
-
-#' @import sparklyr h2o utils
-NULL
-
 # define required spark packages
 spark_dependencies <- function(spark_version, scala_version, ...) {
-  sparklyr::spark_dependency(packages = c(
+  spark_dependency(packages = c(
     sprintf("ai.h2o:sparkling-water-core_%s:1.6.7", scala_version),
     sprintf("ai.h2o:sparkling-water-ml_%s:1.6.7", scala_version),
     sprintf("ai.h2o:sparkling-water-repl_%s:1.6.7", scala_version)
@@ -12,6 +8,6 @@ spark_dependencies <- function(spark_version, scala_version, ...) {
 }
 
 .onLoad <- function(libname, pkgname) {
-  sparklyr::register_extension(pkgname)
+  register_extension(pkgname)
 }
 
