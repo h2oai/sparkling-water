9f50de1fc79753279f0f7321c41955500e8a9f3b
Jan Gorecki <jangorecki@users.noreply.github.com>
2016-11-04 00:26:34 +0000

    use roxygen to produce NAMESPACE, fix roxygen headers (#124)
    (cherry picked from commit 09efe925c0098538648571877683342e0613caac)

--- r/rsparkling/R/package.R
+++ r/rsparkling/R/package.R
@@ -1,3 +1,7 @@
+#' @importFrom utils capture.output browseURL
+#' @importFrom sparklyr spark_dependency register_extension invoke_static invoke spark_connection spark_dataframe sdf_register spark_context
+#' @importFrom h2o h2o.getFrame h2o.getId h2o.init
+
 # define required spark packages
 spark_dependencies <- function(spark_version, scala_version, ...) {
   spark_dependency(packages = c(
