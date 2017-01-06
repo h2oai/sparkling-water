a343258a3a3b80681f0be74cba1a1dadfdf651ab
mdymczyk <dymczyk@gmail.com>
2017-01-07 04:17:56 +0900

    Semi working CNN for sentiment analysis, still needs small tweaks (hyperparameters), predictions and nicer notebook description

--- examples/scripts/DeepWaterCNNTextClassification.script.scala
+++ examples/scripts/DeepWaterCNNTextClassification.script.scala
@@ -2,23 +2,21 @@
   * This script requires deepwater-all.jar to be added through the "--jars" parameter in bin/sparkling-shell script
   *
   * Launch following commands:
-  *   export MASTER="local-cluster[3,2,4096]"
-  *   bin/sparkling-shell -i examples/scripts/DeepWaterCNNTextClassification.script.scala
+  * export MASTER="local-cluster[3,2,4096]"
+  * bin/sparkling-shell -i examples/scripts/DeepWaterCNNTextClassification.script.scala
   *
   * When running using spark shell or using scala rest API:
-  *    SQLContext is available as sqlContext
+  * SQLContext is available as sqlContext
   *     - if you want to use sqlContext implicitly, you have to redefine it like: implicit val sqlContext = sqlContext,
-  *      but better is to use it like this: implicit val sqlContext = SQLContext.getOrCreate(sc)
-  *    SparkContext is available as sc
+  * but better is to use it like this: implicit val sqlContext = SQLContext.getOrCreate(sc)
+  * SparkContext is available as sc
   */
+
 import _root_.hex.deepwater.{DeepWater, DeepWaterParameters}
 import org.apache.spark.rdd.RDD
-import org.apache.spark.SparkContext
 import org.apache.spark.h2o.{H2OContext, H2OFrame}
 import org.apache.spark.mllib.feature.HashingTF
-import water.support.SparkContextSupport
 
-import scala.io.Source
 import scala.io.Source
 
 def cleanString(string: String): String =
@@ -41,19 +39,23 @@ def loadData(): RDD[(Int, Array[String])] = {
   val pos_file = Source.fromURL("https://raw.githubusercontent.com/yoonkim/CNN_sentence/master/rt-polarity.pos", "latin1")
   val neg_file = Source.fromURL("https://raw.githubusercontent.com/yoonkim/CNN_sentence/master/rt-polarity.neg", "latin1")
 
-  val positive_examples = sc.parallelize(pos_file.getLines().toSeq).map(s => (1, cleanString(s.trim).split(" ")) )
-  val negative_examples = sc.parallelize(neg_file.getLines().toSeq).map(s => (0, cleanString(s.trim).split(" ")) )
+  val positive_examples = sc.parallelize(pos_file.getLines().toSeq).map(s => (1, cleanString(s.trim).split(" ")))
+  val negative_examples = sc.parallelize(neg_file.getLines().toSeq).map(s => (0, cleanString(s.trim).split(" ")))
 
   positive_examples.union(negative_examples)
 }
 
 def padSentences(sentences: RDD[(Int, Array[String])], paddingWord: String = "</s>"): RDD[(Int, Array[String])] = {
   val sequenceLength = sentences.values.map(_.length).max()
-  sentences.map{ case (l, s) => ( l, s.padTo(sequenceLength, paddingWord) ) }
+  sentences.map { case (l, s) => (l, s.padTo(sequenceLength, paddingWord)) }
 }
 
-def vectorize(sentences: RDD[(Int, Array[String])], vocabSize: Int) =
-  sentences.map{ case (l, s) => (l, new HashingTF(vocabSize).transform(s))}
+def vectorize(sentences: RDD[(Int, Array[String])], vocabSize: Int) = {
+  sentences.map { case (l, s) => (l, {
+    val htf = new HashingTF(vocabSize)
+    s.map{str =>htf.indexOf(str)}
+  })}
+}
 
 val vocabSize = 65536
 
@@ -61,14 +63,17 @@ val labeledSentences = loadData()
 val paddedSenteces = padSentences(labeledSentences)
 
 val hc = H2OContext.getOrCreate(sc)
+
 import hc.implicits._
 import spark.sqlContext.implicits._
 
 val vectorizedSentences = vectorize(paddedSenteces, vocabSize).toDF
 
-val frame : H2OFrame = vectorizedSentences
+val frame: H2OFrame = vectorizedSentences
 
 val p = new DeepWaterParameters
+
+p._standardize = false
 p._backend = DeepWaterParameters.Backend.tensorflow
 p._train = frame._key
 p._response_column = "_1"
@@ -76,10 +81,9 @@ p._learning_rate = 1e-4
 p._mini_batch_size = 8
 p._train_samples_per_iteration = 8
 p._epochs = 1e-3
-//p._network = DeepWaterParameters.Network.user
 p._network_definition_file = "examples/networks/cnn_text_tensorflow.meta"
-p._image_shape = Array(16247, 1)
+p._image_shape = Array(56, 1)
 p._channels = 1
-val model = new DeepWater(p).trainModel.get
+p._problem_type == DeepWaterParameters.ProblemType.dataset
 
-// TODO add predictions
+val model = new DeepWater(p).trainModel.get
