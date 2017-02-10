e431ea74dde769d935ef8b58defcecec3d100601
Michal Malohlava <michal.malohlava@gmail.com>
2017-02-10 11:48:46 -0800

    [SW-327] Attach a Sparkling Water tab into Spark UI (#185)
    * [SW-327] Attach a Sparkling Water tab into Spark UI
    
    The tab is attached after start of H2OContext.
    The tab contains runtime info about Sparkling Water (for example, IP of Flow UI),
    but also H2O build information.
    
    * List all sparkling water properties (i.e., spark properties starting with `spark.ext.h2o`)
    
    (cherry picked from commit b13b455738e6dde587af7974b266717ed9ac029b)

---
+++ core/src/main/java/org/apache/spark/h2o/ui/SparklingWaterUITab.scala
@@ -0,0 +1,38 @@
+package org.apache.spark.h2o.ui
+
+import org.apache.spark.SparkException
+import org.apache.spark.h2o.H2OContext
+import org.apache.spark.h2o.ui.SparklingWaterUITab.getSparkUI
+import org.apache.spark.ui.{SparkUI, SparkUITab}
+
+
+/**
+  * Enrich Spark UI by a Sparkling Water specific tab.
+  */
+private[h2o] class SparklingWaterUITab(val hc: H2OContext)
+  extends SparkUITab(getSparkUI(hc), "sparkling-water") {
+
+  override val name = "Sparkling Water \u2728"
+
+  val parent = getSparkUI(hc)
+
+  attachPage(new SparklingWaterInfoPage(this))
+
+  def attach() {
+    getSparkUI(hc).attachTab(this)
+  }
+  
+  def detach(): Unit = {
+    getSparkUI(hc).detachTab(this)
+  }
+
+  def getSparkUser: String = parent.getSparkUser
+}
+
+private object SparklingWaterUITab {
+  def getSparkUI(hc: H2OContext): SparkUI = {
+    hc.sparkContext.ui.getOrElse {
+      throw new SparkException("Parent SparkUI to attach this tab to not found!")
+    }
+  }
+}
