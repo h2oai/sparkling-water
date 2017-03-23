4ba9e76bbda387ce46bcaedc2cadeff6e627589a
Jakub Hava <jakub.hava@gmail.com>
2017-03-23 13:38:40 +0100

    [SW-265] Use Spark Session in public API instead of Spark Context

--- core/src/test/scala/org/apache/spark/h2o/H2OConfTestSuite.scala
+++ core/src/test/scala/org/apache/spark/h2o/H2OConfTestSuite.scala
@@ -17,6 +17,7 @@
 package org.apache.spark.h2o
 
 import org.apache.spark.h2o.utils.SparkTestContext
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.{SparkConf, SparkContext}
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
@@ -49,13 +50,13 @@ class H2OConfTestSuite extends FunSuite
       .set("spark.ext.h2o.client.web.port", "13321")
       .set("spark.ext.h2o.dummy.rdd.mul.factor", "2")
 
-    sc = new SparkContext("local", "test-local", sparkConf)
+    val spark = SparkSession.builder().master("local").appName("test-local").config(sparkConf).getOrCreate()
 
     // We don't need to have H2OContext here started and since it has private constructor
     // and getOrCreate methods automatically start H2OContext, we use a little bit of reflection
-    val ctor = classOf[H2OContext].getDeclaredConstructor(classOf[SparkContext], classOf[H2OConf])
+    val ctor = classOf[H2OContext].getDeclaredConstructor(classOf[SparkSession], classOf[H2OConf])
     ctor.setAccessible(true)
-    hc = ctor.newInstance(sc, new H2OConf(sc))
+    hc = ctor.newInstance(spark, new H2OConf(spark))
     val conf = hc.getConf
 
     // Test passed values
