1a709fea733d16794962843d08719d06e4af9d02
Jakub Háva <jakub.hava@gmail.com>
2017-04-17 13:38:18 -0700

    [SW-400][SW-401][SW-365] Fix stopping of h2o when running standalone … (#255)
    * [SW-400][SW-401][SW-365] Fix stopping of h2o when running standalone python jobs. Also ensures that when client doesn't connect to external cluster in specified timeout, the cluster will kill itself.
    
    This fix introduces two additional commands on the configuration for the external backend:
     - setClientConnectionTimeout - sets the timeout in milliseconds how long the cluster waits for the client to conenct before it kill itself
     - setClientCheckRetryTimeout - sets the timeout in milliseconds how often the cluster checks for the presence of the client
    
    The following methods are available also in pysparkling as
     - set_client_connection_timeout
     - set_client_check_retry_timeout
    
    This fix does several additional fixes:
     - better log message when stopping the context in pysparkling
     - removes unnecessary shutdown hook in external h2o backend
     - use H2O.orderShutdown instead of shutdown
    
    * Comment the new setters
    
    (cherry picked from commit 7812a3e3c3fcbb869991dca912bfb50f34ebd0f3)

--- py/pysparkling/conf.py
+++ py/pysparkling/conf.py
@@ -94,7 +94,15 @@ class H2OConf(object):
     def set_h2o_node_log_level(self, level):
         self._jconf.setH2ONodeLogLevel(level)
         return self
+
     def set_cluster_start_timeout(self, timeout):
+        """Set timeout for start of external cluster. If the cluster is not able to cloud within the timeout the
+        the exception is thrown.
+
+        Arguments:
+        timeout -- timeout in seconds
+        
+        """
         self._jconf.setClusterStartTimeout(timeout)
         return self
 
@@ -102,7 +110,34 @@ class H2OConf(object):
         self._jconf.setH2OClientLogLevel(level)
         return self
 
+    def set_client_connection_timeout(self, timeout):
+        """Set timeout for watchdog client connection in external cluster mode. If the client is not connected to the
+         cluster within the specified time, the cluster kill itself.
+
+        Arguments:
+        timeout -- timeout in milliseconds
+        
+        """
+        self._jconf.setClientConnectionTimeout(timeout)
+        return self
+
+    def set_client_check_retry_timeout(self, timeout):
+        """Set retry interval how often nodes in the external cluster mode check for the presence of the h2o client.
+        
+        Arguments:
+        timeout -- timeout in milliseconds
+        
+        """
+        self._jconf.setClientCheckRetryTimeout(timeout)
+        return self
+
 # getters
+
+    def client_connection_timeout(self):
+        return self._jconf.clientConnectionTimeout()
+
+    def client_check_retry_timeout(self):
+        return self._jconf.clientCheckRetryTimeout()
 
     def cluster_start_timeout(self):
         return self._jconf.clusterStartTimeout()
