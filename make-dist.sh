dcb048b31e08f2c98e395606a0c243b702da9b0d
Michal Malohlava <michal.malohlava@gmail.com>
2017-04-18 17:57:39 -0700

    [SW-406] Append Kluster instruction to SW download page. (#261)
    * [SW-406] Append Kluster instruction to SW download page.
    
    * List of all supported Hadoop distributions.
    
    (cherry picked from commit a4842bd3110756465be5d544c039475cbb4f7fb0)

--- make-dist.sh
+++ make-dist.sh
@@ -66,6 +66,10 @@ ZIP_FILE="$DIST_BUILD_DIR/$ZIP_NAME"
 
 # Make distribution package and put it into dist directory
 rsync -rtvW --files-from "$TOPDIR/demofiles.list" "$TOPDIR/" "$DEST_DIR/"
+# Print available H2O Hadoop distributions to config file
+H2O_DRIVERS_LIST="standalone $(./gradlew -q :sparkling-water-assembly-h2o:printHadoopDistributions)"
+echo "$H2O_DRIVERS_LIST" > "$DEST_DIR/h2o_drivers.txt"
+
 ( 
  cd private
  zip -r "$ZIP_FILE" "$DEST_DIRNAME"
@@ -73,7 +77,8 @@ rsync -rtvW --files-from "$TOPDIR/demofiles.list" "$TOPDIR/" "$DEST_DIR/"
 # Copy scaladoc
 rsync -rtvW "$SCALADOC_SRC_DIR" "$SCALADOC_DST_DIR"
 
-GITHASH=`git rev-parse --verify HEAD`
+GITHASH=$(git rev-parse --verify HEAD)
+GITBRANCH=$(git rev-parse --verify --abbrev-ref HEAD)
 
 if [ "${H2O_NAME}" == "master" ]; then
   H2O_BRANCH_NAME="master"
@@ -87,10 +92,13 @@ H2O_BUILD_NUMBER=${H2O_BUILD}
 # Copy dist dir files
 cat "$DIST_DIR/index.html" \
   | sed -e "s/SUBST_PROJECT_VERSION/$VERSION/g"\
+  | sed -e "s/SUBST_PROJECT_PATCH_VERSION/$patch_version/g"\
   | sed -e "s/SUBST_PROJECT_GITHASH/${GITHASH}/g"\
+  | sed -e "s~SUBST_PROJECT_GITBRANCH~${GITBRANCH}~g"\
   | sed -e "s/SUBST_H2O_VERSION/${H2O_VERSION}/g"\
   | sed -e "s/SUBST_H2O_BUILD/${H2O_BUILD}/g"\
   | sed -e "s/SUBST_H2O_NAME/${H2O_NAME}/g"\
+  | sed -e "s/SUBST_H2O_DRIVERS_LIST/${H2O_DRIVERS_LIST}/g"\
   | sed -e "s/SUBST_SPARK_VERSION/${SPARK_VERSION}/g"\
   | sed -e "s/SUBST_H2O_BRANCH_NAME/${H2O_BRANCH_NAME}/g"\
   > "$DIST_BUILD_DIR/index.html"
@@ -103,9 +111,11 @@ cat "$DIST_DIR/buildinfo.json" \
   \
   | sed -e "s/SUBST_PROJECT_VERSION/${VERSION}/g" \
   | sed -e "s/SUBST_LAST_COMMIT_HASH/${GITHASH}/g" \
+  | sed -e "s~SUBST_PROJECT_GITBRANCH~${GITBRANCH}~g" \
   \
   | sed -e "s/SUBST_H2O_NAME/${H2O_NAME}/g"\
   | sed -e "s/SUBST_H2O_VERSION/${H2O_VERSION}/g"\
+  | sed -e "s/SUBST_H2O_DRIVERS_LIST/${H2O_DRIVERS_LIST}/g"\
   \
   | sed -e "s/SUBST_H2O_PROJECT_VERSION/${H2O_PROJECT_VERSION}/g"\
   | sed -e "s/SUBST_H2O_BRANCH_NAME/${H2O_BRANCH_NAME}/g"\
