2c092dfc26425ae8a63a7785eea13fc44a948ac9
Michal Malohlava <michal.malohlava@gmail.com>
2017-04-27 08:52:31 -0700

    [SW-421] Skip yarn files delete (#267)
    * [SW-421] Remove deleting files on YARN
    The delete was marking Yarn job as failed, since we deleted yarn temporary files.
    
    This commit also contains some stylistic changes and improves logging messages to be more consistent.
    
    (cherry picked from commit 423b0129903ce7b461a4232b592ed77491c7df06)

--- core/src/main/scala/org/apache/spark/h2o/backends/external/ExternalH2OBackend.scala
+++ core/src/main/scala/org/apache/spark/h2o/backends/external/ExternalH2OBackend.scala
@@ -18,16 +18,15 @@
 package org.apache.spark.h2o.backends.external
 
 
-import java.io.{File, FileInputStream, IOException}
+import java.io.{File, FileInputStream}
 import java.util.Properties
 
-import org.apache.hadoop.fs.{FileSystem, Path}
-import org.apache.spark.h2o.backends.SparklingBackend
+import org.apache.spark.h2o.backends.{SharedBackendConf, SparklingBackend}
 import org.apache.spark.h2o.utils.NodeDesc
 import org.apache.spark.h2o.{H2OConf, H2OContext}
 import org.apache.spark.internal.Logging
 import water.api.RestAPIManager
-import water.{H2O, H2OStarter, UDPClientEvent}
+import water.{H2O, H2OStarter}
 
 import scala.io.Source
 import scala.util.Random
@@ -42,8 +41,7 @@ class ExternalH2OBackend(val hc: H2OContext) extends SparklingBackend with Exter
   def launchH2OOnYarn(sparkAppId: String, conf: H2OConf): String = {
     import ExternalH2OBackend._
 
-    var cmdToLaunch = Seq[String]("hadoop",
-      "jar", conf.h2oDriverPath.get)
+    var cmdToLaunch = Seq[String]("hadoop", "jar", conf.h2oDriverPath.get)
 
     conf.sslConf match {
       case Some(ssl) =>
@@ -51,14 +49,14 @@ class ExternalH2OBackend(val hc: H2OContext) extends SparklingBackend with Exter
         sslConfig.load(new FileInputStream(ssl))
         cmdToLaunch = cmdToLaunch ++ Array("-files", sslConfig.get("h2o_ssl_jks_internal") + "," + sslConfig.get("h2o_ssl_jts"))
         cmdToLaunch = cmdToLaunch ++ Array("-internal_security", ssl)
-        logInfo(s"Running external cluster in encrypted mode with config $ssl")
+        logInfo(s"Running external H2O cluster in encrypted mode with config: $ssl")
       case _ =>
     }
     // Application tags shown in Yarn Resource Manager UI
     val yarnAppTags = s"${TAG_EXTERNAL_H2O},${TAG_SPARK_APP.format(sparkAppId)}"
-    
+
     cmdToLaunch = cmdToLaunch ++ Seq[String](
-      conf.YARNQueue.map("-Dmapreduce.job.queuename=" + _ ).getOrElse(""),
+      conf.YARNQueue.map("-Dmapreduce.job.queuename=" + _).getOrElse(""),
       s"-Dmapreduce.job.tags=${yarnAppTags}",
       "-nodes", conf.numOfExternalH2ONodes.get,
       "-notify", conf.clusterInfoFile.get,
@@ -74,7 +72,7 @@ class ExternalH2OBackend(val hc: H2OContext) extends SparklingBackend with Exter
       "-J", "-watchdog_client_retry_timeout", "-J", conf.clientCheckRetryTimeout.toString
     )
 
-    // start external h2o cluster and log the output
+    // start external H2O cluster and log the output
     logInfo("Command used to start H2O on yarn: " + cmdToLaunch.mkString(" "))
 
     import scala.sys.process._
@@ -95,14 +93,17 @@ class ExternalH2OBackend(val hc: H2OContext) extends SparklingBackend with Exter
     logError(processErr.toString)
 
 
-    if(!new File(hc.getConf.clusterInfoFile.get).exists()){
+    if (!new File(hc.getConf.clusterInfoFile.get).exists()) {
       throw new RuntimeException(
-            "Cluster notification file wasn't created. The possible causes are: \n" +
-            "   1) The timeout for clouding up is too small and H2O didn't cloud up. \n" +
-                "In that case please try to increase the timeout for starting the external as: \n" +
-                "Python: H2OConf(sc).set_cluster_start_timeout(timeout).... \n" +
-                "Scala:  new H2OConf(sc).setClusterStartTimeout(timeout).... \n" +
-            "   2) The file couldn't be created because of missing write rights."
+        """
+          |Cluster notification file could not be created. The possible causes are:
+          |
+          |1) External H2O cluster did not cloud within the pre-defined timeout. In that case, please try
+          |   to increase the timeout for starting the external cluster as:
+          |   Python: H2OConf(sc).set_cluster_start_timeout(timeout)....
+          |   Scala:  new H2OConf(sc).setClusterStartTimeout(timeout)....
+          |
+          |2) The file could not be created because of missing write rights.""".stripMargin
       )
     }
     // get ip port
@@ -118,40 +119,21 @@ class ExternalH2OBackend(val hc: H2OContext) extends SparklingBackend with Exter
     ipPort
   }
 
-  private def deleteYarnFiles(): Unit = {
-    try {
-      val hdfs = org.apache.hadoop.fs.FileSystem.get(hc.sparkContext.hadoopConfiguration)
-      hdfs.delete(new Path(hc.getConf.HDFSOutputDir.get), true)
-    }catch {
-      case e: Exception =>
-        logError(s"Error when deleting HDFS output dir at ${hc.getConf.HDFSOutputDir.get}" +
-          s", original message: ${e.getMessage}")
-    }
-    try {
-      new File(hc.getConf.clusterInfoFile.get).delete()
-    } catch {
-      case e: Exception =>
-        logError(s"Error when deleting cluster info file at ${hc.getConf.clusterInfoFile.get}" +
-          s", original message: ${e.getMessage}")
-    }
-  }
-
   override def init(): Array[NodeDesc] = {
     if (hc.getConf.isAutoClusterStartUsed) {
       // start h2o instances on yarn
-      logInfo("Starting H2O cluster on YARN")
+      logInfo("Starting the external H2O cluster on YARN.")
       val ipPort = launchH2OOnYarn(hc.sparkContext.applicationId, hc.getConf)
       hc._conf.setH2OCluster(ipPort)
     }
-    // Start H2O in client mode and connect to existing H2O Cluster
-    logTrace("Starting H2O on client mode and connecting it to existing h2o cluster")
+    logTrace("Starting H2O client node and connecting to external H2O cluster.")
 
-    val h2oClientArgs = if(hc.getConf.isAutoClusterStartUsed){
+    val h2oClientArgs = if (hc.getConf.isAutoClusterStartUsed) {
       getH2OClientArgs(hc.getConf) ++ Array("-watchdog_client")
-    }else{
+    } else {
       getH2OClientArgs(hc.getConf)
     }
-    logDebug(s"Arguments used for launching h2o client node: ${h2oClientArgs.mkString(" ")}")
+    logDebug(s"Arguments used for launching the H2O client node: ${h2oClientArgs.mkString(" ")}")
     H2OStarter.start(h2oClientArgs, false)
 
     if (hc.getConf.numOfExternalH2ONodes.isDefined) {
@@ -162,44 +144,37 @@ class ExternalH2OBackend(val hc: H2OContext) extends SparklingBackend with Exter
     H2O.finalizeRegistration()
 
     if (cloudMembers.length == 0) {
-      if (hc.getConf.h2oDriverPath.isEmpty) {
+      if (hc.getConf.isManualClusterStartUsed) {
         throw new H2OClusterNotRunning(
           s"""
-           |
-          |H2O Cluster is not running or couldn't be connected to. Provided configuration:
-           |   cloud name            : ${hc.getConf.cloudName.get}
-
-           |   cloud representative  : ${hc.getConf.h2oCluster.getOrElse(
-            "Not set, connecting to cluster in multicast mode")}
-
-           |
-          | It is possible that in case you provided only cloud name h2o is not able to cloud up because multicast
-           | communication is limited in your network. In that case please consider setting "${ExternalBackendConf.
-            PROP_EXTERNAL_CLUSTER_REPRESENTATIVE._1}
-"
-           | property and starting nodes in h2o cluster with -flatfile option describing the cluster.
+             |External H2O cluster is not running or could not be connected to. Provided configuration:
+             |  cluster name            : ${hc.getConf.cloudName.get}
+             |  cluster representative  : ${hc.getConf.h2oCluster.getOrElse("Not set, using multi-cast!")}
+             |  cluster start timeout   : ${hc.getConf.clusterStartTimeout} seconds"
+             |
+             |It is possible that in case you provided only the cluster name, h2o is not able to cloud up
+             |because multi-cast communication is limited in your network. In that case, please consider starting the
+             |external H2O cluster with flatfile and set the following configuration '${
+            ExternalBackendConf.
+              PROP_EXTERNAL_CLUSTER_REPRESENTATIVE._1
+          }'
         """.stripMargin)
-        }else{
-        throw new H2OClusterNotRunning("Problem during connection to h2o cluster started on yarn. Please check the logs")
+      } else {
+        throw new H2OClusterNotRunning("Problem with connecting to external H2O cluster started on yarn." +
+          "Please check the YARN logs.")
       }
     }
-
     cloudMembers
   }
 
-  override def backendUIInfo: Seq[(String,String)] = {
+  override def backendUIInfo: Seq[(String, String)] = {
     Seq(
       ("External backend YARN AppID", yarnAppId),
       ("External IP", externalIP)
-    ).filter(_._2.nonEmpty).map{ case (k,v) => (k, v.get)}
+    ).filter(_._2.nonEmpty).map { case (k, v) => (k, v.get) }
   }
 
   override def stop(stopSparkContext: Boolean): Unit = {
-    // stop only external h2o cluster running on yarn
-    // otherwise stopping is not supported
-    if(hc.getConf.isAutoClusterStartUsed){
-      deleteYarnFiles()
-    }
     if (stopSparkContext) {
       hc.sparkContext.stop()
     }
@@ -210,16 +185,19 @@ class ExternalH2OBackend(val hc: H2OContext) extends SparklingBackend with Exter
     super.checkAndUpdateConf(conf)
 
 
-    if(conf.isAutoClusterStartUsed) {
-      lazy val driverPath = sys.env.get("H2O_EXTENDED_JAR")
-      if(conf.h2oDriverPath.isEmpty && driverPath.isEmpty){
+    if (conf.isAutoClusterStartUsed) {
+      lazy val driverPath = sys.env.get(ExternalH2OBackend.ENV_H2O_EXTENDED_JAR)
+      if (conf.h2oDriverPath.isEmpty && driverPath.isEmpty) {
         throw new IllegalArgumentException(
-          """Path to h2o driver has to be specified when using automatic cluster start either vie method available
-            | on the configuration or H2O_EXTENDED_JAR property
+          s"""Path to the H2O extended driver has to be specified when using automatic cluster start.
+             |It can be specified either via method available on the configuration object or
+             |using the '${ExternalH2OBackend.ENV_H2O_EXTENDED_JAR}' environmental property.
           """.stripMargin)
       }
       if (conf.h2oDriverPath.isEmpty && driverPath.isDefined) {
-        log.info(s"Obtaining path to extended h2o driver from environment variable. Specified path is ${driverPath.get}")
+        log.info(
+          s"""Obtaining path to the extended H2O driver from the environment variable.
+             |Specified path is: ${driverPath.get}""".stripMargin)
         conf.setH2ODriverPath(driverPath.get)
       }
 
@@ -228,22 +206,23 @@ class ExternalH2OBackend(val hc: H2OContext) extends SparklingBackend with Exter
       }
 
       if (conf.numOfExternalH2ONodes.isEmpty) {
-        throw new IllegalArgumentException("Number of h2o nodes has to be specified in external auto start backend mode.")
+        throw new IllegalArgumentException("Number of external H2O nodes has to be specified in the auto H2O external start mode!")
       }
 
       if (conf.HDFSOutputDir.isEmpty) {
         conf.setHDFSOutputDir(conf.cloudName.get)
       }
 
-      if(conf.clusterInfoFile.isEmpty){
+      if (conf.clusterInfoFile.isEmpty) {
         conf.setClusterConfigFile("notify_" + conf.cloudName.get)
       }
 
     } else {
       if (conf.cloudName.isEmpty) {
         throw new IllegalArgumentException(
-          """Cloud name has to be specified when using external backend cluster mode in manual start mode. It can be set either using H2OConf
-            |instance or via 'spark.ext.h2o.cloud.name' spark configuration property""".stripMargin)
+          s"""Cluster name has to be specified when using the external H2O cluster mode in the manual start mode.
+             |It can be set either on the configuration object or via '${SharedBackendConf.PROP_CLOUD_NAME._1}'
+             |spark configuration property""".stripMargin)
       }
     }
     conf
@@ -257,6 +236,9 @@ object ExternalH2OBackend {
   val TAG_SPARK_APP = "Sparkling-Water/Spark/%s"
   // Job name for H2O Yarn job
   val H2O_JOB_NAME = "H2O_via_SparklingWater_%s"
+
+  // Name of the environmental property, which may contain path to the external H2O driver
+  val ENV_H2O_EXTENDED_JAR = "H2O_EXTENDED_JAR"
 }
 
 class H2OClusterNotRunning(msg: String) extends Exception(msg) with NoStackTrace
