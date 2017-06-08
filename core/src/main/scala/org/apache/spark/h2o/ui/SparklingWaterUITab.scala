34a9fd8b45c149f2280f22c332254b826648d876
Jakub Hava <jakub.hava@gmail.com>
2017-06-08 11:47:58 +0200

    [SW-426] Provide driver logs and link to download the logs in the spark UI and history server
    (cherry picked from commit 875fad9a27f61c2509c9497ecce2400b8a511ce3)

--- core/src/main/scala/org/apache/spark/h2o/ui/SparklingWaterUITab.scala
+++ core/src/main/scala/org/apache/spark/h2o/ui/SparklingWaterUITab.scala
@@ -23,7 +23,7 @@ import org.apache.spark.ui.{SparkUI, SparkUITab}
 /**
   * Enrich Spark UI by a Sparkling Water specific tab.
   */
-private[h2o] class SparklingWaterUITab(val listener: SparklingWaterListener, parent: SparkUI)
+private[h2o] class SparklingWaterUITab(val listener: SparklingWaterListener, val parent: SparkUI)
   extends SparkUITab(parent, "sparkling-water") {
 
   override val name = "Sparkling Water \u2728"
