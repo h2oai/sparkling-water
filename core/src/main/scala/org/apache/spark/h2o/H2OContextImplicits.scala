f8c570e9fb62ef7f22fede12456593bbea61093a
Jakub Hava <jakub.hava@gmail.com>
2017-06-13 12:28:11 +0200

    [SW-452] Add missing spaces after , in H2OContextImplicits
    (cherry picked from commit 16cd4ec9e4812eaecb59992ed4aa038d9d00faa5)

--- core/src/main/scala/org/apache/spark/h2o/H2OContextImplicits.scala
+++ core/src/main/scala/org/apache/spark/h2o/H2OContextImplicits.scala
@@ -30,25 +30,25 @@ abstract class H2OContextImplicits {
   protected def _h2oContext: H2OContext
   /** Implicit conversion from RDD[Supported type] to H2OFrame */
   implicit def asH2OFrameFromRDDProduct[A <: Product : TypeTag](rdd : RDD[A]): H2OFrame = _h2oContext.asH2OFrame(rdd, None)
-  implicit def asH2OFrameFromRDDString(rdd: RDD[String]): H2OFrame = _h2oContext.asH2OFrame(rdd,None)
-  implicit def asH2OFrameFromRDDBool(rdd: RDD[Boolean]): H2OFrame = _h2oContext.asH2OFrame(rdd,None)
-  implicit def asH2OFrameFromRDDDouble(rdd: RDD[Double]): H2OFrame = _h2oContext.asH2OFrame(rdd,None)
-  implicit def asH2OFrameFromRDDLong(rdd: RDD[Long]): H2OFrame = _h2oContext.asH2OFrame(rdd,None)
-  implicit def asH2OFrameFromRDDByte(rdd: RDD[Byte]): H2OFrame = _h2oContext.asH2OFrame(rdd,None)
-  implicit def asH2OFrameFromRDDShort(rdd: RDD[Short]): H2OFrame = _h2oContext.asH2OFrame(rdd,None)
-  implicit def asH2OFrameFromRDDTimeStamp(rdd: RDD[java.sql.Timestamp]): H2OFrame = _h2oContext.asH2OFrame(rdd,None)
-  implicit def asH2OFrameFromRDDLabeledPoint(rdd: RDD[LabeledPoint]): H2OFrame = _h2oContext.asH2OFrame(rdd,None)
+  implicit def asH2OFrameFromRDDString(rdd: RDD[String]): H2OFrame = _h2oContext.asH2OFrame(rdd, None)
+  implicit def asH2OFrameFromRDDBool(rdd: RDD[Boolean]): H2OFrame = _h2oContext.asH2OFrame(rdd, None)
+  implicit def asH2OFrameFromRDDDouble(rdd: RDD[Double]): H2OFrame = _h2oContext.asH2OFrame(rdd, None)
+  implicit def asH2OFrameFromRDDLong(rdd: RDD[Long]): H2OFrame = _h2oContext.asH2OFrame(rdd, None)
+  implicit def asH2OFrameFromRDDByte(rdd: RDD[Byte]): H2OFrame = _h2oContext.asH2OFrame(rdd, None)
+  implicit def asH2OFrameFromRDDShort(rdd: RDD[Short]): H2OFrame = _h2oContext.asH2OFrame(rdd, None)
+  implicit def asH2OFrameFromRDDTimeStamp(rdd: RDD[java.sql.Timestamp]): H2OFrame = _h2oContext.asH2OFrame(rdd, None)
+  implicit def asH2OFrameFromRDDLabeledPoint(rdd: RDD[LabeledPoint]): H2OFrame = _h2oContext.asH2OFrame(rdd, None)
 
   /** Implicit conversion from RDD[Supported type] to H2OFrame key */
-  implicit def toH2OFrameKeyFromRDDProduct[A <: Product : TypeTag](rdd : RDD[A]): Key[_] = _h2oContext.toH2OFrameKey(rdd,None)
-  implicit def toH2OFrameKeyFromRDDString(rdd: RDD[String]): Key[_] = _h2oContext.toH2OFrameKey(rdd,None)
-  implicit def toH2OFrameKeyFromRDDBool(rdd: RDD[Boolean]): Key[_] = _h2oContext.toH2OFrameKey(rdd,None)
-  implicit def toH2OFrameKeyFromRDDDouble(rdd: RDD[Double]): Key[_] = _h2oContext.toH2OFrameKey(rdd,None)
-  implicit def toH2OFrameKeyFromRDDLong(rdd: RDD[Long]): Key[_] = _h2oContext.toH2OFrameKey(rdd,None)
-  implicit def toH2OFrameKeyFromRDDByte(rdd: RDD[Byte]): Key[_] = _h2oContext.toH2OFrameKey(rdd,None)
-  implicit def toH2OFrameKeyFromRDDShort(rdd: RDD[Short]): Key[_] = _h2oContext.toH2OFrameKey(rdd,None)
-  implicit def toH2OFrameKeyFromRDDTimeStamp(rdd: RDD[java.sql.Timestamp]): Key[_] = _h2oContext.toH2OFrameKey(rdd,None)
-  implicit def toH2OFrameKeyFromRDDLabeledPoint(rdd: RDD[LabeledPoint]): Key[_] = _h2oContext.toH2OFrameKey(rdd,None)
+  implicit def toH2OFrameKeyFromRDDProduct[A <: Product : TypeTag](rdd : RDD[A]): Key[_] = _h2oContext.toH2OFrameKey(rdd, None)
+  implicit def toH2OFrameKeyFromRDDString(rdd: RDD[String]): Key[_] = _h2oContext.toH2OFrameKey(rdd, None)
+  implicit def toH2OFrameKeyFromRDDBool(rdd: RDD[Boolean]): Key[_] = _h2oContext.toH2OFrameKey(rdd, None)
+  implicit def toH2OFrameKeyFromRDDDouble(rdd: RDD[Double]): Key[_] = _h2oContext.toH2OFrameKey(rdd, None)
+  implicit def toH2OFrameKeyFromRDDLong(rdd: RDD[Long]): Key[_] = _h2oContext.toH2OFrameKey(rdd, None)
+  implicit def toH2OFrameKeyFromRDDByte(rdd: RDD[Byte]): Key[_] = _h2oContext.toH2OFrameKey(rdd, None)
+  implicit def toH2OFrameKeyFromRDDShort(rdd: RDD[Short]): Key[_] = _h2oContext.toH2OFrameKey(rdd, None)
+  implicit def toH2OFrameKeyFromRDDTimeStamp(rdd: RDD[java.sql.Timestamp]): Key[_] = _h2oContext.toH2OFrameKey(rdd, None)
+  implicit def toH2OFrameKeyFromRDDLabeledPoint(rdd: RDD[LabeledPoint]): Key[_] = _h2oContext.toH2OFrameKey(rdd, None)
 
   /** Implicit conversion from Spark DataFrame to H2OFrame */
   implicit def asH2OFrameFromDataFrame(df : DataFrame) : H2OFrame = _h2oContext.asH2OFrame(df, None)
@@ -62,7 +62,6 @@ abstract class H2OContextImplicits {
   /** Implicit conversion from Spark Dataset to H2OFrame key */
   implicit def toH2OFrameKeyFromDataset[T<: Product : TypeTag](ds: Dataset[T]) : Key[Frame] = _h2oContext.toH2OFrameKey(ds, None)
 
-
   /** Implicit conversion from Frame(H2O) to H2OFrame */
   implicit def asH2OFrameFromFrame(fr: Frame) : H2OFrame = new H2OFrame(fr)
 
@@ -70,6 +69,7 @@ abstract class H2OContextImplicits {
   implicit def toH2OFrameKeyFromFrame[T <: Frame](fr: T): Key[Frame] = fr._key
 
   implicit def toH2OFrameKeyFromH2OFrame(fr: H2OFrame): Key[Frame] = fr.key
+
   /** Transform given Scala symbol to String */
   implicit def symbolToString(sy: scala.Symbol): String = sy.name
 }
