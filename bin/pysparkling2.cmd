32d858beb7f3671ce39c8a60a8b7d3bcf023059c
Jakub Hava <jakub.hava@gmail.com>
2017-06-23 17:54:10 +0200

    revert

--- bin/pysparkling2.cmd
+++ bin/pysparkling2.cmd
@@ -11,9 +11,11 @@ rem end of checking Sparkling environment
 call %LIBSW% :banner
 
 rem setup pysparkling command line
+rem Because of SPARK-18648 we need to put assembly also on driver/executor class paths
+SET SPARK_OPT_JARS=--jars !FAT_JAR_FILE! --conf spark.driver.extraClassPath=!FAT_JAR_FILE! --conf spark.executor.extraClassPath=!FAT_JAR_FILE!
 SET SPARK_OPT_PYFILES=--py-files !PY_ZIP_FILE!
 SET PYTHONPATH=%PY_ZIP_FILE%:%PYTHONPATH%
-call !SPARK_HOME!/bin/pyspark2.cmd !SPARK_OPT_PYFILES! %*
+call !SPARK_HOME!/bin/pyspark2.cmd !SPARK_OPT_PYFILES! %SPARK_OPT_JARS% %*
 
 exit /b %ERRORLEVEL%
 rem end of main script
