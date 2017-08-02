7c57f07fdf8cdf0b4a923455c9f8fe6b063ba171
Jakub Hava <jakub.hava@gmail.com>
2017-08-02 14:32:53 +0200

    [SW-508] More robust get-extended-h2o.sh
    (cherry picked from commit 42e8a3b41877c290f68c41a2bbf8aea52c92c9ac)

--- bin/get-extended-h2o.sh
+++ bin/get-extended-h2o.sh
@@ -21,6 +21,18 @@ EOF
 exit
 fi
 
+if ! [[ " ${AVAILABLE_H2O_DRIVERS} " =~ " ${1} " ]]; then
+cat <<EOF
+
+ ${1} is not supported Hadoop distribution.
+
+ Hadoop distributions supported by H2O:
+    ${AVAILABLE_H2O_DRIVERS}
+
+EOF
+exit
+fi
+
 if [ "$1" == "standalone" ]; then
     hadoop_version=""
 else
@@ -31,6 +43,7 @@ h2odriver_file="h2odriver-$H2O_VERSION.$H2O_BUILD-${hadoop_version}extended.jar"
 h2odriver_url="${S3_RELEASE_BUCKET}/rel-${MAJOR_VERSION}/${PATCH_VERSION}/extended/${h2odriver_file}"
 output_file="h2odriver-${hadoop_version}extended.jar"
 
-echo "Getting h2odriver from ${output_file}..."
-curl -f -o "$output_file" --progress-bar "$h2odriver_url" && echo "H2O driver saved as ${output_file}" || echo "File not found: $h2odriver_url"
+echo
+echo "Getting extended H2O driver from ${h2odriver_url} ..."
+curl -f -o "$output_file" --progress-bar "$h2odriver_url" && echo && echo "Extended H2O driver saved into: $(pwd)/${output_file}" || echo "File not found: $h2odriver_url"
 
