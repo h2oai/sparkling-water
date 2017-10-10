383ba365aa0f8d37d49efa38ca9fbfd4348aee11
Jakub Hava <jakub.hava@gmail.com>
2017-10-10 14:19:32 +0200

    Make timeout configurable

--- core/src/main/scala/org/apache/spark/h2o/backends/external/ExternalH2OBackend.scala
+++ core/src/main/scala/org/apache/spark/h2o/backends/external/ExternalH2OBackend.scala
@@ -143,7 +143,7 @@ class ExternalH2OBackend(val hc: H2OContext) extends SparklingBackend with Exter
     }
     // Register web API for client
     RestAPIManager(hc).registerAll()
-    Thread.sleep(5000)
+    Thread.sleep(hc.sparkContext.getConf.get("spark.ext.h2o.external.deadlock.timeout", "5000").toInt)
     H2O.startServingRestApi()
 
     if (cloudMembers.length == 0) {
