f1676b016dbd32bf4265fcbab9256048c0e0fb30
Jakub Hava <jakub.hava@gmail.com>
2017-10-10 14:05:35 +0200

    fix

--- core/src/main/scala/org/apache/spark/h2o/backends/external/ExternalH2OBackend.scala
+++ core/src/main/scala/org/apache/spark/h2o/backends/external/ExternalH2OBackend.scala
@@ -143,7 +143,7 @@ class ExternalH2OBackend(val hc: H2OContext) extends SparklingBackend with Exter
     }
     // Register web API for client
     RestAPIManager(hc).registerAll()
-    Thread.sleep(3000)
+    Thread.sleep(5000)
     H2O.startServingRestApi()
 
     if (cloudMembers.length == 0) {
