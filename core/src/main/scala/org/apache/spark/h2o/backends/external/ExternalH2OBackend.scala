ff8921e59925b2746c7f925b42ed7d77a82ed458
Jakub Hava <jakub.hava@gmail.com>
2017-11-03 14:44:54 +0100

    remove extra comma

--- core/src/main/scala/org/apache/spark/h2o/backends/external/ExternalH2OBackend.scala
+++ core/src/main/scala/org/apache/spark/h2o/backends/external/ExternalH2OBackend.scala
@@ -71,7 +71,7 @@ class ExternalH2OBackend(val hc: H2OContext) extends SparklingBackend with Exter
       "-J", "-watchdog_stop_without_client",
       "-J", "-watchdog_client_connect_timeout", "-J", conf.clientConnectionTimeout.toString,
       "-J", "-watchdog_client_retry_timeout", "-J", conf.clientCheckRetryTimeout.toString,
-      "-J", "-watchdog_stop_without_client",
+      "-J", "-watchdog_stop_without_client"
     )
 
     if (!hc.getConf.h2oNodeWebEnabled) {
