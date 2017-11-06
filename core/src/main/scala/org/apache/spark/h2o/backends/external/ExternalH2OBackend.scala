50c46a8855d076533b60be99ef998c181d40707a
Jakub Hava <jakub.hava@gmail.com>
2017-11-06 11:29:25 +0100

    Disable

--- core/src/main/scala/org/apache/spark/h2o/backends/external/ExternalH2OBackend.scala
+++ core/src/main/scala/org/apache/spark/h2o/backends/external/ExternalH2OBackend.scala
@@ -68,10 +68,8 @@ class ExternalH2OBackend(val hc: H2OContext) extends SparklingBackend with Exter
       "-J", "-log_level", "-J", conf.h2oNodeLogLevel,
       "-timeout", conf.clusterStartTimeout.toString,
       "-disown",
-      "-J", "-watchdog_stop_without_client",
       "-J", "-watchdog_client_connect_timeout", "-J", conf.clientConnectionTimeout.toString,
       "-J", "-watchdog_client_retry_timeout", "-J", conf.clientCheckRetryTimeout.toString,
-      "-J", "-watchdog_stop_without_client"
     )
 
     if (!hc.getConf.h2oNodeWebEnabled) {
