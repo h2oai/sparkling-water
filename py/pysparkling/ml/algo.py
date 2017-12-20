585049f49f0c5dc972ba84541590210ca56b42ae
Jakub Háva <jakub.hava@gmail.com>
2017-12-20 15:34:33 +0100

    [SW-612] Port all arguments available to Scala ML to PySparkling ML (#505)
    (cherry picked from commit 3cd36810df8c95ecf113aa5e3c80973babe4289e)

--- py/pysparkling/ml/algo.py
+++ py/pysparkling/ml/algo.py
@@ -4,72 +4,95 @@ from pyspark.ml.util import JavaMLReadable, JavaMLWritable
 from pyspark.ml.wrapper import JavaEstimator, JavaModel, JavaTransformer, _jvm
 from pyspark.sql import SparkSession
 from pysparkling import *
-from .params import H2OAlgorithmParams
+from .params import H2OGBMParams, H2ODeepLearningParams
 
-
-class H2OGBM(JavaEstimator, H2OAlgorithmParams, JavaMLReadable, JavaMLWritable):
-
+class H2OGBM(JavaEstimator, H2OGBMParams, JavaMLReadable, JavaMLWritable):
     @keyword_only
-    def __init__(self, ratio=1.0, featuresCols=[], predictionCol=None):
+    def __init__(self, ratio=1.0, predictionCol=None, featuresCols=[], allStringColumnsToCategorical=True, nfolds=0,
+                 keepCrossValidationPredictions=False, keepCrossValidationFoldAssignment=False, parallelizeCrossValidation=False,
+                 seed=-1, distribution="AUTO", ntrees=50, maxDepth=5, minRows=10.0, nbins=20, nbinsCats=1024, minSplitImprovement=1e-5,
+                 histogramType="AUTO", r2Stopping=SparkSession.builder.getOrCreate()._jvm.Double.MAX_VALUE,
+                 nbinsTopLevel=1<<10, buildTreeOneNode=False, scoreTreeInterval=0,
+                 sampleRate=0.632, sampleRatePerClass=None, colSampleRateChangePerLevel=1.0, colSampleRatePerTree=1.0,
+                 learnRate=0.1, learnRateAnnealing=1.0, colSampleRate=1.0, maxAbsLeafnodePred=SparkSession.builder.getOrCreate()._jvm.Double.MAX_VALUE,
+                 predNoiseBandwidth=0.0):
         super(H2OGBM, self).__init__()
+        self._hc = H2OContext.getOrCreate(SparkSession.builder.getOrCreate(), verbose=False)
         self._java_obj = self._new_java_obj("org.apache.spark.ml.h2o.algos.H2OGBM",
                                             self.uid,
-                                            H2OContext.getOrCreate(SparkSession.builder.getOrCreate(), verbose=False)._jhc.h2oContext(),
-                                            H2OContext.getOrCreate(SparkSession.builder.getOrCreate(), verbose=False)._jsql_context
-                                            )
-        self._setDefault( ratio=1.0, featuresCols=[], predictionCol=None)
+                                            self._hc._jhc.h2oContext(),
+                                            self._hc._jsql_context)
+
+        self._setDefault(ratio=1.0, predictionCol=None, featuresCols=[], allStringColumnsToCategorical=True,
+                         nfolds=0, keepCrossValidationPredictions=False, keepCrossValidationFoldAssignment=False, parallelizeCrossValidation=False,
+                         seed=-1, distribution=self._hc._jvm.hex.genmodel.utils.DistributionFamily.valueOf("AUTO"),
+                         ntrees=50, maxDepth=5, minRows=10.0, nbins=20, nbinsCats=1024, minSplitImprovement=1e-5,
+                         histogramType=self._hc._jvm.hex.tree.SharedTreeModel.SharedTreeParameters.HistogramType.valueOf("AUTO"),
+                         r2Stopping=self._hc._jvm.Double.MAX_VALUE, nbinsTopLevel=1<<10, buildTreeOneNode=False, scoreTreeInterval=0,
+                         sampleRate=0.632, sampleRatePerClass=None, colSampleRateChangePerLevel=1.0, colSampleRatePerTree=1.0,
+                         learnRate=0.1, learnRateAnnealing=1.0, colSampleRate=1.0, maxAbsLeafnodePred=self._hc._jvm.Double.MAX_VALUE,
+                         predNoiseBandwidth=0.0)
         kwargs = self._input_kwargs
+        print self._input_kwargs
         self.setParams(**kwargs)
 
     @keyword_only
-    def setParams(self, ratio=1.0, featuresCols=[], predictionCol=None):
+    def setParams(self, ratio=1.0, predictionCol=None, featuresCols=[], allStringColumnsToCategorical=True,
+                  nfolds=0, keepCrossValidationPredictions=False, keepCrossValidationFoldAssignment=False,parallelizeCrossValidation=False,
+                  seed=-1, distribution="AUTO", ntrees=50, maxDepth=5, minRows=10.0, nbins=20, nbinsCats=1024, minSplitImprovement=1e-5,
+                  histogramType="AUTO", r2Stopping=SparkSession.builder.getOrCreate()._jvm.Double.MAX_VALUE,
+                  nbinsTopLevel=1<<10, buildTreeOneNode=False, scoreTreeInterval=0,
+                  sampleRate=0.632, sampleRatePerClass=None, colSampleRateChangePerLevel=1.0, colSampleRatePerTree=1.0,
+                  learnRate=0.1, learnRateAnnealing=1.0, colSampleRate=1.0, maxAbsLeafnodePred=SparkSession.builder.getOrCreate()._jvm.Double.MAX_VALUE,
+                  predNoiseBandwidth=0.0):
         kwargs = self._input_kwargs
+
+        # we need to convert few parameters explicitly to float as py4j can't handle that
+        if "minRows" in kwargs:
+            kwargs["minRows"] = float(kwargs["minRows"])
+        if "predNoiseBandwidth" in kwargs:
+            kwargs["predNoiseBandwidth"] = float(kwargs["predNoiseBandwidth"])
         return self._set(**kwargs)
 
     def _create_model(self, java_model):
         return H2OGBMModel(java_model)
 
-
 class H2OGBMModel(JavaModel, JavaMLWritable, JavaMLReadable):
     pass
 
-class H2ODeepLearning(JavaEstimator, H2OAlgorithmParams, JavaMLReadable, JavaMLWritable):
-
-    epochs = Param(Params._dummy(), "epochs", "The number of passes over the training dataset to be carried out")
-
-    l1 = Param(Params._dummy(), "l1", "A regularization method that constrains the absolute value of the weights and"
-                                      " has the net effect of dropping some weights (setting them to zero) from"
-                                      " a model to reduce complexity and avoid overfitting.")
-
-    l2 = Param(Params._dummy(), "l2", "A regularization method that constrains the sum of the squared weights."
-                                      " This method introduces bias into parameter estimates, but frequently"
-                                      " produces substantial gains in modeling as estimate variance is reduced.")
-
-    hidden = Param(Params._dummy(), "hidden", "The number and size of each hidden layer in the model")
-
+class H2ODeepLearning(JavaEstimator, H2ODeepLearningParams, JavaMLReadable, JavaMLWritable):
 
     @keyword_only
-    def __init__(self, ratio=1.0, featuresCols=[], predictionCol=None, epochs=10.0, l1=0.0, l2=0.0, hidden=[200,200]):
+    def __init__(self, ratio=1.0, predictionCol=None, featuresCols=[], allStringColumnsToCategorical=True,
+                 nfolds=0, keepCrossValidationPredictions=False, keepCrossValidationFoldAssignment=False,parallelizeCrossValidation=False,
+                 seed=-1, distribution="AUTO", epochs=10.0, l1=0.0, l2=0.0, hidden=[200,200]):
         super(H2ODeepLearning, self).__init__()
+        self._hc = H2OContext.getOrCreate(SparkSession.builder.getOrCreate(), verbose=False)
         self._java_obj = self._new_java_obj("org.apache.spark.ml.h2o.algos.H2ODeepLearning",
                                             self.uid,
-                                            H2OContext.getOrCreate(SparkSession.builder.getOrCreate(), verbose=False)._jhc.h2oContext(),
-                                            H2OContext.getOrCreate(SparkSession.builder.getOrCreate(), verbose=False)._jsql_context
-                                            )
-        self._setDefault( ratio=1.0, featuresCols=[], predictionCol=None, epochs=10.0, l1=0.0, l2=0.0, hidden=[200,200])
+                                            self._hc._jhc.h2oContext(),
+                                            self._hc._jsql_context)
+
+        self._setDefault(ratio=1.0, predictionCol=None, featuresCols=[], allStringColumnsToCategorical=True,
+                         nfolds=0, keepCrossValidationPredictions=False, keepCrossValidationFoldAssignment=False,parallelizeCrossValidation=False,
+                         seed=-1, distribution=self._hc._jvm.hex.genmodel.utils.DistributionFamily.valueOf("AUTO"),
+                         epochs=10.0, l1=0.0, l2=0.0, hidden=[200,200])
         kwargs = self._input_kwargs
         self.setParams(**kwargs)
 
     @keyword_only
-    def setParams(self, ratio=1.0, featuresCols=[], predictionCol=None, epochs=10.0, l1=0.0, l2=0.0, hidden=[200,200]):
+    def setParams(self, ratio=1.0, predictionCol=None, featuresCols=[], allStringColumnsToCategorical=True,
+                  nfolds=0, keepCrossValidationPredictions=False, keepCrossValidationFoldAssignment=False,parallelizeCrossValidation=False,
+                  seed=-1, distribution="AUTO", epochs=10.0, l1=0.0, l2=0.0, hidden=[200,200]):
         kwargs = self._input_kwargs
 
-        kwargs["epochs"] = float(kwargs["epochs"]) # we need to convert explicitly to float
+        # we need to convert few parameters explicitly to float as py4j can't handle that
+        if "epochs" in kwargs:
+            kwargs["epochs"] = float(kwargs["epochs"])
         return self._set(**kwargs)
 
     def _create_model(self, java_model):
         return H2ODeepLearningModel(java_model)
-
 
 
 class H2ODeepLearningModel(JavaModel, JavaMLWritable, JavaMLReadable):
