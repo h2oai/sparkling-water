3cd36810df8c95ecf113aa5e3c80973babe4289e
Jakub Háva <jakub.hava@gmail.com>
2017-12-20 15:34:33 +0100

    [SW-612] Port all arguments available to Scala ML to PySparkling ML (#505)

--- py/examples/pipelines/ham_or_spam_deep_learning.py
+++ py/examples/pipelines/ham_or_spam_deep_learning.py
@@ -35,19 +35,16 @@ tokenizer = RegexTokenizer(inputCol="text",
                            gaps=False,
                            pattern="[a-zA-Z]+")
 
-
 ## Remove ignored words
 stopWordsRemover = StopWordsRemover(inputCol=tokenizer.getOutputCol(),
                                     outputCol="filtered",
                                     stopWords=["the", "a", "", "in", "on", "at", "as", "not", "for"],
                                     caseSensitive=False)
 
-
 ## Hash the words
 hashingTF = HashingTF(inputCol=stopWordsRemover.getOutputCol(),
                       outputCol="wordToIndex",
                       numFeatures=1 << 10)
-
 
 ## Create inverse document frequencies model
 idf = IDF(inputCol=hashingTF.getOutputCol(),
@@ -65,7 +62,7 @@ dl = H2ODeepLearning(epochs=10,
 ## Remove all helper columns
 colPruner = ColumnPruner(columns=[idf.getOutputCol(), hashingTF.getOutputCol(), stopWordsRemover.getOutputCol(), tokenizer.getOutputCol()])
 
-##  Create the pipeline by defining all the stages
+## Create the pipeline by defining all the stages
 pipeline = Pipeline(stages=[tokenizer, stopWordsRemover, hashingTF, idf, dl, colPruner])
 
 ## Train the pipeline model
