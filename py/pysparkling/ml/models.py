62df89326df8a24d01a0f1af8cc79f20dc74e78e
Jakub Hava <jakub.hava@gmail.com>
2018-02-07 09:25:20 +0100

    [SW-708] Make sure H2OMojoModel does not required H2OContext to be initialized

--- py/pysparkling/ml/models.py
+++ py/pysparkling/ml/models.py
@@ -1,14 +1,15 @@
 from pyspark.sql import SparkSession
 from pyspark.ml.util import JavaMLReadable, JavaMLWritable
 from pyspark.ml.wrapper import JavaModel
-from pysparkling import *
+from pysparkling.initializer import Initializer
 
 class H2OMOJOModel(JavaModel, JavaMLWritable, JavaMLReadable):
 
     @staticmethod
     def create_from_mojo(path_to_mojo):
-        jvm = H2OContext.getOrCreate(SparkSession.builder.getOrCreate(), verbose=False)._jvm
-        return H2OMOJOModel(jvm.org.apache.spark.ml.h2o.models.JavaH2OMOJOModelHelper.createFromMojo(path_to_mojo))
+        spark_session = SparkSession.builder.getOrCreate()
+        Initializer.load_sparkling_jar(spark_session._sc)
+        return H2OMOJOModel(spark_session._jvm.org.apache.spark.ml.h2o.models.JavaH2OMOJOModelHelper.createFromMojo(path_to_mojo))
 
     def predict(self, dataframe):
         return self.transform(dataframe)
