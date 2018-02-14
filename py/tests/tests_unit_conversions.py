742da0ef3ac970fb435080038397e3007ba13ecb
Jakub Hava <jakub.hava@gmail.com>
2018-02-14 11:30:51 +0100

    [SW-720] Fix parquet import test on external backend

--- py/tests/tests_unit_conversions.py
+++ py/tests/tests_unit_conversions.py
@@ -235,7 +235,7 @@ class FrameTransformationsTest(unittest.TestCase):
     def test_simple_parquet_import(self):
         df = self._spark.sparkContext.parallelize([(num, "text") for num in range(0,100)]).toDF()
         df.write.mode('overwrite').parquet("file://" + os.path.abspath("build/tests_tmp/test.parquet"))
-        frame = h2o.import_file(path=os.path.abspath("build/tests_tmp/test.parquet"), pattern=".*\.parquet")
+        frame = h2o.upload_file(path=os.path.abspath("build/tests_tmp/test.parquet"), pattern=".*\.parquet")
         assert frame.ncols == len(df.columns)
         assert frame.nrows == df.count()
         assert frame[0, 1] == "text"
