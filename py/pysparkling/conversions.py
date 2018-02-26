513fb0835c621b55628383708ff8477873e620f1
Jakub Háva <jakub.hava@gmail.com>
2018-02-26 13:06:44 +0100

    [SW-728] Use new light endpoint introduced in 3.18.0.1 (#580)
    (cherry picked from commit 4340ea3dd2cebd37b75b5f0911ba9d0d55e9e416)

--- py/pysparkling/conversions.py
+++ py/pysparkling/conversions.py
@@ -4,49 +4,49 @@ from h2o.backend import H2OConnection
 class FrameConversions:
 
     @staticmethod
-    def _as_h2o_frame_from_RDD_String(h2oContext, rdd, frame_name):
+    def _as_h2o_frame_from_RDD_String(h2oContext, rdd, frame_name, full_cols=100):
         j_h2o_frame = h2oContext._jhc.asH2OFrameFromRDDString(rdd._to_java_object_rdd(), frame_name)
         j_h2o_frame_key = j_h2o_frame.key()
-        return H2OFrame.from_java_h2o_frame(j_h2o_frame,j_h2o_frame_key)
+        return H2OFrame.from_java_h2o_frame(j_h2o_frame,j_h2o_frame_key, full_cols)
 
     @staticmethod
-    def _as_h2o_frame_from_RDD_Bool(h2oContext, rdd, frame_name):
+    def _as_h2o_frame_from_RDD_Bool(h2oContext, rdd, frame_name, full_cols=100):
         j_h2o_frame = h2oContext._jhc.asH2OFrameFromRDDBool(rdd._to_java_object_rdd(), frame_name)
         j_h2o_frame_key = j_h2o_frame.key()
-        return H2OFrame.from_java_h2o_frame(j_h2o_frame,j_h2o_frame_key)
+        return H2OFrame.from_java_h2o_frame(j_h2o_frame,j_h2o_frame_key, full_cols)
 
     @staticmethod
-    def _as_h2o_frame_from_RDD_Int(h2oContext, rdd, frame_name):
+    def _as_h2o_frame_from_RDD_Int(h2oContext, rdd, frame_name, full_cols=100):
         j_h2o_frame = h2oContext._jhc.asH2OFrameFromRDDInt(rdd._to_java_object_rdd(), frame_name)
         j_h2o_frame_key = j_h2o_frame.key()
-        return H2OFrame.from_java_h2o_frame(j_h2o_frame,j_h2o_frame_key)
+        return H2OFrame.from_java_h2o_frame(j_h2o_frame,j_h2o_frame_key, full_cols)
 
     @staticmethod
-    def _as_h2o_frame_from_RDD_Double(h2oContext, rdd, frame_name):
+    def _as_h2o_frame_from_RDD_Double(h2oContext, rdd, frame_name, full_cols=100):
         j_h2o_frame = h2oContext._jhc.asH2OFrameFromPythonRDDDouble(rdd._to_java_object_rdd(), frame_name)
         j_h2o_frame_key = j_h2o_frame.key()
-        return H2OFrame.from_java_h2o_frame(j_h2o_frame,j_h2o_frame_key)
+        return H2OFrame.from_java_h2o_frame(j_h2o_frame,j_h2o_frame_key, full_cols)
 
     @staticmethod
-    def _as_h2o_frame_from_RDD_Float(h2oContext, rdd, frame_name):
-        return FrameConversions._as_h2o_frame_from_RDD_Double(h2oContext, rdd, frame_name)
+    def _as_h2o_frame_from_RDD_Float(h2oContext, rdd, frame_name, full_cols=100):
+        return FrameConversions._as_h2o_frame_from_RDD_Double(h2oContext, rdd, frame_name, full_cols)
 
     @staticmethod
-    def _as_h2o_frame_from_RDD_Long(h2oContext, rdd, frame_name):
+    def _as_h2o_frame_from_RDD_Long(h2oContext, rdd, frame_name, full_cols=100):
         j_h2o_frame = h2oContext._jhc.asH2OFrameFromPythonRDDLong(rdd._to_java_object_rdd(), frame_name)
         j_h2o_frame_key = j_h2o_frame.key()
-        return H2OFrame.from_java_h2o_frame(j_h2o_frame,j_h2o_frame_key)
+        return H2OFrame.from_java_h2o_frame(j_h2o_frame,j_h2o_frame_key, full_cols)
 
     @staticmethod
-    def _as_h2o_frame_from_dataframe(h2oContext, dataframe, frame_name):
+    def _as_h2o_frame_from_dataframe(h2oContext, dataframe, frame_name, full_cols=100):
         if dataframe.count() == 0:
             raise ValueError('Cannot transform empty H2OFrame')
         j_h2o_frame = h2oContext._jhc.asH2OFrame(dataframe._jdf, frame_name)
         j_h2o_frame_key = j_h2o_frame.key()
-        return H2OFrame.from_java_h2o_frame(j_h2o_frame,j_h2o_frame_key)
+        return H2OFrame.from_java_h2o_frame(j_h2o_frame,j_h2o_frame_key, full_cols)
 
     @staticmethod
-    def _as_h2o_frame_from_complex_type(h2oContext, dataframe, frame_name):
+    def _as_h2o_frame_from_complex_type(h2oContext, dataframe, frame_name, full_cols=100):
         # Creates a DataFrame from an RDD of tuple/list, list or pandas.DataFrame.
         # On scala backend, to transform RDD of Product to H2OFrame, we need to know Type Tag.
         # Since there is no alternative for Product class in Python, we first transform the rdd to dataframe
@@ -54,7 +54,7 @@ class FrameConversions:
         df = h2oContext._spark_session.createDataFrame(dataframe)
         j_h2o_frame = h2oContext._jhc.asH2OFrame(df._jdf, frame_name)
         j_h2o_frame_key = j_h2o_frame.key()
-        return H2OFrame.from_java_h2o_frame(j_h2o_frame,j_h2o_frame_key)
+        return H2OFrame.from_java_h2o_frame(j_h2o_frame,j_h2o_frame_key, full_cols)
 
     @staticmethod
     def init_scala_int_session():
