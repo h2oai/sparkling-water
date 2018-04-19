d1c982ea8de9a8b8bdb27a831a9db375d2dfa2c6
Jakub Háva <jakub.hava@gmail.com>
2018-04-19 13:39:05 +0200

    [SW-809] Remove unnecessary line from sw pipeline (#678)
    (cherry picked from commit 7f20c1440b76a6f3b460d33acb113d2d86f8d7db)

--- jenkins/sparklingWaterPipeline.groovy
+++ jenkins/sparklingWaterPipeline.groovy
@@ -127,7 +127,6 @@ def prepareSparklingWaterEnvironment() {
                     git clone https://github.com/h2oai/h2o-3.git
                     cd h2o-3
                     git checkout ${config.h2oBranch}
-                    ${env.WORKSPACE}/gradlew build -x check
                     cd ..
                     if [ ${config.backendMode} = external ]; then
                         # In this case, PySparkling build is driven by H2O_HOME property
