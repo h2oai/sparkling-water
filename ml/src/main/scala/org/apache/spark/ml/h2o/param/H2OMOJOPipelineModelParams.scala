d24e6df94b9349da1174ed467faf67da61b82e42
Jakub Háva <jakub.hava@gmail.com>
2018-06-11 12:52:14 +0200

    [SW-851] Properly type and name output columns of mojo pipeline (#734)
    * [SW-851] Properly type and name output columns of mojo pipeline
    
    The columns are now properly typed as double. Also, we currently store
    the prediction output in an array for each row, loosing the information
    about the name of the output column.
    
    This has been fixed as well, however the behaviour is disabled by
    default. It will be enabled by default in Spark 2.4
    
    (cherry picked from commit 32d928dbbe337d10d21e24656182a03acea3de20)

---
+++ ml/src/main/scala/org/apache/spark/ml/h2o/param/H2OMOJOPipelineModelParams.scala
@@ -0,0 +1,50 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package org.apache.spark.ml.h2o.param
+
+import org.apache.spark.ml.param._
+
+/**
+  * Parameters which need to be available on the model itself for prediction purposes. This can't be backed
+  * byt H2OAlgoParamsHelper as at the time of prediction we might be using mojo and binary parameters are not available.
+  */
+trait H2OMOJOPipelineModelParams extends Params {
+
+  //
+  // Param definitions
+  //
+  final val namedMojoOutputColumns: Param[Boolean] = new Param[Boolean](this, "namedMojoOutputColumns", "Mojo Output is not stored" +
+    " in the array but in the properly named columns")
+
+  //
+  // Default values
+  //
+  setDefault(namedMojoOutputColumns -> false)
+
+  //
+  // Getters
+  //
+  /** @group getParam */
+  def getNamedMojoOutputColumns() = $(namedMojoOutputColumns)
+
+  //
+  // Setters
+  //
+  /** @group setParam */
+  def setNamedMojoOutputColumns(value: Boolean): this.type = set(namedMojoOutputColumns, value)
+
+}
