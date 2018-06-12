b194317b6670642d17a99ec8cc00a4e6c98416f2
Jakub Háva <jakub.hava@gmail.com>
2018-06-12 13:09:28 +0200

    [SW-852] Create spark UDF used to extract predicted values (#740)
    (cherry picked from commit 1c825b4d11083df95eb815add25857cd0e814373)

--- ml/src/test/scala/org/apache/spark/ml/spark/models/H2OMOJOPipelineModelTest.scala
+++ ml/src/test/scala/org/apache/spark/ml/spark/models/H2OMOJOPipelineModelTest.scala
@@ -18,7 +18,6 @@
 package org.apache.spark.ml.spark.models
 
 import java.sql.{Date, Timestamp}
-import java.time.temporal.TemporalField
 
 import ai.h2o.mojos.runtime.utils.MojoDateTime
 import org.apache.spark.SparkContext
@@ -87,11 +86,22 @@ class H2OMOJOPipelineModelTest extends FunSuite with SparkTestContext {
 
 
     val transDf = mojo.transform(df)
+    val udfSelection = transDf.select(mojo.predictedValsFor("AGE"))
+    val normalSelection = transDf.select("prediction.preds")
+
     println(s"\n\nSpark Transformer Output:\n${transDf.dtypes.map { case (n, t) => s"${n}[${t}]" }.mkString(" ")}")
-    println("Predictions:")
-    val preds = transDf.select("prediction.preds").take(5)
-    assertPredictedValues(preds)
-    println(preds.mkString("\n"))
+
+    println("Predictions from normal selection:")
+    val valuesNormalSelection = normalSelection.take(5)
+    assertPredictedValues(valuesNormalSelection)
+    println(valuesNormalSelection.mkString("\n"))
+
+    // Verify also output of the udf prediction method. The UDF method always returns one column with correct name
+
+    println("Predictions from udf selection:")
+    val valuesUdfSelection = udfSelection.take(5)
+    assertPredictedValuesForNamedCols(valuesUdfSelection)
+    println(valuesUdfSelection.mkString("\n"))
   }
 
   test("Verify that output columns are correct when using the named columns") {
@@ -104,14 +114,32 @@ class H2OMOJOPipelineModelTest extends FunSuite with SparkTestContext {
     mojo.setNamedMojoOutputColumns(true)
 
     val transDf = mojo.transform(df)
+    val udfSelection = transDf.select(mojo.predictedValsFor("AGE"))
+    val normalSelection = transDf.select("prediction.AGE")
+
+    // Check that frames returned using udf and normal selection are the same
+    assert(udfSelection.schema.head.name==normalSelection.schema.head.name)
+    assert(udfSelection.schema.head.dataType==normalSelection.schema.head.dataType)
+    assert(udfSelection.first()==normalSelection.first())
+
     println("Predictions:")
-    val preds = transDf.select("prediction.AGE").take(5)
-    assert(preds(0).getDouble(0) == 65.36320409515132)
-    assert(preds(1).getDouble(0) == 64.96902128114817)
-    assert(preds(2).getDouble(0) == 64.96721023747583)
-    assert(preds(3).getDouble(0) == 65.78772654671035)
-    assert(preds(4).getDouble(0) == 66.11327967814829)
+    assertPredictedValuesForNamedCols(udfSelection.take(5))
   }
+
+  test("Selection using udf on non-existent column"){
+    val df = spark.read.option("header", "true").csv("examples/smalldata/prostate/prostate.csv")
+    // Test mojo
+    val mojo = H2OMOJOPipelineModel.createFromMojo(
+      this.getClass.getClassLoader.getResourceAsStream("mojo2data/pipeline.mojo"),
+      "prostate_pipeline.mojo")
+    mojo.setNamedMojoOutputColumns(true)
+
+    val transDf = mojo.transform(df)
+    intercept[IllegalArgumentException] {
+      transDf.select(mojo.predictedValsFor("I_DO_NOT_EXIST")).first()
+    }
+  }
+
 
   /**
     * The purpose of this test is to simply pass and don't throw NullPointerException
@@ -178,5 +206,13 @@ class H2OMOJOPipelineModelTest extends FunSuite with SparkTestContext {
     assert(preds(2).getSeq[Double](0).head == 64.96721023747583)
     assert(preds(3).getSeq[Double](0).head == 65.78772654671035)
     assert(preds(4).getSeq[Double](0).head == 66.11327967814829)
+  }
+
+  private def assertPredictedValuesForNamedCols(preds: Array[Row]): Unit = {
+    assert(preds(0).getDouble(0) == 65.36320409515132)
+    assert(preds(1).getDouble(0) == 64.96902128114817)
+    assert(preds(2).getDouble(0) == 64.96721023747583)
+    assert(preds(3).getDouble(0) == 65.78772654671035)
+    assert(preds(4).getDouble(0) == 66.11327967814829)
   }
 }
