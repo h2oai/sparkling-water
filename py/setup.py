05d83a6e19ece49934697e7d35bb2f45ba088a87
Jakub Háva <jakub.hava@gmail.com>
2018-06-14 15:53:16 +0200

    [SW-863] Integrate with Spark 2.3.1 (#753)

--- py/setup.py
+++ py/setup.py
@@ -46,7 +46,7 @@ setup(
     packages = find_packages(),
 
     # run-time dependencies
-    install_requires=['requests', 'tabulate', 'future', 'colorama>=0.3.8', 'pyspark==2.3.0'],
+    install_requires=['requests', 'tabulate', 'future', 'colorama>=0.3.8', 'pyspark>=2.3.0,<=2.3.1'],
 
     # bundled binary packages
     package_data={'sparkling_water': ['*.jar'],
