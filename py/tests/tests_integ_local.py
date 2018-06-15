a926a79d85717ba807f710866106d2f34b2c0422
Jakub Háva <jakub.hava@gmail.com>
2018-06-15 09:36:12 +0200

    [SW-876] FIx local PySparkling integtest on jenkins infrastracture (#755)
    (cherry picked from commit db89e4be5ebdf11c7dfef08875f4ec3f67c8dfd6)
    (cherry picked from commit 8a9717cecc9071a363f5ef060ea4b3d05925b2d1)

--- py/tests/tests_integ_local.py
+++ py/tests/tests_integ_local.py
@@ -22,58 +22,40 @@ import generic_test_utils
 from integ_test_utils import *
 import unittest
 
+
 class LocalIntegTestSuite(unittest.TestCase):
 
     def test_pipeline_gbm_mojo(self):
         env = IntegTestEnv()
-
-        env.set_spark_master("local")
-        env.conf("spark.yarn.max.executor.failures", 1) # In fail of executor, fail the test
-        env.conf("spark.executor.instances", 3)
-        env.conf("spark.executor.memory", "2g")
+        env.set_spark_master("local-cluster[3,1,2048]")
         env.conf("spark.ext.h2o.port.base", 63331)
-        env.conf("spark.driver.memory", "2g")
 
-        launch(env, "examples/pipelines/ham_or_spam_multi_algo.py", "gbm")
+        return_code = launch(env, "examples/pipelines/ham_or_spam_multi_algo.py", "gbm")
+        self.assertTrue(return_code == 0, "Process ended in a wrong way. It ended with return code "+str(return_code))
 
     def test_pipeline_deep_learning(self):
         env = IntegTestEnv()
-
-        env.set_spark_master("local")
-        # Configure YARN environment
-        env.conf("spark.yarn.max.executor.failures", 1) # In fail of executor, fail the test
-        env.conf("spark.executor.instances", 3)
-        env.conf("spark.executor.memory", "2g")
+        env.set_spark_master("local-cluster[3,1,2048]")
         env.conf("spark.ext.h2o.port.base", 63331)
-        env.conf("spark.driver.memory", "2g")
 
-        launch(env, "examples/pipelines/ham_or_spam_multi_algo.py", "dl")
+        return_code = launch(env, "examples/pipelines/ham_or_spam_multi_algo.py", "dl")
+        self.assertTrue(return_code == 0, "Process ended in a wrong way. It ended with return code "+str(return_code))
 
     def test_pipeline_automl(self):
         env = IntegTestEnv()
-
-        env.set_spark_master("local")
-        # Configure YARN environment
-        env.conf("spark.yarn.max.executor.failures", 1) # In fail of executor, fail the test
-        env.conf("spark.executor.instances", 3)
-        env.conf("spark.executor.memory", "2g")
+        env.set_spark_master("local-cluster[3,1,2048]")
         env.conf("spark.ext.h2o.port.base", 63331)
-        env.conf("spark.driver.memory", "2g")
 
-        launch(env, "examples/pipelines/ham_or_spam_multi_algo.py", "automl")
+        return_code = launch(env, "examples/pipelines/ham_or_spam_multi_algo.py", "automl")
+        self.assertTrue(return_code == 0, "Process ended in a wrong way. It ended with return code "+str(return_code))
 
     def test_import_pysparkling_standalone_app(self):
         env = IntegTestEnv()
-
-        env.set_spark_master("local")
-        # Configure YARN environment
-        env.conf("spark.yarn.max.executor.failures", 1) # In fail of executor, fail the test
-        env.conf("spark.executor.instances", 3)
-        env.conf("spark.executor.memory", "2g")
+        env.set_spark_master("local-cluster[3,1,2048]")
         env.conf("spark.ext.h2o.port.base", 63331)
-        env.conf("spark.driver.memory", "2g")
 
-        launch(env, "py/scripts/tests/pysparkling_ml_import_overrides_spark_test.py")
+        return_code = launch(env, "examples/scripts/tests/pysparkling_ml_import_overrides_spark_test.py")
+        self.assertTrue(return_code == 0, "Process ended in a wrong way. It ended with return code "+str(return_code))
 
 
 if __name__ == '__main__':
