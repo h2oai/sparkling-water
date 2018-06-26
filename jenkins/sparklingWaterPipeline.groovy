d3b834385ecbd448810ec6248af5d87a8e2a638c
Jakub Háva <jakub.hava@gmail.com>
2018-06-26 12:56:55 +0200

    [SW-899] Upgrade Hadoop image to 47 (#787)
    (cherry picked from commit fc6805f1dd17ae081d92d6851b4ce421dc6bf8f3)
    (cherry picked from commit 5281b42b8339fed825967436f4a28c473cefe6c4)
    (cherry picked from commit 0557e083ce7c9d06beead0750f3c5ae21710c1b8)

--- jenkins/sparklingWaterPipeline.groovy
+++ jenkins/sparklingWaterPipeline.groovy
@@ -25,7 +25,6 @@ def call(params, body) {
             withEnv(customEnv) {
                 timeout(time: 180, unit: 'MINUTES') {
                     dir("${env.WORKSPACE}") {
-
                         prepareSparkEnvironment()(config)
                         prepareSparklingWaterEnvironment()(config)
                         buildAndLint()(config)
@@ -70,6 +69,7 @@ def withDocker(config, code) {
       sh "docker pull docker.h2o.ai/${image}"
     }
     docker.image(image).inside("--init --dns 172.16.0.200") {
+        sh "activate_java_8"
         code()
     }
 }
