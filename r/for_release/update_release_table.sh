397bcfbcad5f6789d25cbfd99010c6d95c18d5c3
Jakub Hava <jakub.hava@gmail.com>
2018-08-02 10:27:35 +0200

    [SW-972] Fix Content of RSparkling release table
    (cherry picked from commit af8619261c480e399a8b0378f537adc08a5575ab)

--- r/for_release/update_release_table.sh
+++ r/for_release/update_release_table.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-R -e "source('for_release/utils.R'); write_release_table('build/r_release_table.csv', $1, $2, $3, $4)"
+echo $1 $2 $3 $4
+R -e "source('for_release/utils.R'); update_release_tables($1, $2, $3, $4); write_release_table('build/r_release_table.csv', $1)"
 
 python for_release/csv2rst.py "build/r_release_table.csv" > build/r_release_table.rst
 
