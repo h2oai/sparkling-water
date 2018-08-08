af08f2edbfd7ff30cc8252bf8cb248497aa7a41c
Jakub Háva <jakub.hava@gmail.com>
2018-08-08 09:11:37 +0200

    [SW-979] Verify that we are running on correct Spark for PySparkling at init time (#879)
    (cherry picked from commit 2a3a9923dafcdd2a417ef06fd99e9cc4efa5b458)

--- py/pysparkling/__init__.py
+++ py/pysparkling/__init__.py
@@ -8,6 +8,8 @@ pySparkling - The Sparkling-Water Python Package
 import zipfile
 from codecs import open
 from os import path
+import pyspark
+import warnings
 
 here = path.abspath(path.dirname(__file__))
 
@@ -18,6 +20,22 @@ if '.zip' in here:
 else:
     with open(path.join(here, 'version.txt'), encoding='utf-8') as f:
         __version__ = f.read().strip()
+
+pyspark_version = pyspark.__version__.split(".")
+pysparkling_version = __version__.split(".")
+
+def custom_formatwarning(msg, *args, **kwargs):
+    # ignore everything except the message
+    return str(msg) + '\n'
+
+warnings.formatwarning = custom_formatwarning
+
+
+if not (pyspark_version[0] == pysparkling_version[0] and pyspark_version[1] == pysparkling_version[1]):
+    warnings.warn("""
+    You are using PySparkling of version {pysparkling_version}, but your PySpark is of
+    version {pyspark_version}. Please make sure Spark and PySparkling versions are compatible. """.format(pysparkling_version=__version__, pyspark_version=pyspark.__version__))
+
 
 # set imports from this project which will be available when the module is imported
 from pysparkling.context import H2OContext
