66d5962c705cfd38966949891aaaf500cdfd02ec
Jakub Hava <jakub.hava@gmail.com>
2018-09-19 15:05:29 +0200

    add test for export to s3

--- py/tests/tests_hadoop_smoke.py
+++ py/tests/tests_hadoop_smoke.py
@@ -133,129 +133,20 @@ class HadoopSmokeTestSuite(unittest.TestCase):
                 assert fr[10, 10] == "NA"
                 assert fr[43977, 30] == "YES"
 
-        # def test_export_csv_s3_h2o(self):
-        #         airlines_csv_s3 = "https://s3.amazonaws.com/h2o-airlines-unpacked/allyears2k.csv"
-        #         airlines_csv_s3_df = h2o.import_file(path=airlines_csv_s3)
-        #         airlines_export = "https://s3.amazonaws.com/h2o-airlines-unpacked/allyears2k_export.csv"
-        #         isOK = True
-        #         try:
-        #                 h2o.export_file(frame=airlines_csv_s3_df, path=airlines_export, force=True)
-        #         except:
-        #                 isOK = False
-        #         assert isOK
-        #
-        # def test_csv_h2o_to_sw(self):
-        #         hc = self._hc
-        #         airlines_csv_s3 = "https://s3.amazonaws.com/h2o-airlines-unpacked/allyears2k.csv"
-        #         airlines_csv_s3_df = h2o.import_file(path=airlines_csv_s3)
-        #         airlines_csv_sw_df = hc.as_spark_frame(airlines_csv_s3_df)
-        #         assert airlines_csv_sw_df.head()[0] == 1987
-        #         assert airlines_csv_sw_df.head()[3] == 3
-        #         assertEquals(airlines_csv_s3_df.nrow, airlines_csv_sw_df.count(),"Number of rows should match")
-        #         assertEquals(airlines_csv_s3_df.ncol, len(airlines_csv_sw_df.columns),"Number of columns should match")
-        #         assertEquals(airlines_csv_s3_df.names, airlines_csv_sw_df.columns,"Column names should match")
-        #
-        # def test_csv_sw_to_h2o(self):
-        #         hc = self._hc
-        #         airlines_csv_s3 = "https://s3.amazonaws.com/h2o-airlines-unpacked/allyears2k.csv"
-        #         airlines_csv_s3_df = h2o.import_file(path=airlines_csv_s3)
-        #         airlines_csv_sw_df = hc.as_spark_frame(airlines_csv_s3_df)
-        #         airlines_csv_h2o_df = hc.as_h2o_frame(airlines_csv_sw_df)
-        #         assert airlines_csv_h2o_df[0,0] == 1987
-        #         assert airlines_csv_h2o_df[1,21] == 0
-        #         assertEquals(airlines_csv_h2o_df.nrow, airlines_csv_sw_df.count(),"Number of rows should match")
-        #         assertEquals(airlines_csv_h2o_df.ncol, len(airlines_csv_sw_df.columns),"Number of columns should match")
-        #         assertEquals(airlines_csv_h2o_df.names, airlines_csv_sw_df.columns,"Column names should match")
-        #
-        # def test_parquet_h2o_to_sw(self):
-        #         hc = self._hc
-        #         userdata_parquet_h2o = "hdfs://127.0.0.1/user/jenkins/userdata2.parquet"
-        #         userdata_parquet_h2o_df = h2o.import_file(path=userdata_parquet_h2o,header=1)
-        #         userdata_parquet_sw_df = hc.as_spark_frame(userdata_parquet_h2o_df)
-        #         assert userdata_parquet_sw_df.head()[1] == 1
-        #         assert userdata_parquet_sw_df.head()[3] == "Lewis"
-        #         assertEquals(userdata_parquet_h2o_df.nrow, userdata_parquet_sw_df.count(),"Number of rows should match")
-        #         assertEquals(userdata_parquet_h2o_df.ncol, len(userdata_parquet_sw_df.columns),"Number of columns should match")
-        #         assertEquals(userdata_parquet_h2o_df.names, userdata_parquet_sw_df.columns,"Column names should match")
-        #
-        # def test_parquet_sw_to_h2o(self):
-        #         hc = self._hc
-        #         userdata_parquet_h2o = "hdfs://127.0.0.1/user/jenkins/userdata2.parquet"
-        #         userdata_parquet_h2o_df = h2o.import_file(path=userdata_parquet_h2o,header=1)
-        #         userdata_parquet_sw_df = hc.as_spark_frame(userdata_parquet_h2o_df)
-        #         userdata_parquet_2_h2o_df = hc.as_h2o_frame(userdata_parquet_sw_df)
-        #         assert userdata_parquet_2_h2o_df[0, 1] == 1
-        #         assert userdata_parquet_2_h2o_df[0, 3] == "Lewis"
-        #         assertEquals(userdata_parquet_2_h2o_df.nrow, userdata_parquet_sw_df.count(),"Number of rows should match")
-        #         assertEquals(userdata_parquet_2_h2o_df.ncol, len(userdata_parquet_sw_df.columns),"Number of columns should match")
-        #         assertEquals(userdata_parquet_2_h2o_df.names, userdata_parquet_sw_df.columns,"Column names should match")
-        #
-        # def test_orc_h2o_to_sw(self):
-        #         hc = self._hc
-        #         userdata_orc_h2o = "hdfs://127.0.0.1/user/jenkins/userdata1_orc"
-        #         userdata_orc_h2o_df = h2o.import_file(path=userdata_orc_h2o,header=1)
-        #         userdata_orc_sw_df = hc.as_spark_frame(userdata_orc_h2o_df)
-        #         assert userdata_orc_sw_df.head()[2] == "Amanda"
-        #         assert userdata_orc_sw_df.head()[5] == "Female"
-        #         assertEquals(userdata_orc_h2o_df.nrow, userdata_orc_sw_df.count(),"Number of rows should match")
-        #         assertEquals(userdata_orc_h2o_df.ncol, len(userdata_orc_sw_df.columns),"Number of columns should match")
-        #         assertEquals(userdata_orc_h2o_df.names, userdata_orc_sw_df.columns,"Column names should match")
-        #
-        # def test_orc_sw_to_h2o(self):
-        #         hc = self._hc
-        #         userdata_orc_h2o = "hdfs://127.0.0.1/user/jenkins/userdata1_orc"
-        #         userdata_orc_h2o_df = h2o.import_file(path=userdata_orc_h2o,header=1)
-        #         userdata_orc_sw_df = hc.as_spark_frame(userdata_orc_h2o_df)
-        #         userdata_orc_h2o_df = hc.as_h2o_frame(userdata_orc_sw_df)
-        #         assert userdata_orc_h2o_df[0,2] == "Amanda"
-        #         assert userdata_orc_h2o_df[0,5] == "Female"
-        #         assertEquals(userdata_orc_h2o_df.nrow, userdata_orc_sw_df.count(),"Number of rows should match")
-        #         assertEquals(userdata_orc_h2o_df.ncol, len(userdata_orc_sw_df.columns),"Number of columns should match")
-        #         assertEquals(userdata_orc_h2o_df.names, userdata_orc_sw_df.columns,"Column names should match")
-        #
-        # """
-        # def test_import_csv_wasb(self):
-        #         cars_csv_wasb = "wasb://h2o@aurelienbriand.blob.core.windows.net/cars.csv"
-        #         cars_csv_wasb_df = h2o.import_file(path=cars_csv_wasb)
-        #         assert cars_csv_wasb_df.ncol == 12
-        #         assert cars_csv_wasb_df.nrow == 32
-        #         assert cars_csv_wasb_df[0,0] == "Mazda RX4"
-        #         assert cars_csv_wasb_df[1,0] == "Mazda RX4 Wag"
-        #         assert cars_csv_wasb_df[0,1] == 21
-        #         assert cars_csv_wasb_df[0,11] == 4
-        #         assert cars_csv_wasb_df[10,11] == 4
-        # """
-        # """
-        # def test_export_csv_wasb(cls):
-        #     cars_csv_wasb = "wasb://h2o@aurelienbriand.blob.core.windows.net/cars.csv"
-        #     cars_csv_wasb_df = h2o.import_file(path=cars_csv_wasb)
-        #     cars_csv_wasb_export = "wasb://h2o@aurelienbriand.blob.core.windows.net/cars_export.csv"
-        #     isOK = True
-        #     try:
-        #         h2o.export_file(frame=cars_csv_wasb_df,path=cars_csv_wasb_export,force=True)
-        #     except:
-        #         isOK = False
-        #     assert isOK
-        # """
-        #
-        # def test_upload_csv_zip_hdfs(self):
-        #         userdata_csv_zip_hdfs = "/home/jenkins/prostate.csv.zip"
-        #         userdata_csv_zip_hdfs_df = h2o.upload_file(path=userdata_csv_zip_hdfs,header=1)
-        #         assert userdata_csv_zip_hdfs_df.ncol == 9
-        #         assert userdata_csv_zip_hdfs_df.nrow == 194560
-        #         assert userdata_csv_zip_hdfs_df[0, 0] == 1
-        #         assert userdata_csv_zip_hdfs_df[7, 8] == 7
-        #         assert userdata_csv_zip_hdfs_df[19459, 8] == 9
-        #
-        # def test_import_http_csv_panda(self):
-        #         eyestate_pd_csv = "http://www.stat.berkeley.edu/~ledell/data/eeg_eyestate_splits.csv"
-        #         eyestate_pd_csv_df = pd.read_csv(eyestate_pd_csv)
-        #         eyestate_h2o_csv = h2o.H2OFrame(eyestate_pd_csv_df)
-        #         assert eyestate_h2o_csv[0, 0] == 4329.23
-        #         assert eyestate_h2o_csv[54, 7] == 4620
-        #         assert eyestate_h2o_csv.nrow == 14980
-        #         assert eyestate_h2o_csv.ncol == 16
+        def test_export_csv_s3_h2o(self):
+                fr = h2o.import_file(path="https://s3.amazonaws.com/h2o-airlines-unpacked/allyears2k.csv")
+                export_path = "https://s3.amazonaws.com/h2o-airlines-unpacked/allyears2k_export.csv"
+                failure = False
+                try:
+                        h2o.export_file(frame=fr, path=export_path, force=True)
+                except:
+                        failure = True
+                assert not failure
 
+                imported = h2o.import_file(path=export_path, header=1)
+                assert imported.ncol == fr.ncol
+                assert imported.nrow == fr.nrow
+        
 
 if __name__ == '__main__':
         generic_test_utils.run_tests([HadoopSmokeTestSuite], file_name="py_hadoop_smoke_tests_report")
