04034ad01219229f40d517e13d7a87897fef100f
Jakub Hava <jakub.hava@gmail.com>
2019-02-15 08:56:09 +0100

    Be consistent in naming

--- templates/src/terraform/aws/network/outputs.tf
+++ templates/src/terraform/aws/network/outputs.tf
@@ -2,11 +2,11 @@
  ## Output variables
  ##
 
- output "aws_vpc" {
+ output "aws_vp_id" {
      value = "${aws_vpc.main.id}"
  }
 
- output "aws_subnet" {
+ output "aws_subnet_id" {
      value = "${aws_subnet.main.id}"
  }
 
