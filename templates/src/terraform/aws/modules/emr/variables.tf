b906e2599886f1231224e357c6e28ef2bc51261b
Jakub Háva <jakub.hava@gmail.com>
2019-02-19 07:47:48 +0100

    [SW-1141] Improvement of the terraform EMR template (#1064)
    (cherry picked from commit 87173f710ec19d602279e075a2c5ce57f8f92ab1)

---
+++ templates/src/terraform/aws/modules/emr/variables.tf
@@ -0,0 +1,27 @@
+##
+## Input Variables
+##
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_vpc_id" {}
+variable "aws_subnet_id" {}
+variable "aws_region" {
+  default = "us-east-1"
+}
+variable "aws_emr_version" {
+  default = "emr-5.20.0"
+}
+variable "aws_core_instance_count" {
+  default = "2"
+}
+variable "aws_instance_type" {
+  default = "m3.xlarge"
+}
+variable "sw_major_version" {
+  default = "SUBST_MAJOR_VERSION"
+}
+variable "sw_patch_version" {
+  default = "SUBST_MINOR_VERSION"
+}
+
+
