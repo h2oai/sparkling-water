e725e8d895a11866343fb024002c0cd5ac650a84
Jakub Háva <jakub.hava@gmail.com>
2019-03-12 14:20:41 +0100

    [SW-1145] Start jupyter notebook with Scala & Python Spark in AWS EMR Terraform template (#1091)
    (cherry picked from commit f08e6209f229fd02e3706ebcf48bb09ac4f08218)

--- templates/src/terraform/aws/modules/emr/outputs.tf
+++ templates/src/terraform/aws/modules/emr/outputs.tf
@@ -5,3 +5,6 @@
 output "master_public_dns" {
   value = "${aws_emr_cluster.sparkling-water-cluster.master_public_dns}"
 }
+output "jupyter_url" {
+  value = "https://${aws_emr_cluster.sparkling-water-cluster.master_public_dns}:9443"
+}
