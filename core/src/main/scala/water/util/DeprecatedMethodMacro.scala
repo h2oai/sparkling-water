feb4a2cd21109d55d7255fcbab166f298809fcf8
Marek Novotný <marek.novotny@h2o.ai>
2019-04-14 22:40:21 +0200

    Adding headers and improving scala style

--- core/src/main/scala/water/util/DeprecatedMethodMacro.scala
+++ core/src/main/scala/water/util/DeprecatedMethodMacro.scala
@@ -1,3 +1,20 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package water.util
 
 import scala.annotation.{StaticAnnotation, compileTimeOnly}
@@ -41,7 +58,7 @@ object DeprecatedMethodMacro {
       annottees.map(_.tree).toList match {
         case q"${Modifiers(f, p, a)} def $methodName[..$tpes](...$args): $returnType = { ..$body }" :: Nil => {
 
-          val daMessage = if(replacement.isEmpty) "" else s"Use '$replacement' instead!"
+          val daMessage = if (replacement.isEmpty) "" else s"Use '$replacement' instead!"
           val da = q"""new deprecated($daMessage, "")"""
           val warnMessage = s"The method '$methodName' is deprecated. $daMessage"
 
--- core/src/test/scala/water/api/DeprecatedMethodMacroSuite.scala
+++ core/src/test/scala/water/api/DeprecatedMethodMacroSuite.scala
@@ -1,3 +1,20 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package water.api
 
 import org.junit.runner.RunWith
@@ -8,7 +25,7 @@ import water.util.DeprecatedMethod
 import scala.language.experimental.macros
 
 @RunWith(classOf[JUnitRunner])
-class DeprecateMacroSuite extends FunSuite {
+class DeprecatedMethodMacroSuite extends FunSuite {
 
   class VariousAnnotationUsages {
     @DeprecatedMethod(replacement = "replacing method")
@@ -37,7 +54,7 @@ class DeprecateMacroSuite extends FunSuite {
     def method1: String = "method1"
 
     @DeprecatedMethod(replacement = "replacing method")
-    def method2(parameter:String): Unit = println(parameter)
+    def method2(parameter: String): Unit = println(parameter)
 
     @DeprecatedMethod(replacement = "replacing method")
     def method3[T](parameter: T): T = parameter
