d13f30c52326f6d175c3e2562e241403df5aa7d5
Jakub Hava <jakub.hava@gmail.com>
2019-04-15 16:36:53 +0200

    Missing import

--- py/tests/tests_unit_conversions.py
+++ py/tests/tests_unit_conversions.py
@@ -20,6 +20,7 @@ Unit tests for PySparkling Data Conversions;
 """
 import sys
 import os
+
 sys.path.insert(0, sys.argv[1])
 os.environ['PYSPARK_PYTHON'] = sys.executable
 import unittest
@@ -36,6 +37,8 @@ import os
 from pyspark.mllib.linalg import *
 from pyspark.sql.types import *
 from pysparkling.ml.algos import H2OGLM, H2OGridSearch, H2OGBM
+from pyspark.ml import Pipeline, PipelineModel
+
 
 # Test of transformations from dataframe/rdd to h2o frame and from h2o frame back to dataframe
 class FrameTransformationsTest(unittest.TestCase):
@@ -46,16 +49,15 @@ class FrameTransformationsTest(unittest.TestCase):
         cls._spark = SparkSession.builder.config(conf=cls._conf).getOrCreate()
         cls._hc = H2OContext.getOrCreate(cls._spark, H2OConf(cls._spark).set_num_of_external_h2o_nodes(1))
 
-
     # test transformation from dataframe to h2o frame
     def test_df_to_h2o_frame(self):
         hc = self._hc
-        df = self._spark.sparkContext.parallelize([(num,"text") for num in range(0,100)]).toDF()
+        df = self._spark.sparkContext.parallelize([(num, "text") for num in range(0, 100)]).toDF()
         h2o_frame = hc.as_h2o_frame(df)
-        self.assertEquals(h2o_frame.nrow, df.count(),"Number of rows should match")
-        self.assertEquals(h2o_frame.ncol, len(df.columns),"Number of columns should match")
-        self.assertEquals(h2o_frame.names, df.columns,"Column names should match")
-        self.assertEquals(df.first()._2, "text","Value should match")
+        self.assertEquals(h2o_frame.nrow, df.count(), "Number of rows should match")
+        self.assertEquals(h2o_frame.ncol, len(df.columns), "Number of columns should match")
+        self.assertEquals(h2o_frame.names, df.columns, "Column names should match")
+        self.assertEquals(df.first()._2, "text", "Value should match")
 
     # test transformation from wide dataframe to h2o frame and edit it
     def test_wide_df_to_h2o_frame_and_edit(self):
@@ -72,12 +74,12 @@ class FrameTransformationsTest(unittest.TestCase):
         h2o_frame.refresh()
         self.assertEquals(h2o_frame.dim[1], n_col, "Number of cols after replace should match")
 
-     # test transformation from RDD consisting of python integers to h2o frame
+    # test transformation from RDD consisting of python integers to h2o frame
     def test_rdd_int_h2o_frame(self):
         hc = self._hc
-        rdd = self._spark.sparkContext.parallelize([num for num in range(0,100)])
+        rdd = self._spark.sparkContext.parallelize([num for num in range(0, 100)])
         h2o_frame = hc.as_h2o_frame(rdd)
-        self.assertEquals(h2o_frame[0,0], 0, "Value should match")
+        self.assertEquals(h2o_frame[0, 0], 0, "Value should match")
         unit_test_utils.asert_h2o_frame(self, h2o_frame, rdd)
 
     # test transformation from RDD consisting of python booleans to h2o frame
@@ -85,59 +87,59 @@ class FrameTransformationsTest(unittest.TestCase):
         hc = self._hc
         rdd = self._spark.sparkContext.parallelize([True, False, True, True, False])
         h2o_frame = hc.as_h2o_frame(rdd)
-        self.assertEquals(h2o_frame[0,0],1,"Value should match")
-        self.assertEquals(h2o_frame[1,0],0,"Value should match")
+        self.assertEquals(h2o_frame[0, 0], 1, "Value should match")
+        self.assertEquals(h2o_frame[1, 0], 0, "Value should match")
         unit_test_utils.asert_h2o_frame(self, h2o_frame, rdd)
 
     # test transformation from RDD consisting of python strings to h2o frame
     def test_rdd_str_h2o_frame(self):
         hc = self._hc
-        rdd = self._spark.sparkContext.parallelize(["a","b","c"])
+        rdd = self._spark.sparkContext.parallelize(["a", "b", "c"])
         h2o_frame = hc.as_h2o_frame(rdd)
-        self.assertEquals(h2o_frame[0,0],"a","Value should match")
-        self.assertEquals(h2o_frame[2,0],"c","Value should match")
+        self.assertEquals(h2o_frame[0, 0], "a", "Value should match")
+        self.assertEquals(h2o_frame[2, 0], "c", "Value should match")
         unit_test_utils.asert_h2o_frame(self, h2o_frame, rdd)
 
     # test transformation from RDD consisting of python floats to h2o frame
     def test_rdd_float_h2o_frame(self):
         hc = self._hc
-        rdd = self._spark.sparkContext.parallelize([0.5,1.3333333333,178])
+        rdd = self._spark.sparkContext.parallelize([0.5, 1.3333333333, 178])
         h2o_frame = hc.as_h2o_frame(rdd)
-        self.assertEquals(h2o_frame[0,0],0.5,"Value should match")
-        self.assertEquals(h2o_frame[1,0],1.3333333333,"Value should match")
+        self.assertEquals(h2o_frame[0, 0], 0.5, "Value should match")
+        self.assertEquals(h2o_frame[1, 0], 1.3333333333, "Value should match")
         unit_test_utils.asert_h2o_frame(self, h2o_frame, rdd)
 
     # test transformation from RDD consisting of python doubles to h2o frame
     def test_rdd_double_h2o_frame(self):
         hc = self._hc
-        rdd = self._spark.sparkContext.parallelize([0.5,1.3333333333,178])
+        rdd = self._spark.sparkContext.parallelize([0.5, 1.3333333333, 178])
         h2o_frame = hc.as_h2o_frame(rdd)
-        self.assertEquals(h2o_frame[0,0],0.5,"Value should match")
-        self.assertEquals(h2o_frame[1,0],1.3333333333,"Value should match")
+        self.assertEquals(h2o_frame[0, 0], 0.5, "Value should match")
+        self.assertEquals(h2o_frame[1, 0], 1.3333333333, "Value should match")
         unit_test_utils.asert_h2o_frame(self, h2o_frame, rdd)
 
     # test transformation from RDD consisting of python complex types to h2o frame
     def test_rdd_complex_h2o_frame_1(self):
         hc = self._hc
-        rdd = self._spark.sparkContext.parallelize([("a",1,0.5),("b",2,1.5)])
+        rdd = self._spark.sparkContext.parallelize([("a", 1, 0.5), ("b", 2, 1.5)])
         h2o_frame = hc.as_h2o_frame(rdd)
-        self.assertEquals(h2o_frame[0,0],"a","Value should match")
-        self.assertEquals(h2o_frame[1,0],"b","Value should match")
-        self.assertEquals(h2o_frame[1,2],1.5,"Value should match")
-        self.assertEquals(h2o_frame.nrow, rdd.count(),"Number of rows should match")
-        self.assertEquals(h2o_frame.ncol, 3,"Number of columns should match")
-        self.assertEquals(h2o_frame.names, ["_1","_2","_3"],"Column names should match")
+        self.assertEquals(h2o_frame[0, 0], "a", "Value should match")
+        self.assertEquals(h2o_frame[1, 0], "b", "Value should match")
+        self.assertEquals(h2o_frame[1, 2], 1.5, "Value should match")
+        self.assertEquals(h2o_frame.nrow, rdd.count(), "Number of rows should match")
+        self.assertEquals(h2o_frame.ncol, 3, "Number of columns should match")
+        self.assertEquals(h2o_frame.names, ["_1", "_2", "_3"], "Column names should match")
 
     # test transformation from RDD consisting of python long to h2o frame
     def test_rdd_long_h2o_frame(self):
         hc = self._hc
         min = hc._jvm.Integer.MIN_VALUE - 1
         max = hc._jvm.Integer.MAX_VALUE + 1
-        rdd = self._spark.sparkContext.parallelize([1, min , max])
+        rdd = self._spark.sparkContext.parallelize([1, min, max])
         h2o_frame = hc.as_h2o_frame(rdd)
-        self.assertEquals(h2o_frame[0,0],1,"Value should match")
-        self.assertEquals(h2o_frame[1,0],min,"Value should match")
-        self.assertEquals(h2o_frame[2,0],max,"Value should match")
+        self.assertEquals(h2o_frame[0, 0], 1, "Value should match")
+        self.assertEquals(h2o_frame[1, 0], min, "Value should match")
+        self.assertEquals(h2o_frame[2, 0], max, "Value should match")
         unit_test_utils.asert_h2o_frame(self, h2o_frame, rdd)
 
     # test transformation of numeric too big to be handled by standard java types
@@ -145,7 +147,7 @@ class FrameTransformationsTest(unittest.TestCase):
         hc = self._hc
         min = hc._jvm.Long.MIN_VALUE - 1
         max = hc._jvm.Long.MAX_VALUE + 1
-        rdd = self._spark.sparkContext.parallelize([1, min , max])
+        rdd = self._spark.sparkContext.parallelize([1, min, max])
         with self.assertRaises(ValueError):
             h2o_frame = hc.as_h2o_frame(rdd)
 
@@ -157,7 +159,7 @@ class FrameTransformationsTest(unittest.TestCase):
         df = hc.as_spark_frame(h2o_frame)
         self.assertEquals(df.count(), h2o_frame.nrow, "Number of rows should match")
         self.assertEquals(len(df.columns), h2o_frame.ncol, "Number of columns should match")
-        self.assertEquals(df.columns,h2o_frame.names, "Column names should match")
+        self.assertEquals(df.columns, h2o_frame.names, "Column names should match")
 
     def test_h2o_frame_2_data_frame_second_conversion(self):
         hc = self._hc
@@ -172,7 +174,7 @@ class FrameTransformationsTest(unittest.TestCase):
     # on h2o context
     def test_h2o_frame_2_data_frame_2(self):
         hc = self._hc
-        rdd = self._spark.sparkContext.parallelize(["a","b","c"])
+        rdd = self._spark.sparkContext.parallelize(["a", "b", "c"])
         h2o_frame = hc.as_h2o_frame(rdd)
         df = hc.as_spark_frame(h2o_frame)
         self.assertEquals(df.count(), h2o_frame.nrow, "Number of rows should match")
@@ -210,12 +212,14 @@ class FrameTransformationsTest(unittest.TestCase):
         t0 = time.time()
         self._hc.as_h2o_frame(df)
         total = time.time() - t0
-        self.assertTrue(total < 20) # The conversion should not take longer then 20 seconds
+        self.assertTrue(total < 20)  # The conversion should not take longer then 20 seconds
 
     def test_load_mojo_gbm(self):
         from pysparkling.ml import H2OMOJOModel, H2OGBM
-        mojo = H2OMOJOModel.create_from_mojo("file://" + os.path.abspath("../ml/src/test/resources/binom_model_prostate.mojo"))
-        prostate_frame = self._hc.as_spark_frame(h2o.upload_file(unit_test_utils.locate("smalldata/prostate/prostate.csv")))
+        mojo = H2OMOJOModel.create_from_mojo(
+            "file://" + os.path.abspath("../ml/src/test/resources/binom_model_prostate.mojo"))
+        prostate_frame = self._hc.as_spark_frame(
+            h2o.upload_file(unit_test_utils.locate("smalldata/prostate/prostate.csv")))
 
         gbm = H2OGBM(ntrees=2, seed=42, distribution="bernoulli", predictionCol="capsule")
 
@@ -230,8 +234,10 @@ class FrameTransformationsTest(unittest.TestCase):
 
     def test_load_mojo_deeplearning(self):
         from pysparkling.ml import H2OMOJOModel, H2ODeepLearning
-        mojo = H2OMOJOModel.create_from_mojo("file://" + os.path.abspath("../ml/src/test/resources/deep_learning_prostate.mojo"))
-        prostate_frame = self._hc.as_spark_frame(h2o.upload_file(unit_test_utils.locate("smalldata/prostate/prostate.csv")))
+        mojo = H2OMOJOModel.create_from_mojo(
+            "file://" + os.path.abspath("../ml/src/test/resources/deep_learning_prostate.mojo"))
+        prostate_frame = self._hc.as_spark_frame(
+            h2o.upload_file(unit_test_utils.locate("smalldata/prostate/prostate.csv")))
 
         dl = H2ODeepLearning(seed=42, reproducible=True, predictionCol="CAPSULE")
 
@@ -245,9 +251,8 @@ class FrameTransformationsTest(unittest.TestCase):
             self.assertEquals(pred_mojo[i], pred_model[i])
 
     def test_simple_parquet_import(self):
-        df = self._spark.sparkContext.parallelize([(num, "text") for num in range(0,100)]).toDF().coalesce(1)
+        df = self._spark.sparkContext.parallelize([(num, "text") for num in range(0, 100)]).toDF().coalesce(1)
         df.write.mode('overwrite').parquet("file://" + os.path.abspath("build/tests_tmp/test.parquet"))
-
 
         parquet_file = None
         for file in os.listdir(os.path.abspath("build/tests_tmp/test.parquet")):
@@ -289,13 +294,14 @@ class FrameTransformationsTest(unittest.TestCase):
         self.assertEquals(fr.ncols, 0)
 
     def test_import_gcs(self):
-        fr = h2o.import_file("gs://gcp-public-data-nexrad-l2/2018/01/01/KABR/NWS_NEXRAD_NXL2DPBL_KABR_20180101050000_20180101055959.tar")
+        fr = h2o.import_file(
+            "gs://gcp-public-data-nexrad-l2/2018/01/01/KABR/NWS_NEXRAD_NXL2DPBL_KABR_20180101050000_20180101055959.tar")
 
     def test_glm_in_spark_pipeline(self):
         prostate_frame = self._spark.read.csv("file://" + unit_test_utils.locate("smalldata/prostate/prostate.csv"),
                                               header=True, inferSchema=True)
 
-        algo = H2OGLM(featuresCols=["CAPSULE", "RACE", "DPROS", "DCAPS", "PSA" , "VOL", "GLEASON"],
+        algo = H2OGLM(featuresCols=["CAPSULE", "RACE", "DPROS", "DCAPS", "PSA", "VOL", "GLEASON"],
                       predictionCol="AGE",
                       seed=1,
                       ratio=0.8)
@@ -314,7 +320,7 @@ class FrameTransformationsTest(unittest.TestCase):
         prostate_frame = self._spark.read.csv("file://" + unit_test_utils.locate("smalldata/prostate/prostate.csv"),
                                               header=True, inferSchema=True)
 
-        algo = H2OGridSearch(predictionCol="AGE", hyperParameters={"_seed": [1,2,3]}, ratio=0.8, algo=H2OGBM(),
+        algo = H2OGridSearch(predictionCol="AGE", hyperParameters={"_seed": [1, 2, 3]}, ratio=0.8, algo=H2OGBM(),
                              strategy="RandomDiscrete", maxModels=3, maxRuntimeSecs=60)
 
         pipeline = Pipeline(stages=[algo])
@@ -326,6 +332,7 @@ class FrameTransformationsTest(unittest.TestCase):
         loaded_model = PipelineModel.load("file://" + os.path.abspath("build/grid_gbm_pipeline_model"))
 
         loaded_model.transform(prostate_frame).count()
+
 
 if __name__ == '__main__':
     generic_test_utils.run_tests([FrameTransformationsTest], file_name="py_unit_tests_conversions_report")
