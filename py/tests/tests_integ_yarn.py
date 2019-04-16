e88a3530a726683dd9ba6c567de3622fc13f65b6
Jakub Háva <jakub.hava@gmail.com>
2019-04-16 07:37:36 +0200

    [SW-1204] Fix formating in python tests (#1133)
    (cherry picked from commit 77b9247a1e132b3633a6489583bab5f24bbb3059)
    (cherry picked from commit 9876404d657ca5bdf3f6958f5cf157d68a952783)

--- py/tests/tests_integ_yarn.py
+++ py/tests/tests_integ_yarn.py
@@ -30,30 +30,29 @@ class YarnIntegTestSuite(unittest.TestCase):
 
         env.set_spark_master("local[*]")
         # Configure YARN environment
-        env.conf("spark.yarn.max.executor.failures", 1) # In fail of executor, fail the test
+        env.conf("spark.yarn.max.executor.failures", 1)  # In fail of executor, fail the test
         env.conf("spark.executor.instances", 1)
         env.conf("spark.executor.memory", "2g")
         env.conf("spark.ext.h2o.port.base", 63331)
         env.conf("spark.driver.memory", "2g")
 
         return_code = launch(env, "examples/scripts/tests/xgboost_test_medium.py")
-        self.assertTrue(return_code == 0, "Process ended in a wrong way. It ended with return code "+str(return_code))
+        self.assertTrue(return_code == 0, "Process ended in a wrong way. It ended with return code " + str(return_code))
 
     def test_chicago_crime(self):
         env = IntegTestEnv()
 
         env.set_spark_master("local[*]")
         # Configure YARN environment
-        env.conf("spark.yarn.max.executor.failures", 1) # In fail of executor, fail the test
+        env.conf("spark.yarn.max.executor.failures", 1)  # In fail of executor, fail the test
         env.conf("spark.executor.instances", 1)
         env.conf("spark.executor.memory", "2g")
         env.conf("spark.ext.h2o.port.base", 63331)
         env.conf("spark.driver.memory", "2g")
 
         return_code = launch(env, "examples/scripts/ChicagoCrimeDemo.py")
-        self.assertTrue(return_code == 0, "Process ended in a wrong way. It ended with return code "+str(return_code))
-
+        self.assertTrue(return_code == 0, "Process ended in a wrong way. It ended with return code " + str(return_code))
 
 
 if __name__ == '__main__':
-        generic_test_utils.run_tests([YarnIntegTestSuite], file_name="py_integ_yarn_tests_report")
+    generic_test_utils.run_tests([YarnIntegTestSuite], file_name="py_integ_yarn_tests_report")
