6fb0b57ef692ae1ea17322304309d05aa1605e3b
Jakub Hava <jakub.hava@gmail.com>
2019-04-29 22:45:33 +0200

    [SW-1255] Unify launcher scripts

--- bin/run-sparkling2.cmd
+++ bin/run-sparkling2.cmd
@@ -35,7 +35,7 @@ call %SPARK_HOME%/bin/spark-submit2.cmd ^
  --master %MASTER% ^
  --driver-memory %DRIVER_MEMORY% ^
  --driver-java-options "%H2O_SYS_OPS%" ^
- --conf spark.driver.extraJavaOptions="-XX:MaxPermSize=384m" ^
+ --conf spark.driver.extraJavaOptions="%EXTRA_JVM_ARGS%" ^
  %VERBOSE% ^
  %FAT_JAR_FILE% ^
  %*
