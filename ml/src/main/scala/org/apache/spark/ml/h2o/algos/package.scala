9a5cc1f937765396b9adcd86a68f7df829ca2690
Jakub Hava <jakub.hava@gmail.com>
2019-05-01 20:11:27 +0200

    fix deprecation message

--- ml/src/main/scala/org/apache/spark/ml/h2o/algos/package.scala
+++ ml/src/main/scala/org/apache/spark/ml/h2o/algos/package.scala
@@ -24,42 +24,48 @@ package object algos extends Logging{
 
   implicit class H2OGBMWithImplicits(algo: H2OGBM) {
     def apply(h2oContext: H2OContext, sqlContext: SQLContext): H2OGBM = {
-      logWarning("The constructor 'new H2OGBM()(implicit h2oContext: H2OContext, sqlContext: SQLContext)'. Use 'new H2OGBM()'!")
+      logWarning("The constructor 'new H2OGBM()(implicit h2oContext: H2OContext, sqlContext: SQLContext)'" +
+        " is deprecated. Use 'new H2OGBM()'!")
       algo
     }
   }
 
   implicit class H2OGLMWithImplicits(algo: H2OGLM) {
     def apply(h2oContext: H2OContext, sqlContext: SQLContext): H2OGLM = {
-      logWarning("The constructor 'new H2OGLM()(implicit h2oContext: H2OContext, sqlContext: SQLContext)'. Use 'new H2OGLM()'!")
+      logWarning("The constructor 'new H2OGLM()(implicit h2oContext: H2OContext, sqlContext: SQLContext)'" +
+        " is deprecated. Use 'new H2OGLM()'!")
       algo
     }
   }
 
   implicit class H2ODeepLearningWithImplicits(algo: H2ODeepLearning) {
     def apply(h2oContext: H2OContext, sqlContext: SQLContext): H2ODeepLearning = {
-      logWarning("The constructor 'new H2ODeepLearning()(implicit h2oContext: H2OContext, sqlContext: SQLContext)'. Use 'new H2ODeepLearning()'!")
+      logWarning("The constructor 'new H2ODeepLearning()(implicit h2oContext: H2OContext, sqlContext: SQLContext)'" +
+        " is deprecated. Use 'new H2ODeepLearning()'!")
       algo
     }
   }
 
   implicit class H2OXGboostWithImplicits(algo: H2OXGBoost) {
     def apply(h2oContext: H2OContext, sqlContext: SQLContext): H2OXGBoost = {
-      logWarning("The constructor 'new H2OXGBoost()(implicit h2oContext: H2OContext, sqlContext: SQLContext)'. Use 'new H2OXGBoost()'!")
+      logWarning("The constructor 'new H2OXGBoost()(implicit h2oContext: H2OContext, sqlContext: SQLContext)'" +
+        " is deprecated. Use 'new H2OXGBoost()'!")
       algo
     }
   }
 
   implicit class H2OAutoMLWithImplicits(algo: H2OAutoML) {
     def apply(h2oContext: H2OContext, sqlContext: SQLContext): H2OAutoML = {
-      logWarning("The constructor 'new H2OAutoML()(implicit h2oContext: H2OContext, sqlContext: SQLContext)'. Use 'new H2OAutoML()'!")
+      logWarning("The constructor 'new H2OAutoML()(implicit h2oContext: H2OContext, sqlContext: SQLContext)'" +
+        " is deprecated. Use 'new H2OAutoML()'!")
       algo
     }
   }
 
   implicit class H2OGridSearchWithImplicits(algo: H2OGridSearch) {
     def apply(h2oContext: H2OContext, sqlContext: SQLContext): H2OGridSearch = {
-      logWarning("The constructor 'new H2OGridSearch()(implicit h2oContext: H2OContext, sqlContext: SQLContext)'. Use 'new H2OGridSearch()'!")
+      logWarning("The constructor 'new H2OGridSearch()(implicit h2oContext: H2OContext, sqlContext: SQLContext)'" +
+        " is deprecated. Use 'new H2OGridSearch()'!")
       algo
     }
   }
