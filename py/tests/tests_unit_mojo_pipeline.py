f53ade8a1ba57bff6e8df72452298186091514f6
Jakub Háva <jakub.hava@gmail.com>
2019-05-08 17:56:26 +0200

    [SW-1187][Pre-step] Use H2OMojoModel to load mojo & mojo pipelines as well (#1191)
    (cherry picked from commit c8bdd68c2f9044247bc40794679507aa215a4305)

--- py/tests/tests_unit_mojo_pipeline.py
+++ py/tests/tests_unit_mojo_pipeline.py
@@ -72,7 +72,6 @@ class H2OMojoPipelineTest(unittest.TestCase):
         assert udfSelection[4][0] == 66.11327967814829
 
         assert mojo.get_output_names()[0] == "AGE"
-        assert mojo.get_output_types()[0] == "Float64"
 
     # test predictions on H2O Pipeline MOJO
     def test_h2o_mojo_pipeline_predictions_with_named_cols(self):
