9a210fe353c3698e649de9a6de08c3fc9da0c8fb
Jakub Hava <jakub.hava@gmail.com>
2019-05-15 14:32:34 +0200

    [SW-1281] Fix bad representation of predictionCol on H2OMOJOModel

--- ml/src/main/scala/org/apache/spark/ml/h2o/param/H2OMOJOModelParams.scala
+++ ml/src/main/scala/org/apache/spark/ml/h2o/param/H2OMOJOModelParams.scala
@@ -17,48 +17,32 @@
 package org.apache.spark.ml.h2o.param
 
 import org.apache.spark.ml.param._
-import water.util.DeprecatedMethod
+import org.apache.spark.ml.param.shared.HasPredictionCol
 
 /**
   * Parameters which need to be available on the model itself for prediction purposes. This can't be backed
   * byt H2OAlgoParamsHelper as at the time of prediction we might be using mojo and binary parameters are not available.
   */
-trait H2OMOJOModelParams extends DeprecatableParams {
-
-  override protected def renamingMap: Map[String, String] = Map(
-    "predictionCol" -> "labelCol"
-  )
+trait H2OMOJOModelParams extends Params with HasPredictionCol {
 
   //
   // Param definitions
   //
-  private val labelCol: Param[String] = new Param[String](this, "labelCol", "Label column name")
   protected final val featuresCols: StringArrayParam = new StringArrayParam(this, "featuresCols", "Name of feature columns")
-  private val outputCol: Param[String] = new Param[String](this, "outputCol", "Column where predictions are created")
   private val convertUnknownCategoricalLevelsToNa = new BooleanParam(this,
     "convertUnknownCategoricalLevelsToNa",
-    "Convert unknown categorical levels to NA during predictions")
+    "If set to 'true', the model converts unknown categorical levels to NA during making predictions.")
   //
   // Default values
   //
-  setDefault(labelCol -> "label")
   setDefault(featuresCols -> Array.empty[String])
-  setDefault(outputCol -> "prediction_output")
+  setDefault(predictionCol -> "prediction_output")
   setDefault(convertUnknownCategoricalLevelsToNa -> false)
 
   //
   // Getters
   //
-  @DeprecatedMethod
-  def getPredictionsCol(): String = getLabelCol()
-
-  @DeprecatedMethod
-  def getLabelCol(): String = $(labelCol)
-
   def getFeaturesCols(): Array[String] = $(featuresCols)
-
-  @DeprecatedMethod
-  def getOutputCol(): String = $(outputCol)
 
   def getConvertUnknownCategoricalLevelsToNa(): Boolean = $(convertUnknownCategoricalLevelsToNa)
 
@@ -66,17 +50,5 @@ trait H2OMOJOModelParams extends DeprecatableParams {
   //
   // Setters
   //
-  @DeprecatedMethod
-  def setFeaturesCols(cols: Array[String]): this.type = set(featuresCols, cols)
-
-  @DeprecatedMethod
-  def setPredictionCol(value: String): this.type = setLabelCol(value)
-
-  @DeprecatedMethod
-  def setLabelCol(value: String): this.type = set(labelCol, value)
-
-  @DeprecatedMethod
-  def setOutputCol(value: String): this.type = set(outputCol, value)
-
   def setConvertUnknownCategoricalLevelsToNa(value: Boolean): this.type = set(convertUnknownCategoricalLevelsToNa, value)
 }
