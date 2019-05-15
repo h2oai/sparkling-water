b21ac58a4b4a388b4fe611bbad9b0d9c79461c77
Jakub Háva <jakub.hava@gmail.com>
2019-05-15 16:58:25 +0200

    [SW-1281] Fix bad representation of predictionCol on H2OMOJOModel (#1199)
    (cherry picked from commit ae3f9c0297884ab3803d643226a7655845f3d9e3)
    (cherry picked from commit a0b6c6486013a80f7915f2ef6480649f7cde2ca9)

--- ml/src/main/scala/org/apache/spark/ml/h2o/models/H2OMOJOReader.scala
+++ ml/src/main/scala/org/apache/spark/ml/h2o/models/H2OMOJOReader.scala
@@ -18,13 +18,48 @@
 package org.apache.spark.ml.h2o.models
 
 import org.apache.hadoop.fs.Path
+import org.apache.spark.ml.param.Params
+import org.apache.spark.ml.util.DefaultParamsReader.Metadata
 import org.apache.spark.ml.util._
 import org.apache.spark.sql._
+import org.apache.spark.util.Utils
+import org.json4s.jackson.JsonMethods._
+import org.json4s.{DefaultFormats, JObject, JsonAST}
 
 private[models] class H2OMOJOReader[T <: HasMojoData] extends DefaultParamsReader[T] {
 
+  private def getAndSetParams(
+                       instance: Params,
+                       metadata: Metadata,
+                       skipParams: Option[List[String]] = None): Unit = {
+    implicit val format = DefaultFormats
+    metadata.params match {
+      case JObject(pairs) =>
+        pairs.foreach { case (paramName, jsonValue) =>
+          if (skipParams == None || !skipParams.get.contains(paramName)) {
+            val param = instance.getParam(paramName)
+            val value = param.jsonDecode(compact(render(jsonValue)))
+            instance.set(param, value)
+          }
+        }
+      case _ =>
+        throw new IllegalArgumentException(
+          s"Cannot recognize JSON metadata: ${metadata.metadataJson}.")
+    }
+  }
+
   override def load(path: String): T  = {
-    val model = super.load(path)
+    val metadata = DefaultParamsReader.loadMetadata(path, sc)
+    val cls = Utils.classForName(metadata.className)
+    val instance =
+      cls.getConstructor(classOf[String]).newInstance(metadata.uid).asInstanceOf[Params]
+
+    val parsedParams = metadata.params.asInstanceOf[JsonAST.JObject].obj.map(_._1)
+    val allowedParams = instance.params.map(_.name)
+    val filteredParams = parsedParams.diff(allowedParams)
+
+    getAndSetParams(instance, metadata, Some(filteredParams))
+    val model = instance.asInstanceOf[T]
 
     val inputPath = new Path(path, H2OMOJOProps.serializedFileName)
     val fs = inputPath.getFileSystem(SparkSession.builder().getOrCreate().sparkContext.hadoopConfiguration)
--- ml/src/main/scala/org/apache/spark/ml/h2o/param/H2OMOJOModelParams.scala
+++ ml/src/main/scala/org/apache/spark/ml/h2o/param/H2OMOJOModelParams.scala
@@ -17,48 +17,35 @@
 package org.apache.spark.ml.h2o.param
 
 import org.apache.spark.ml.param._
-import water.util.DeprecatedMethod
 
 /**
   * Parameters which need to be available on the model itself for prediction purposes. This can't be backed
   * byt H2OAlgoParamsHelper as at the time of prediction we might be using mojo and binary parameters are not available.
   */
-trait H2OMOJOModelParams extends DeprecatableParams {
-
-  override protected def renamingMap: Map[String, String] = Map(
-    "predictionCol" -> "labelCol"
-  )
+trait H2OMOJOModelParams extends Params {
 
   //
   // Param definitions
   //
-  private val labelCol: Param[String] = new Param[String](this, "labelCol", "Label column name")
+  private val predictionCol: Param[String] = new Param[String](this, "predictionCol", "Prediction column name")
   protected final val featuresCols: StringArrayParam = new StringArrayParam(this, "featuresCols", "Name of feature columns")
-  private val outputCol: Param[String] = new Param[String](this, "outputCol", "Column where predictions are created")
   private val convertUnknownCategoricalLevelsToNa = new BooleanParam(this,
     "convertUnknownCategoricalLevelsToNa",
-    "Convert unknown categorical levels to NA during predictions")
+    "If set to 'true', the model converts unknown categorical levels to NA during making predictions.")
   //
   // Default values
   //
-  setDefault(labelCol -> "label")
   setDefault(featuresCols -> Array.empty[String])
-  setDefault(outputCol -> "prediction_output")
+  setDefault(predictionCol -> "prediction_output")
   setDefault(convertUnknownCategoricalLevelsToNa -> false)
 
   //
   // Getters
   //
-  @DeprecatedMethod
-  def getPredictionsCol(): String = getLabelCol()
 
-  @DeprecatedMethod
-  def getLabelCol(): String = $(labelCol)
+  def getPredictionCol(): String = $(predictionCol)
 
   def getFeaturesCols(): Array[String] = $(featuresCols)
-
-  @DeprecatedMethod
-  def getOutputCol(): String = $(outputCol)
 
   def getConvertUnknownCategoricalLevelsToNa(): Boolean = $(convertUnknownCategoricalLevelsToNa)
 
@@ -66,17 +53,5 @@ trait H2OMOJOModelParams extends DeprecatableParams {
   //
   // Setters
   //
-  @DeprecatedMethod
-  def setFeaturesCols(cols: Array[String]): this.type = set(featuresCols, cols)
-
-  @DeprecatedMethod
-  def setPredictionCol(value: String): this.type = setLabelCol(value)
-
-  @DeprecatedMethod
-  def setLabelCol(value: String): this.type = set(labelCol, value)
-
-  @DeprecatedMethod
-  def setOutputCol(value: String): this.type = set(outputCol, value)
-
   def setConvertUnknownCategoricalLevelsToNa(value: Boolean): this.type = set(convertUnknownCategoricalLevelsToNa, value)
 }
