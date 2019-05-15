f4ace1adccd777271431a1349e66fd4c15dda47b
Jakub Háva <jakub.hava@gmail.com>
2019-05-15 13:47:39 +0200

    [SW-1289] Add predictionCol to mojo pipeline model (#1198)
    (cherry picked from commit 96fab038b2ac90bf01130d526d6a2324311303d6)
    (cherry picked from commit 52077ee62ca637e0f8a8a0f827a4c9499b1fbfa4)

--- ml/src/main/scala/org/apache/spark/ml/h2o/models/H2OMOJOPipelineModel.scala
+++ ml/src/main/scala/org/apache/spark/ml/h2o/models/H2OMOJOPipelineModel.scala
@@ -40,8 +40,6 @@ import scala.util.Random
 
 class H2OMOJOPipelineModel(override val uid: String)
   extends SparkModel[H2OMOJOPipelineModel] with H2OMOJOPipelineModelParams with MLWritable with HasMojoData {
-  
-  val outputCol = "prediction"
 
   case class Mojo2Prediction(preds: List[Double])
 
@@ -130,7 +128,7 @@ class H2OMOJOPipelineModel(override val uid: String)
     val args = relevantColumnNames.map(flattenedDF(_))
 
     // get the altered frame
-    val frameWithPredictions = flattenedDF.select(col("*"), modelUdf(relevantColumnNames)(struct(args: _*)).as(outputCol))
+    val frameWithPredictions = flattenedDF.select(col("*"), modelUdf(relevantColumnNames)(struct(args: _*)).as(getPredictionCol))
 
     val fr = if (getNamedMojoOutputColumns()) {
 
@@ -152,12 +150,12 @@ class H2OMOJOPipelineModel(override val uid: String)
       var frameWithExtractedPredictions: DataFrame = frameWithPredictions
       getOutputNames().indices.foreach { idx =>
         frameWithExtractedPredictions = frameWithExtractedPredictions.withColumn(tempColNames(idx),
-          selectFromArray(idx)(frameWithExtractedPredictions.col(outputCol + ".preds")))
+          selectFromArray(idx)(frameWithExtractedPredictions.col(getPredictionCol + ".preds")))
       }
 
       // Transform the columns at the top level under "output" column
       val nestedPredictionCols = tempColNames.indices.map { idx => tempCols(idx).alias(getOutputNames()(idx)) }
-      val frameWithNestedPredictions = frameWithExtractedPredictions.withColumn(outputCol, struct(nestedPredictionCols: _*))
+      val frameWithNestedPredictions = frameWithExtractedPredictions.withColumn(getPredictionCol, struct(nestedPredictionCols: _*))
 
       // Remove the temporary columns at the top level and return
       val frameWithoutTempCols = frameWithNestedPredictions.drop(tempColNames: _*)
@@ -174,7 +172,7 @@ class H2OMOJOPipelineModel(override val uid: String)
 
   def predictionSchema(): Seq[StructField] = {
     val fields = StructField("original", ArrayType(DoubleType)) :: Nil
-    Seq(StructField(outputCol, StructType(fields), nullable = false))
+    Seq(StructField(getPredictionCol, StructType(fields), nullable = false))
   }
 
   override def transformSchema(schema: StructType): StructType = {
@@ -195,10 +193,10 @@ class H2OMOJOPipelineModel(override val uid: String)
       val func = udf[Double, Double] {
         identity
       }
-      func(col(s"$outputCol.`$column`")).alias(column)
+      func(col(s"$getPredictionCol.`$column`")).alias(column)
     } else {
       val func = selectFromArray(getOutputNames().indexOf(column))
-      func(col(s"$outputCol.preds")).alias(column)
+      func(col(s"$getPredictionCol.preds")).alias(column)
     }
   }
 
--- py/py_sparkling/ml/models.py
+++ py/py_sparkling/ml/models.py
@@ -5,6 +5,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf
 from pyspark.sql.types import DoubleType
 from .util import JavaH2OMLReadable
+from pyspark.ml.param.shared import HasPredictionCol
 import warnings
 
 
@@ -43,6 +44,7 @@ class H2OMOJOModel(JavaModel, JavaMLWritable, JavaH2OMLReadable):
             spark_session._jvm.py_sparkling.ml.models.H2OMOJOModel.createFromMojo(path_to_mojo))
 
     def predict(self, dataframe):
+        warnings.warn("The method 'predict' is deprecated. Use 'transform' instead!")
         return self.transform(dataframe)
 
     def getConvertUnknownCategoricalLevelsToNa(self):
@@ -53,7 +55,7 @@ class H2OMOJOModel(JavaModel, JavaMLWritable, JavaH2OMLReadable):
         return self
 
 
-class H2OMOJOPipelineModel(JavaModel, JavaMLWritable, JavaH2OMLReadable):
+class H2OMOJOPipelineModel(JavaModel, JavaMLWritable, JavaH2OMLReadable, HasPredictionCol):
 
     @staticmethod
     def create_from_mojo(path_to_mojo):
@@ -64,6 +66,7 @@ class H2OMOJOPipelineModel(JavaModel, JavaMLWritable, JavaH2OMLReadable):
             spark_session._jvm.py_sparkling.ml.models.H2OMOJOPipelineModel.createFromMojo(path_to_mojo))
 
     def predict(self, dataframe):
+        warnings.warn("The method 'predict' is deprecated. Use 'transform' instead!")
         return self.transform(dataframe)
 
     def get_input_names(self):
@@ -104,8 +107,8 @@ class H2OMOJOPipelineModel(JavaModel, JavaMLWritable, JavaH2OMLReadable):
 
         if self.getNamedMojoOutputColumns():
             func = udf(lambda d: d, DoubleType())
-            return func("prediction." + column).alias(column)
+            return func(self.getPredictionCol() + "." + column).alias(column)
         else:
             idx = list(self._java_obj.getOutputNames()).index(column)
             func = udf(lambda arr: arr[idx], DoubleType())
-            return func("prediction.preds").alias(column)
+            return func(self.getPredictionCol() + ".preds").alias(column)
