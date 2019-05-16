0b6db640fdda18cdccbdda416e3106deb246c592
Jakub Háva <jakub.hava@gmail.com>
2019-05-16 12:21:20 +0200

    [SW-1283] Correctly return mojo model in pysparkling after fit (#1202)
    (cherry picked from commit 1fd78d8b24e3c8143b55f4240d9c3b815967953a)

--- py/pysparkling/ml/models.py
+++ py/pysparkling/ml/models.py
@@ -1,30 +1,9 @@
 import py_sparkling.ml.models
 
 
-class H2OGBMModel(py_sparkling.ml.models.H2OGBMModel):
-    pass
-
-
-class H2ODeepLearningModel(py_sparkling.ml.models.H2ODeepLearningModel):
-    pass
-
-
-class H2OAutoMLModel(py_sparkling.ml.models.H2OAutoMLModel):
-    pass
-
-
-class H2OXGBoostModel(py_sparkling.ml.models.H2OXGBoostModel):
-    pass
-
-class H2OGLMModel(py_sparkling.ml.models.H2OGLMModel):
-    pass
-
-
 class H2OMOJOModel(py_sparkling.ml.models.H2OMOJOModel):
     pass
 
-class H2OMOJOPipelineModel(py_sparkling.ml.models.H2OMOJOPipelineModel):
-    pass
 
-class H2OGridSearch(py_sparkling.ml.models.H2OGridSearchModel):
+class H2OMOJOPipelineModel(py_sparkling.ml.models.H2OMOJOPipelineModel):
     pass
