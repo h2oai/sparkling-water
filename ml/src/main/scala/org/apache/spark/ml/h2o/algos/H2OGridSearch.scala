2bafc92ee802dcf1f624d412a3e99155f675b1ac
Jakub Hava <jakub.hava@gmail.com>
2019-05-30 15:33:53 +0200

    remove

--- ml/src/main/scala/org/apache/spark/ml/h2o/algos/H2OGridSearch.scala
+++ ml/src/main/scala/org/apache/spark/ml/h2o/algos/H2OGridSearch.scala
@@ -50,8 +50,7 @@ class H2OGridSearch(override val uid: String) extends Estimator[H2OMOJOModel]
   with H2OAlgorithmCommons with DefaultParamsWritable with H2OGridSearchParams {
 
   // Override default values
-  logWarning("Default value of 'convertUnknownCategoricalLevelsToNa' parameter on H2OGridSearch will be changed to 'false' from 'true' in the next" +
-    " major release.")
+  logWarning("Default value of 'convertUnknownCategoricalLevelsToNa' parameter will be changed to 'false' in the next major release.")
   setDefault(convertUnknownCategoricalLevelsToNa, true)
 
   private lazy val hc = H2OContext.getOrCreate(SparkSession.builder().getOrCreate())
