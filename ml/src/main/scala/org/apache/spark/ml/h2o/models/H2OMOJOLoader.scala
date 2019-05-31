279e31d5b3f4feed1b102aada91efe0be524e216
Marek Novotný <marek.novotny@h2o.ai>
2019-05-31 10:41:00 +0200

    [SW-1305] Move ability to change behavior of MOJO models to MOJOLoader (#1226)
    (cherry picked from commit 192ff5fc56f247460ba299d634a28d6e88032806)

--- ml/src/main/scala/org/apache/spark/ml/h2o/models/H2OMOJOLoader.scala
+++ ml/src/main/scala/org/apache/spark/ml/h2o/models/H2OMOJOLoader.scala
@@ -25,18 +25,24 @@ import org.apache.spark.sql._
 
 trait H2OMOJOLoader[T] {
 
-  def createFromMojo(path: String): T = {
+  def createFromMojo(path: String): T = createFromMojo(path, H2OMOJOSettings.default)
+
+  def createFromMojo(path: String, settings: H2OMOJOSettings): T = {
     val inputPath = new Path(path)
     val fs = inputPath.getFileSystem(SparkSession.builder().getOrCreate().sparkContext.hadoopConfiguration)
     val qualifiedInputPath = inputPath.makeQualified(fs.getUri, fs.getWorkingDirectory)
     val is = fs.open(qualifiedInputPath)
 
-    createFromMojo(is, Identifiable.randomUID(inputPath.getName))
+    createFromMojo(is, Identifiable.randomUID(inputPath.getName), settings)
   }
 
-  def createFromMojo(is: InputStream, uid: String): T = {
-    createFromMojo(Stream.continually(is.read).takeWhile(_ != -1).map(_.toByte).toArray, uid)
+  def createFromMojo(is: InputStream, uid: String): T = createFromMojo(is, uid, H2OMOJOSettings.default)
+
+  def createFromMojo(is: InputStream, uid: String, settings: H2OMOJOSettings): T = {
+    createFromMojo(Stream.continually(is.read).takeWhile(_ != -1).map(_.toByte).toArray, uid, settings)
   }
 
-  def createFromMojo(mojoData: Array[Byte], uid: String): T
+  def createFromMojo(mojoData: Array[Byte], uid: String): T = createFromMojo(mojoData, uid, H2OMOJOSettings.default)
+
+  def createFromMojo(mojoData: Array[Byte], uid: String, settings: H2OMOJOSettings): T
 }
