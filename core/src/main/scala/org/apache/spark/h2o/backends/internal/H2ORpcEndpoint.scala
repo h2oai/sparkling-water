9744cc52da8ba519f7c3269496ef03db450cb33a
Jakub Hava <jakub.hava@gmail.com>
2019-06-18 15:52:25 +0200

    consistent naming

--- core/src/main/scala/org/apache/spark/h2o/backends/internal/H2ORpcEndpoint.scala
+++ core/src/main/scala/org/apache/spark/h2o/backends/internal/H2ORpcEndpoint.scala
@@ -46,7 +46,7 @@ class H2ORpcEndpoint(override val rpcEnv: RpcEnv)
         Log.info(s"Adding $h2oNode to ${H2O.SELF}'s flatfile")
         H2O.addNodeToFlatfile(h2oNode)
       }
-      HeartBeatUtils.sendHeartbeatToAll()
+      HeartBeatUtils.sendHeartBeatToAll()
       Log.info(s"Full flatfile: ${H2O.getFlatfile.asScala.mkString(", ")}")
 
     case StopEndpointMsg =>
--- core/src/main/scala/water/HeartBeatUtils.scala
+++ core/src/main/scala/water/HeartBeatUtils.scala
@@ -18,7 +18,7 @@
 package water
 
 object HeartBeatUtils {
-  def sendHeartbeatToAll(): Unit = {
+  def sendHeartBeatToAll(): Unit = {
     val ab = new AutoBuffer()
     H2O.SELF._heartbeat.write(ab)
     water.init.NetworkInit.multicast(ab._bb, UDP.udp.heartbeat._prior)
