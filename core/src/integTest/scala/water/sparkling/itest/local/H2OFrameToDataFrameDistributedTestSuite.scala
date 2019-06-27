3641718ce7fcc1f0fd918e50575eb4cccef7d145
Jakub Hava <jakub.hava@gmail.com>
2019-06-27 09:06:11 +0200

    lower memory limits

--- core/src/integTest/scala/water/sparkling/itest/local/H2OFrameToDataFrameDistributedTestSuite.scala
+++ core/src/integTest/scala/water/sparkling/itest/local/H2OFrameToDataFrameDistributedTestSuite.scala
@@ -30,11 +30,11 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class H2OFrameToDataFrameDistributedTestSuite extends FunSuite with SharedH2OTestContext {
 
-  override def createSparkContext: SparkContext = new SparkContext("local-cluster[3,1,2048]", this.getClass.getName, conf = defaultSparkConf)
+  override def createSparkContext: SparkContext = new SparkContext("local-cluster[2,2,2048]", this.getClass.getName, conf = defaultSparkConf)
 
   test("Convert H2OFrame to DataFrame when H2OFrame was changed in DKV") {
 
-    val rdd = sc.parallelize(1 to 100, 3)
+    val rdd = sc.parallelize(1 to 100, 2)
     val h2oFrame = hc.asH2OFrame(rdd)
     assert(h2oFrame.anyVec().nChunks() == 3)
     val updatedFrame = h2oFrame.add(h2oFrame)
