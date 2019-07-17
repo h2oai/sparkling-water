09b0d31926fd360bb30e40ecef9ea7ce2202ad50
Jakub Háva <jakub.hava@gmail.com>
2019-07-17 14:24:16 +0200

    [SW-1434] Fix issues with getBucket (#1352)
    (cherry picked from commit 1623692f0af7e56571cab5239abe129ddcced4b4)

--- templates/src/terraform/aws/modules/emr/main.tf
+++ templates/src/terraform/aws/modules/emr/main.tf
@@ -53,7 +53,7 @@ sudo python2.7 -m pip install -U scikit-learn
 mkdir -p /home/hadoop/h2o
 cd /home/hadoop/h2o
 
-wget https://s3.amazonaws.com/h2o-release/SUBST_S3_BUCKET_PREFIX/sparkling-water-${var.sw_version}.zip
+wget https://s3.amazonaws.com/h2o-release/sparkling-water/spark-SUBST_SPARK_MAJOR_VERSION/SUBST_S3_PATH${var.sw_version}/sparkling-water-${var.sw_version}.zip
 
 unzip -o sparkling-water-${var.sw_version}.zip 1> /dev/null & wait
 
