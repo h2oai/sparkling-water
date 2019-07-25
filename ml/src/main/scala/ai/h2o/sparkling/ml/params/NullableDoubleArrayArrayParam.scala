99f37e6fa01ee8198eac138a7430df1448407974
Jakub Hava <jakub.hava@gmail.com>
2019-07-25 16:45:58 +0200

    [SW-1464] Fix nullableArrayArray param for pyspakrling
    (cherry picked from commit 527795e6ab17658623c834cb04e128c11d5c79f3)

--- ml/src/main/scala/ai/h2o/sparkling/ml/params/NullableDoubleArrayArrayParam.scala
+++ ml/src/main/scala/ai/h2o/sparkling/ml/params/NullableDoubleArrayArrayParam.scala
@@ -18,9 +18,11 @@
 package ai.h2o.sparkling.ml.params
 
 import org.apache.spark.ml.h2o.param.DoubleParam
-import org.apache.spark.ml.param.{Param, Params}
+import org.apache.spark.ml.param.{Param, ParamPair, Params}
 import org.json4s.JsonAST.{JArray, JNull}
 import org.json4s.jackson.JsonMethods.{compact, parse, render}
+
+import scala.collection.JavaConverters._
 
 class NullableDoubleArrayArrayParam(parent: Params, name: String, doc: String,
                                     isValid: Array[Array[Double]] => Boolean)
@@ -28,6 +30,10 @@ class NullableDoubleArrayArrayParam(parent: Params, name: String, doc: String,
 
   def this(parent: Params, name: String, doc: String) =
     this(parent, name, doc, _ => true)
+
+  /** Creates a param pair with a `java.util.List` of values (for Java and Python). */
+  def w(value: java.util.List[java.util.List[java.lang.Double]]): ParamPair[Array[Array[Double]]] =
+    w(value.asScala.map(_.asScala.map(_.asInstanceOf[Double]).toArray).toArray)
 
   override def jsonEncode(value: Array[Array[Double]]): String = {
     if (value == null) {
