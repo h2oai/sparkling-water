f43b0445e43c3d160842d06d0e2c025131968e95
Jakub Hava <jakub.hava@gmail.com>
2019-07-30 13:34:08 +0200

    [SW-1446][FollowUp] Implement H2OKmeans

--- py/ai/h2o/sparkling/ml/algos/__init__.py
+++ py/ai/h2o/sparkling/ml/algos/__init__.py
@@ -14,4 +14,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from ai.h2o.sparkling.ml.algos.kmeans import H2OKMeans
+from ai.h2o.sparkling.ml.algos.H2OKmeans import H2OKMeans
