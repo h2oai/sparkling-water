31b89c4f9636a06f9adb3a7c1a701b3676d18b81
Jakub Hava <jakub.hava@gmail.com>
2019-08-01 09:28:36 +0200

    [SW-1368] Preparation for mojo scoring package

---
+++ utils/src/main/scala/org/apache/spark/expose/Logging.scala
@@ -0,0 +1,6 @@
+package org.apache.spark.expose
+
+/**
+  * The trait is used just to make visible the logic of [[org.apache.spark.internal.Logging]].
+  */
+trait Logging extends org.apache.spark.internal.Logging
