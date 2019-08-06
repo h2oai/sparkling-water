555679654cff7c1d9757aa5af3bbab1ebadfd06c
Marek Novotný <marek.novotny@h2o.ai>
2019-08-06 16:59:19 -0700

    [SW-1488] Benchmarks: Use persist(StorageLevel.DISK_ONLY) for Materialization of Parsed DataFrames (#1416)
    (cherry picked from commit 6dfd00bee673467d468478f669d164d9928b6309)

--- benchmarks/src/main/scala/ai/h2o/sparkling/benchmarks/DummyDataFrameBenchmark.scala
+++ benchmarks/src/main/scala/ai/h2o/sparkling/benchmarks/DummyDataFrameBenchmark.scala
@@ -19,11 +19,14 @@ package ai.h2o.sparkling.benchmarks
 
 import org.apache.spark.sql.DataFrame
 
-class DataFrameToH2OFrameConversionBenchmark(context: BenchmarkContext) extends BenchmarkBase[DataFrame](context) {
+/**
+  *  The purpose of this benchmark is to measure how much time is spent on deserialization from data frame cache.
+  */
+class DummyDataFrameBenchmark(context: BenchmarkContext) extends BenchmarkBase[DataFrame](context) {
 
   override protected def initialize(): DataFrame = loadDataToDataFrame()
 
-  override protected def body(dataFrame: DataFrame): Unit = context.hc.asH2OFrame(dataFrame)
+  override protected def body(dataFrame: DataFrame): Unit = dataFrame.count()
 
   override protected def cleanUp(dataFrame: DataFrame): Unit = removeFromCache(dataFrame)
 }
