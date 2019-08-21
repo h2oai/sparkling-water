ab62c165ab602fdb96134a33a2a84be3e3a8e902
Jakub Hava <jakub.hava@gmail.com>
2019-08-21 15:58:27 +0200

    [SW-1546] Fix SpreadRDDBuilder not serializable exception

--- utils/src/main/scala/org/apache/spark/expose/Logging.scala
+++ utils/src/main/scala/org/apache/spark/expose/Logging.scala
@@ -19,4 +19,4 @@ package org.apache.spark.expose
 /**
   * The trait is used just to make visible the logic of [[org.apache.spark.internal.Logging]].
   */
-trait Logging extends org.apache.spark.internal.Logging
+trait Logging extends org.apache.spark.internal.Logging with Serializable
