821b36f9d055e25f6160b83a71ab8575e8d83a41
Jakub Hava <jakub.hava@gmail.com>
2019-08-22 15:14:53 +0200

    [SW-1548] Move all pysparkling source files into single src dir
    (cherry picked from commit 83a72358a131558cfe43890f7891a8f76505246b)

---
+++ py/src/ai/h2o/sparkling/ml/algos/H2OAutoML.py
@@ -0,0 +1,72 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from pyspark import keyword_only
+from pyspark.sql.dataframe import DataFrame
+
+from ai.h2o.sparkling import Initializer
+from src.ai.h2o.sparkling.ml import Utils
+from src.ai.h2o.sparkling.ml.algos import H2OAlgoBase
+from src.ai.h2o.sparkling.ml import H2OAutoMLParams
+
+
+class H2OAutoML(H2OAutoMLParams, H2OAlgoBase):
+
+    @keyword_only
+    def __init__(self,
+                 ignoredCols=[],
+                 includeAlgos=["GLM", "DRF", "GBM", "DeepLearning", "StackedEnsemble", "XGBoost"],
+                 excludeAlgos=[],
+                 projectName=None,
+                 maxRuntimeSecs=3600.0,
+                 stoppingRounds=3,
+                 stoppingTolerance=0.001,
+                 stoppingMetric="AUTO",
+                 sortMetric="AUTO",
+                 balanceClasses=False,
+                 classSamplingFactors=None,
+                 maxAfterBalanceSize=5.0,
+                 keepCrossValidationPredictions=True,
+                 keepCrossValidationModels=True,
+                 maxModels=0,
+                 labelCol="label",
+                 foldCol=None,
+                 weightCol=None,
+                 splitRatio=1.0,
+                 seed=-1,
+                 nfolds=5,
+                 allStringColumnsToCategorical=True,
+                 columnsToCategorical=[],
+                 predictionCol="prediction",
+                 detailedPredictionCol="detailed_prediction",
+                 withDetailedPredictionCol=False,
+                 featuresCols=[],
+                 convertUnknownCategoricalLevelsToNa=False,
+                 convertInvalidNumbersToNa=False):
+        Initializer.load_sparkling_jar()
+        super(H2OAutoML, self).__init__()
+        self._java_obj = self._new_java_obj("ai.h2o.sparkling.ml.algos.H2OAutoML", self.uid)
+        self._setDefaultValuesFromJava()
+        kwargs = Utils.getInputKwargs(self)
+        self._set(**kwargs)
+
+    def leaderboard(self):
+        leaderboard_java = self._java_obj.leaderboard()
+        if leaderboard_java.isDefined():
+            return DataFrame(leaderboard_java.get(), self._hc._sql_context)
+        else:
+            return None
---
+++ py/src/ai/h2o/sparkling/ml/algos/H2OXGBoost.py
@@ -0,0 +1,96 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from pyspark import keyword_only
+
+from ai.h2o.sparkling import Initializer
+from src.ai.h2o.sparkling.ml import Utils
+from src.ai.h2o.sparkling.ml.algos import H2OAlgoBase
+from src.ai.h2o.sparkling.ml import H2OXGBoostParams
+
+
+class H2OXGBoost(H2OXGBoostParams, H2OAlgoBase):
+
+    @keyword_only
+    def __init__(self,
+                 quietMode=True,
+                 ntrees=50,
+                 nEstimators=0,
+                 maxDepth=6,
+                 minRows=1.0,
+                 minChildWeight=1.0,
+                 learnRate=0.3,
+                 eta=0.3,
+                 learnRateAnnealing=1.0,
+                 sampleRate=1.0,
+                 subsample=1.0,
+                 colSampleRate=1.0,
+                 colSampleByLevel=1.0,
+                 colSampleRatePerTree=1.0,
+                 colSampleByTree=1.0,
+                 maxAbsLeafnodePred=0.0,
+                 maxDeltaStep=0.0,
+                 scoreTreeInterval=0,
+                 initialScoreInterval=4000,
+                 scoreInterval=4000,
+                 minSplitImprovement=0.0,
+                 gamma=0.0,
+                 nthread=-1,
+                 maxBins=256,
+                 maxLeaves=0,
+                 minSumHessianInLeaf=100.0,
+                 minDataInLeaf=0.0,
+                 treeMethod="auto",
+                 growPolicy="depthwise",
+                 booster="gbtree",
+                 dmatrixType="auto",
+                 regLambda=0.0,
+                 regAlpha=0.0,
+                 sampleType="uniform",
+                 normalizeType="tree",
+                 rateDrop=0.0,
+                 oneDrop=False,
+                 skipDrop=0.0,
+                 gpuId=0,
+                 backend="auto",
+                 modelId=None,
+                 keepCrossValidationPredictions=False,
+                 keepCrossValidationFoldAssignment=False,
+                 parallelizeCrossValidation=True,
+                 distribution="AUTO",
+                 labelCol="label",
+                 foldCol=None,
+                 weightCol=None,
+                 splitRatio=1.0,
+                 seed=-1,
+                 nfolds=0,
+                 allStringColumnsToCategorical=True,
+                 columnsToCategorical=[],
+                 predictionCol="prediction",
+                 detailedPredictionCol="detailed_prediction",
+                 withDetailedPredictionCol=False,
+                 featuresCols=[],
+                 convertUnknownCategoricalLevelsToNa=False,
+                 convertInvalidNumbersToNa=False,
+                 **DeprecatedArgs):
+        Initializer.load_sparkling_jar()
+        super(H2OXGBoost, self).__init__()
+        self._java_obj = self._new_java_obj("ai.h2o.sparkling.ml.algos.H2OXGBoost", self.uid)
+        self._setDefaultValuesFromJava()
+        kwargs = Utils.getInputKwargs(self)
+        Utils.propagateValueFromDeprecatedProperty(kwargs, "colsampleBytree", "colSampleByTree")
+        self._set(**kwargs)
