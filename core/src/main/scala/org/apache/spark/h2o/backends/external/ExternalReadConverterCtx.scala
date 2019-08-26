5deec95776e74ee3535f80fbb3033586f3ccdcb8
Jakub Háva <jakub.hava@gmail.com>
2019-08-26 08:23:20 +0200

    [SW-1540] Remove unnecessary read confirmation timeout (#1452)

--- core/src/main/scala/org/apache/spark/h2o/backends/external/ExternalReadConverterCtx.scala
+++ core/src/main/scala/org/apache/spark/h2o/backends/external/ExternalReadConverterCtx.scala
@@ -20,26 +20,28 @@ package org.apache.spark.h2o.backends.external
 
 import org.apache.spark.h2o.converters.ReadConverterCtx
 import org.apache.spark.h2o.utils.NodeDesc
-import water.{ExternalFrameConfirmationException, ExternalFrameReaderClient, ExternalFrameUtils}
+import water.ExternalFrameReaderClient
+
 /**
   *
-  * @param keyName key name of frame to query data from
+  * @param keyName  key name of frame to query data from
   * @param chunkIdx chunk index
   * @param nodeDesc the h2o node which has data for chunk with the chunkIdx
   */
 class ExternalReadConverterCtx(override val keyName: String, override val chunkIdx: Int,
                                val nodeDesc: NodeDesc, expectedTypes: Array[Byte], selectedColumnIndices: Array[Int],
-                               val readTimeout: Int, val driverTimeStamp: Short)
+                               val driverTimeStamp: Short)
   extends ReadConverterCtx {
   override type DataSource = ExternalFrameReaderClient
 
-  private val socketChannel = ExternalFrameUtils.getConnection(nodeDesc.hostname, nodeDesc.port, driverTimeStamp)
-  val externalFrameReader = new ExternalFrameReaderClient(socketChannel, keyName, chunkIdx, selectedColumnIndices, expectedTypes)
+  private val externalFrameReader = ExternalFrameReaderClient.create(
+    nodeDesc.hostname, nodeDesc.port, driverTimeStamp,
+    keyName, chunkIdx, selectedColumnIndices, expectedTypes)
 
   override def numRows: Int = externalFrameReader.getNumRows
 
   override def returnOption[T](read: DataSource => T)(columnNum: Int): Option[T] = {
-    Option(read(externalFrameReader)).filter( _ => !externalFrameReader.isLastNA)
+    Option(read(externalFrameReader)).filter(_ => !externalFrameReader.isLastNA)
   }
 
   override def returnSimple[T](ifMissing: String => T, read: DataSource => T)(columnNum: Int): T = {
@@ -47,25 +49,26 @@ class ExternalReadConverterCtx(override val keyName: String, override val chunkI
     if (externalFrameReader.isLastNA) ifMissing(s"Row $rowIdx column $columnNum") else value
   }
 
-  override protected def booleanAt(source: ExternalFrameReaderClient): Boolean = source.readBoolean()
-  override protected def byteAt(source: ExternalFrameReaderClient): Byte = source.readByte()
-  override protected def shortAt(source: ExternalFrameReaderClient): Short = source.readShort()
-  override protected def intAt(source: ExternalFrameReaderClient): Int = source.readInt()
+  override protected def booleanAt(source: DataSource): Boolean = source.readBoolean()
+
+  override protected def byteAt(source: DataSource): Byte = source.readByte()
+
+  override protected def shortAt(source: DataSource): Short = source.readShort()
+
+  override protected def intAt(source: DataSource): Int = source.readInt()
+
   override protected def longAt(source: DataSource): Long = source.readLong()
-  override protected def floatAt(source: ExternalFrameReaderClient): Float = source.readFloat()
+
+  override protected def floatAt(source: DataSource): Float = source.readFloat()
+
   override protected def doubleAt(source: DataSource): Double = source.readDouble()
+
   override protected def string(source: DataSource) = source.readString()
 
-  /**
-    * @throws ExternalFrameConfirmationException in case of confirmation failure.
-    *         This has also effect of Spark stopping the current job and
-    *         rescheduling it
-    */
   override def hasNext: Boolean = {
     val isNext = super.hasNext
-    if(!isNext){
-      externalFrameReader.waitUntilAllReceived(readTimeout)
-      socketChannel.close()
+    if (!isNext) {
+      externalFrameReader.close()
     }
     isNext
   }
