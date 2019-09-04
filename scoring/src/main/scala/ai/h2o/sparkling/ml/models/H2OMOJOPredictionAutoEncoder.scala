bec9cdb5c131d13c7b773e50f14972f01168f6ee
Jakub Hava <jakub.hava@gmail.com>
2019-09-04 19:31:51 +0200

    [SW-1598] Deprecate cases in H2OMojoPrediction where the prediction column does not directly contain the predicted value.

--- scoring/src/main/scala/ai/h2o/sparkling/ml/models/H2OMOJOPredictionAutoEncoder.scala
+++ scoring/src/main/scala/ai/h2o/sparkling/ml/models/H2OMOJOPredictionAutoEncoder.scala
@@ -26,6 +26,9 @@ trait H2OMOJOPredictionAutoEncoder {
   self: H2OMOJOModel =>
 
   def getAutoEncoderPredictionUDF(): UserDefinedFunction = {
+    logWarning("Starting from next major release, the content of 'prediction' column will be generated to " +
+      " 'detailed_prediction' instead. The 'prediction' column will contain directly the original input the" +
+      " way AutoEncoder model sees it (1-hot encoded categorical values) .")
     udf[Base, Row] { r: Row =>
       val pred = easyPredictModelWrapper.predictAutoEncoder(RowConverter.toH2ORowData(r))
       Base(pred.original, pred.reconstructed)
