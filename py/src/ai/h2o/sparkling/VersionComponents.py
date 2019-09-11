c8ed6af560704800ac3d68605eeb4de9d2d6505f
Jakub Háva <jakub.hava@gmail.com>
2019-09-11 11:13:40 +0200

    [SW-1604] PySparkling fails to parse pyspark version with build number, such as 2.3.1.dev0 (#1515)
    (cherry picked from commit 279a3c2ed7c4ef2f651459f77fe6394d537f5fee)

--- py/src/ai/h2o/sparkling/VersionComponents.py
+++ py/src/ai/h2o/sparkling/VersionComponents.py
@@ -20,13 +20,25 @@ import re
 class VersionComponents(object):
 
     @staticmethod
-    def parseFromVersion(version):
-        match = re.search(r"^((\d+\.\d+).(\d+))(-(\d+))?-(\d+.\d+)$", version)
+    def parseFromSparklingWaterVersion(version):
+        match = re.search(r"^((\d+\.\d+)\.(\d+))(-(\d+))?-(\d+\.\d+)$", version)
         result = VersionComponents()
         result.fullVersion = match.group(0)
         result.sparklingVersion = match.group(1)
         result.sparklingMajorVersion = match.group(2)
         result.sparklingMinorVersion = match.group(3)
         result.nightlyVersion = match.group(5)
-        result.sparkVersion = match.group(6)
+        result.sparkMajorMinorVersion = match.group(6)
+        return result
+
+    @staticmethod
+    def parseFromPySparkVersion(version):
+        match = re.search(r"^((\d+)\.(\d+))\.(\d+)(\.([0-9A-Za-z]+))?$", version)
+        result = VersionComponents()
+        result.fullVersion = match.group(0)
+        result.sparkMajorMinorVersion = match.group(1)
+        result.sparkMajorVersion = match.group(2)
+        result.sparkMinorVersion = match.group(3)
+        result.sparkPatchVersion = match.group(4)
+        result.sparkBuildVersion = match.group(6)
         return result
