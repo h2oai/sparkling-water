c70cc12dfbc5918061905891b0ac6c1910c4e866
Jakub Hava <jakub.hava@gmail.com>
2019-09-13 09:13:17 +0200

    [SW-1614] Fix running python integ tests on external backend

--- py/tests/integration_local/conftest.py
+++ py/tests/integration_local/conftest.py
@@ -15,10 +15,11 @@
 # limitations under the License.
 #
 import pytest
+from tests import integ_test_utils
 
 
 @pytest.fixture(scope="module")
 def integ_spark_conf(spark_conf, dist):
     spark_conf["spark.master"] = "local[*]"
     spark_conf["spark.submit.pyFiles"] = dist
-    return spark_conf
+    return integ_test_utils.get_default_spark_conf(spark_conf)
