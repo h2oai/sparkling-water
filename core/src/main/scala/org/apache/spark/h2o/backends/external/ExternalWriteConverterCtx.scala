bf5091676ef0f35b32e24d53f77dd66bf19fc91b
Jakub Hava <jakub.hava@gmail.com>
2019-09-18 12:21:39 +0200

    [SW-1626] Intermittent failure during conversion to h2o frame on External backend in PySparking

--- core/src/main/scala/org/apache/spark/h2o/backends/external/ExternalWriteConverterCtx.scala
+++ core/src/main/scala/org/apache/spark/h2o/backends/external/ExternalWriteConverterCtx.scala
@@ -41,7 +41,9 @@ class ExternalWriteConverterCtx(nodeDesc: NodeDesc, writeTimeout: Int, driverTim
 
   override def finalizeFrame(key: String, rowsPerChunk: Array[Long], colTypes: Array[Byte], domains: Array[Array[String]] = null): H2OFrame = {
     externalFrameWriter.finalizeFrame(key, rowsPerChunk, colTypes, domains)
-    new H2OFrame(DKV.getGet[Frame](key))
+    val fr = new H2OFrame(DKV.getGet[Frame](key))
+    DKV.put(fr)
+    fr
   }
 
   /**
