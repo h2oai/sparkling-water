d6b057f998ac9010c58e8d575a7ddbbb4b5d91ec
Marek Novotný <marek.novotny@h2o.ai>
2019-10-07 13:46:30 +0200

    Using different headers

--- core/src/test/scala/org/apache/spark/h2o/backends/SharedBackendUtilsTestSuite.scala
+++ core/src/test/scala/org/apache/spark/h2o/backends/SharedBackendUtilsTestSuite.scala
@@ -4,18 +4,18 @@ import org.scalatest.{FunSuite, Matchers}
 
 class SharedBackendUtilsTestSuite extends FunSuite with Matchers {
 
-  val expectedHttpHeaderArgs = Seq(
-    "-add_http_header", "X-Frame-Options", "deny",
-    "-add_http_header", "X-XSS-Protection", "X-XSS-Protection: 1; mode=block",
-    "-add_http_header", "X-Content-Type-Options", "nosniff",
-    "-add_http_header", "Content-Security-Policy", "default-src 'self' 'unsafe-eval' 'unsafe-inline'; img-src 'self' data:")
+  private val expectedHttpHeaderArgs = Seq(
+    "-add_http_header", "X-Request-ID", "f058ebd6-02f7-4d3f-942e-904344e8cde5",
+    "-add_http_header", "X-Csrf-Token", "i8XNjC4b8KVok4uw5RftR38Wgp2BFwq",
+    "-add_http_header", "X-Forwarded-For", "129.78.138.66, 129.78.64.103",
+    "-add_http_header", "Content-Security-Policy-Report-Only", "default-src 'none'; style-src cdn.example.com:8080; report-uri /_/csp-reports")
 
   test("parseStringToHttpArgs parses list of http headers with regular formatting") {
     val input =
-      """X-Frame-Options: deny
-        |X-XSS-Protection: X-XSS-Protection: 1; mode=block
-        |X-Content-Type-Options: nosniff
-        |Content-Security-Policy: default-src 'self' 'unsafe-eval' 'unsafe-inline'; img-src 'self' data:
+      """X-Request-ID: f058ebd6-02f7-4d3f-942e-904344e8cde5
+        |X-Csrf-Token: i8XNjC4b8KVok4uw5RftR38Wgp2BFwq
+        |X-Forwarded-For: 129.78.138.66, 129.78.64.103
+        |Content-Security-Policy-Report-Only: default-src 'none'; style-src cdn.example.com:8080; report-uri /_/csp-reports
       """.stripMargin
 
     val result = SharedBackendUtils.parseStringToHttpHeaderArgs(input)
@@ -25,10 +25,10 @@ class SharedBackendUtilsTestSuite extends FunSuite with Matchers {
 
   test("parseStringToHttpArgs parses list of http headers with random formatting") {
     val input =
-      """X-Frame-Options:     deny
-        |        X-XSS-Protection: X-XSS-Protection: 1; mode=block
-        |    X-Content-Type-Options:     nosniff
-        |   Content-Security-Policy:default-src 'self' 'unsafe-eval' 'unsafe-inline'; img-src 'self' data:
+      """   X-Request-ID:    f058ebd6-02f7-4d3f-942e-904344e8cde5
+        |   X-Csrf-Token:   i8XNjC4b8KVok4uw5RftR38Wgp2BFwq
+        |X-Forwarded-For:           129.78.138.66, 129.78.64.103
+        |Content-Security-Policy-Report-Only:default-src 'none'; style-src cdn.example.com:8080; report-uri /_/csp-reports
       """.stripMargin
 
     val result = SharedBackendUtils.parseStringToHttpHeaderArgs(input)
@@ -41,16 +41,13 @@ class SharedBackendUtilsTestSuite extends FunSuite with Matchers {
       """
         |
         |
-        |X-Frame-Options: deny
-        |
-        |X-XSS-Protection: X-XSS-Protection: 1; mode=block
+        |X-Request-ID: f058ebd6-02f7-4d3f-942e-904344e8cde5
         |
         |
+        |X-Csrf-Token: i8XNjC4b8KVok4uw5RftR38Wgp2BFwq
         |
-        |X-Content-Type-Options: nosniff
-        |
-        |
-        |Content-Security-Policy: default-src 'self' 'unsafe-eval' 'unsafe-inline'; img-src 'self' data:
+        |X-Forwarded-For: 129.78.138.66, 129.78.64.103
+        |Content-Security-Policy-Report-Only: default-src 'none'; style-src cdn.example.com:8080; report-uri /_/csp-reports
         |
         |
       """.stripMargin
