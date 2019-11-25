d8d3bd0493790494fd174c881052fa38054e0800
Marek Novotný <marek.novotny@h2o.ai>
2019-11-25 14:16:34 +0100

    Remove extra parenthesis

--- py/src/ai/h2o/sparkling/H2OContext.py
+++ py/src/ai/h2o/sparkling/H2OContext.py
@@ -279,7 +279,7 @@ class H2OContext(object):
           Spark DataFrame
         """
         if isinstance(h2o_frame, H2OFrame):
-            frame_id = h2o_frame.frame_id()
+            frame_id = h2o_frame.frame_id
             jdf = self._jhc.asDataFrame(frame_id, copy_metadata)
             df = DataFrame(jdf, self._sql_context)
             # Attach h2o_frame to dataframe which forces python not to delete the frame when we leave the scope of this
