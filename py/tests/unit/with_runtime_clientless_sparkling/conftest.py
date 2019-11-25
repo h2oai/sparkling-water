3536dd6f3fd3a5fb54ec32a5760ec3eab0d674ff
Marek Novotný <marek.novotny@h2o.ai>
2019-11-25 16:08:56 +0100

    [SW-1733] Tests for Clientless Conversion from H2OFrames to DataFrames

---
+++ py/tests/unit/with_runtime_clientless_sparkling/conftest.py
@@ -0,0 +1,46 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import pytest
+from pyspark.sql import SparkSession
+from pysparkling.conf import H2OConf
+from pysparkling.context import H2OContext
+
+from tests import unit_test_utils
+
+
+@pytest.fixture(scope="module")
+def spark(spark_conf):
+    conf = unit_test_utils.get_default_spark_conf(spark_conf)
+    return SparkSession.builder.config(conf=conf).getOrCreate()
+
+
+@pytest.fixture(scope="module")
+def hc(spark):
+    conf = H2OConf(spark)
+    conf.set_cluster_size(1)
+    conf.set("spark.ext.h2o.rest.api.based.client", "true")
+    conf.use_auto_cluster_start()
+    conf.set_external_cluster_mode()
+    conf.set_h2o_node_web_enabled()
+    return H2OContext.getOrCreate(spark, conf)
+
+
+@pytest.fixture(scope="module")
+def prostateDataset(spark):
+    return spark.read.csv("file://" + unit_test_utils.locate("smalldata/prostate/prostate.csv"),
+                          header=True, inferSchema=True)
---
+++ py/tests/unit/with_runtime_clientless_sparkling/test_conversions.py
@@ -0,0 +1,99 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import h2o
+import pytest
+from pyspark.mllib.linalg import *
+from pyspark.sql.types import *
+
+from tests import generic_test_utils
+from tests import unit_test_utils
+
+def testH2OFrameToDataframe(hc):
+    frame = h2o.upload_file(generic_test_utils.locate("smalldata/prostate/prostate.csv"))
+    df = hc.as_spark_frame(frame)
+    assert df.count() == frame.nrow, "Number of rows should match"
+    assert len(df.columns) == frame.ncol, "Number of columns should match"
+    assert df.columns == frame.names, "Column names should match"
+
+
+def testH2OFrameToDataframeWithSecondConversion(hc):
+    frame = h2o.upload_file(generic_test_utils.locate("smalldata/prostate/prostate.csv"))
+    df1 = hc.as_spark_frame(frame)
+    df2 = hc.as_spark_frame(frame)
+    assert df1.count() == df2.count(), "Number of rows should match"
+    assert len(df1.columns) == len(df2.columns), "Number of columns should match"
+    assert df1.columns == df2.columns, "Column names should match"
+
+
+@pytest.mark.parametrize(
+    "data,sparkType",
+    [
+        pytest.param(
+            [1, 2, 3, 4, -3, -2, -1],
+            ByteType(),
+            id="byte"
+        ),
+        pytest.param(
+            [1001, 1002, 1003, 1004, -1003, -1002, -1001],
+            ShortType(),
+            id="short"
+        ),
+        pytest.param(
+            [100001, 100002, 100003, 100004, -100003, -100002, -100001],
+            IntegerType(),
+            id="integer"
+        ),
+        pytest.param(
+            [100000000001, 100000000002, 100000000003, 100000000004, -100000000003, -100000000002, -100000000001],
+            LongType(),
+            id="long"
+        ),
+        pytest.param(
+            [1.1, 2.2, 3.3, 4.4, -3.3, -2.2, -1.1],
+            DoubleType(),
+            id="double"
+        ),
+        pytest.param(
+            ["a", "b", "c", "d", "c", "b", "a"],
+            StringType(),
+            id="string"
+        ),
+    ],
+)
+def testH2OFrameOfSpecificTypeToDataframe(spark, hc, data, sparkType):
+    columnName = 'A'
+    schema = StructType([
+        StructField(columnName, sparkType, False),
+    ])
+
+    originalDF = spark.createDataFrame(map(lambda i: (i,), data), schema)
+    frame = h2o.H2OFrame(data, column_names=[columnName])
+
+    transformedDF = hc.as_spark_frame(frame)
+
+    unit_test_utils.assert_data_frames_are_identical(originalDF, transformedDF)
+    assert originalDF.dtypes == transformedDF.dtypes
+
+
+# test for SW-321
+def testInnerCbindTransform(hc):
+    frame1 = h2o.H2OFrame({'A': [1, 2, 3]})
+    frame2 = h2o.H2OFrame({'B': [4, 5, 6]})
+    df = hc.as_spark_frame(frame1.cbind(frame2))
+    count = df.count()
+    assert count == 3, "Number of rows is 3"
