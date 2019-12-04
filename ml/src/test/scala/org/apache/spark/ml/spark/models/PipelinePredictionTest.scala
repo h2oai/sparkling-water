5f7dc8ae115d60e1df3fff56e76df269e650f37d
Marek Novotný <marek.novotny@h2o.ai>
2019-12-04 15:59:44 +0100

    Adressing review comments

--- ml/src/test/scala/org/apache/spark/ml/spark/models/PipelinePredictionTest.scala
+++ ml/src/test/scala/org/apache/spark/ml/spark/models/PipelinePredictionTest.scala
@@ -49,9 +49,7 @@ abstract class PipelinePredictionTestBase extends FunSuite with SharedH2OTestCon
   }
 
   def trainedPipelineModel(spark: SparkSession): PipelineModel = {
-    implicit val hc: H2OContext = H2OContext.getOrCreate(sc, new H2OConf(spark).setClusterSize(1))
 
-    //implicit val sqlContext: SQLContext = spark.sqlContext
     /**
       * Define the pipeline stages
       */
