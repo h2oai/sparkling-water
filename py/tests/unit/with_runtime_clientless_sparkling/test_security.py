e9745da1fa1fc5b6e1adaff106a4e6f434fcce69
Jakub Hava <jakub.hava@gmail.com>
2019-12-13 12:59:31 +0100

    finalize

--- py/tests/unit/with_runtime_clientless_sparkling/test_security.py
+++ py/tests/unit/with_runtime_clientless_sparkling/test_security.py
@@ -5,41 +5,38 @@
 # limitations under the License.
 #
 
+import pytest
 from pysparkling.context import H2OContext
 
 from tests.unit.with_runtime_clientless_sparkling.clientless_test_utils import *
 
 
-def testStartWithSSLAndAuthorization(spark):
-    with open('build/login.conf', 'w') as f:
-        f.write('user:pass')
-
+def testSSL(spark):
     conf = createH2OConf(spark)
-    # Require authentication
-    conf.set_hash_login_enabled()
-    conf.set_login_conf("build/login.conf")
-    # Secure External H2O backend by SSL
     conf.set_internal_secure_connections_enabled()
 
-    context = H2OContext.getOrCreate(spark, conf, auth=("user", "pass"))
+    context = H2OContext.getOrCreate(spark, conf)
     path = context.download_h2o_logs("build", "LOG")
 
     with open(path, 'r') as f:
         originalLines = f.readlines()
         lines = list(filter(lambda line: "H2O node running in encrypted mode using" in line, originalLines))
         assert len(lines) >= 1
-        lines = list(filter(lambda line: "-hash_login" in line, originalLines))
-        assert len(lines) >= 1
     context.stop()
 
-def testFailsWhenAuthNotSpecified(spark):
+
+def testAuth(spark):
     with open('build/login.conf', 'w') as f:
         f.write('user:pass')
 
     conf = createH2OConf(spark)
-    # Require authentication
     conf.set_hash_login_enabled()
     conf.set_login_conf("build/login.conf")
+    context = H2OContext.getOrCreate(spark, conf, auth=("user", "pass"))
+    path = context.download_h2o_logs("build", "LOG")
 
-    context = H2OContext.getOrCreate(spark, conf)
+    with open(path, 'r') as f:
+        originalLines = f.readlines()
+        lines = list(filter(lambda line: "-hash_login" in line, originalLines))
+        assert len(lines) >= 1
     context.stop()
