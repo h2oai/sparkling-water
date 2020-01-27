394edef5a84cef0cde284d2fef19bfdfd1404510
Jakub Háva <jakub.hava@gmail.com>
2020-01-28 11:08:58 +1300

    fixes

--- r-cran/src/R/package.R
+++ r-cran/src/R/package.R
@@ -1,14 +1,20 @@
-spark_dependencies <- function(spark_version, scala_version, ...) {
-  content <- read.csv(url("http://h2o-release.s3.amazonaws.com/sparkling-water/spark-2.4/latest"),
-                      header = FALSE)
-  version <- as.vector(content[1, 1])
-  stop(paste0('You are using RSParkling installed from CRAN, but that version is not maintained anymore. Please install latest RSparkling.
+getVersion <- function(sparkMajor) {
+  content <- read.csv(url(paste0("http://h2o-release.s3.amazonaws.com/sparkling-water/spark-", sparkMajor, "/latest")), header = FALSE)
+  as.vector(content[1, 1])
+}
 
-  For Spark 2.4 -> install.packages("rsparkling", type = "source", repos = "http://h2o-release.s3.amazonaws.com/sparkling-water/spark-2.4/', version, '/R")
-  For Spark 2.3 -> install.packages("rsparkling", type = "source", repos = "http://h2o-release.s3.amazonaws.com/sparkling-water/spark-2.3/', version, '/R")
-  For Spark 2.2 -> install.packages("rsparkling", type = "source", repos = "http://h2o-release.s3.amazonaws.com/sparkling-water/spark-2.2/', version, '/R")
-  For Spark 2.1 -> install.packages("rsparkling", type = "source", repos = "http://h2o-release.s3.amazonaws.com/sparkling-water/spark-2.1/', version, '/R")
-  '))
+getLink <- function(sparkMajor) {
+  paste0('install.packages("rsparkling", type = "source", repos = "http://h2o-release.s3.amazonaws.com/sparkling-water/spark-', sparkMajor, '/', getVersion(sparkMajor), '/R")')
+}
+
+spark_dependencies <- function(spark_version, scala_version, ...) {
+
+  stop(paste0('You are using RSParkling installed from CRAN which is not maintained anymore. Please install latest RSparkling as:
+
+  For Spark 2.4 -> ', getLink("2.4"), '
+  For Spark 2.3 -> ', getLink("2.3"), '
+  For Spark 2.2 -> ', getLink("2.2"), '
+  For Spark 2.1 -> ', getLink("2.1")))
   spark_dependency(jars = c())
 }
 
