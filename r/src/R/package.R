178e9e8dc6096f7229f827a8974ff70b5821bd6c
Jakub Háva <jakub.hava@gmail.com>
2020-01-28 08:16:10 +1300

    [SW-1848] Cleanup package.R in RSparkling (#1744)

--- r/src/R/package.R
+++ r/src/R/package.R
@@ -1,17 +1,7 @@
-#' @importFrom utils capture.output browseURL
-#' @importFrom sparklyr spark_dependency register_extension invoke_static invoke spark_connection spark_dataframe sdf_register spark_context
-#' @importFrom h2o h2o.getFrame h2o.getId h2o.init
-#' @importFrom utils read.table
-#' @importFrom utils packageVersion
-NULL
-
-
-
-# Verify H2O version
 verify_h2o_version <- function(h2o_version, h2o_build_name, h2o_build_version, sw_version) {
-    current_h2o_version <- paste(packageVersion("h2o"))
-    if (current_h2o_version != h2o_version) {
-        message(paste0('\nDetected H2O ', current_h2o_version ,'. Please install H2O ', h2o_version ,', which is required for Sparkling Water ', sw_version ,'.\n
+  current_h2o_version <- paste(packageVersion("h2o"))
+  if (current_h2o_version != h2o_version) {
+    message(paste0('\nDetected H2O ', current_h2o_version, '. Please install H2O ', h2o_version, ', which is required for Sparkling Water ', sw_version, '.\n
 
   To update your h2o R package, copy/paste the following commands and then restart your R session:
 
@@ -22,7 +12,6 @@ verify_h2o_version <- function(h2o_version, h2o_build_name, h2o_build_version, s
      install.packages("h2o", type = "source", repos = "https://h2o-release.s3.amazonaws.com/h2o/rel-', h2o_build_name ,'/', h2o_build_version ,'/R")\n'))
     }
 }
-
 
 # Define required spark packages
 spark_dependencies <- function(spark_version, scala_version, ...) {
@@ -43,4 +32,3 @@ spark_dependencies <- function(spark_version, scala_version, ...) {
 .onLoad <- function(libname, pkgname) {
   register_extension(pkgname)
 }
-
