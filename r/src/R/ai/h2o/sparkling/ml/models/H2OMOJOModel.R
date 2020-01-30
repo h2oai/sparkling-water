98a59e4a8ad91b190d1d5c2e57ccb81fa3a83b48
Jakub Háva <jakub.hava@gmail.com>
2020-01-30 15:57:26 +1300

    [SW-1830] Support h2o3 mojo prediction in rsparkling

---
+++ r/src/R/ai/h2o/sparkling/ml/models/H2OMOJOModel.R
@@ -0,0 +1,58 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+source(file.path("R", "H2OMOJOModelBase.R"))
+
+H2OMOJOModel.createFromMojo <- function(pathToMojo, settings = H2OMOJOSettings.default()) {
+  sc <- spark_connection_find()[[1]]
+  jmojo <- invoke_static(sc, "ai.h2o.sparkling.ml.models.H2OMOJOModel", "createFromMojo", pathToMojo, settings$toJavaObject())
+  className <- invoke(invoke(jmojo, "getClass"), "getSimpleName")
+  if (className == "H2OTreeBasedSupervisedMOJOModel") {
+    H2OTreeBasedSupervisedMOJOModel(jmojo)
+  } else if (className == "H2OSupervisedMOJOModel") {
+    H2OSupervisedMOJOModel(jmojo)
+  } else if (className == "H2OUnsupervisedMOJOModel") {
+    H2OUnsupervisedMOJOModel(jmojo)
+  } else {
+    H2OMOJOModel(jmojo)
+  }
+}
+
+#' @export H2OMOJOModel
+H2OMOJOModel <- setRefClass("H2OMOJOModel", contains = ("H2OMOJOModelBase"), methods = list(
+  getModelDetails = function() {
+    invoke(.self$jmojo, "getModelDetails")
+  }
+))
+
+#' @export H2OSupervisedMOJOModel
+H2OSupervisedMOJOModel <- setRefClass("H2OSupervisedMOJOModel", contains = ("H2OMOJOModel"), methods = list(
+  getOffsetCol = function() {
+    invoke(.self$jmojo, "getOffsetCol")
+  }
+))
+
+#' @export H2OTreeBasedSupervisedMOJOModel
+H2OTreeBasedSupervisedMOJOModel <- setRefClass("H2OTreeBasedSupervisedMOJOModel", contains = ("H2OSupervisedMOJOModel"), methods = list(
+  getNtrees = function() {
+    invoke(.self$jmojo, "getNtrees")
+  }
+))
+
+#' @export H2OUnsupervisedMOJOModel
+H2OUnsupervisedMOJOModel <- setRefClass("H2OUnupervisedMOJOModel", contains = ("H2OMOJOModel"), fields = list(jmojo = "ANY"), methods = list(
+))
---
+++ r/src/tests/testthat/testMojo.R
@@ -0,0 +1,73 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+context("Test MOJO predictions")
+
+config <- spark_config()
+config <- c(config, list(
+  "spark.hadoop.yarn.timeline-service.enabled" = "false",
+  "spark.ext.h2o.external.cluster.size" = "1",
+  "spark.ext.h2o.backend.cluster.mode" = Sys.getenv("spark.ext.h2o.backend.cluster.mode"),
+  "spark.ext.h2o.client.ip" = Sys.getenv("H2O_CLIENT_IP"),
+  "sparklyr.gateway.connect.timeout" = 240,
+  "sparklyr.gateway.start.timeout" = 240,
+  "sparklyr.backend.timeout" = 240,
+  "spark.ext.h2o.external.start.mode" = "auto",
+  "spark.ext.h2o.external.disable.version.check" = "true"
+))
+
+locate <- function(fileName) {
+  firstPath <- file.path("/home/0xdiag/", fileName)
+  if (file.exists(firstPath)) {
+    firstPath
+  } else {
+    normalizePath(file.path("../../../../examples/", fileName))
+  }
+}
+
+
+test_that("test MOJO predictions", {
+  sc <- spark_connect(master = "local[*]", config = config)
+  path <- paste0("file://", locate("smalldata/prostate/prostate.csv"))
+  dataset <- spark_read_csv(sc, path = path, infer_schema = TRUE, header = TRUE)
+  # Try loading the Mojo and prediction on it without starting H2O Context
+  mojo <- H2OMOJOModel.createFromMojo(paste0("file://", normalizePath("../../../../ml/src/test/resources/binom_model_prostate.mojo")))
+  sdf <- mojo$transform(dataset)
+  data <- dplyr::collect(mojo$transform(sdf))
+  expect_equal(colnames(data), c("ID", "CAPSULE", "AGE", "RACE", "DPROS", "DCAPS", "PSA", "VOL", "GLEASON", "prediction"))
+})
+
+
+test_that("test MOJO predictions on unseen categoricals", {
+  sc <- spark_connect(master = "local[*]", config = config)
+  path <- paste0("file://", normalizePath("../../../../ml/src/test/resources/deep_learning_airlines_categoricals.zip"))
+  settings <- H2OMOJOSettings(convertUnknownCategoricalLevelsToNa = TRUE)
+  mojo <- H2OMOJOModel.createFromMojo(path, settings)
+
+  df <- as.data.frame(t(c(5.1, 3.5, 1.4, 0.2, "Missing_categorical")))
+  colnames(df) <- c("sepal_len", "sepal_wid", "petal_len", "petal_wid", "class")
+  sdf <- copy_to(sc, df, overwrite = TRUE)
+
+  data <- dplyr::collect(mojo$transform(sdf))
+
+  expect_equal(as.character(dplyr::select(data, class)), "Missing_categorical")
+  expect_equal(as.double(dplyr::select(data, petal_len)), 1.4)
+  expect_equal(as.double(dplyr::select(data, petal_wid)), 0.2)
+  expect_equal(as.double(dplyr::select(data, sepal_len)), 5.1)
+  expect_equal(as.double(dplyr::select(data, sepal_wid)), 3.5)
+  expect_equal(as.double(dplyr::select(data, prediction)), 5.240174068202646)
+})
