28028ad4195aa47246212126d6c216a3924a9483
Marek Novotný <marek.novotny@h2o.ai>
2020-01-30 12:12:16 +0100

    [SW-1854] Expose Individual Chunks via REST API (#1752)
    * [SW-1854] Expose Individual Chunks via REST API
    
    * Adding servlet provider
    
    * Reference to Sparkling Water Utils
    
    * Remove invalid checks

---
+++ extensions/src/main/scala/ai/h2o/sparkling/extensions/serde/ChunkAutoBufferReader.scala
@@ -0,0 +1,101 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package ai.h2o.sparkling.extensions.serde
+
+import java.io.{Closeable, InputStream}
+import java.sql.Timestamp
+
+import water.AutoBuffer
+import SerializationUtils._
+
+final class ChunkAutoBufferReader(val inputStream: InputStream) extends Closeable {
+  private val buffer = new AutoBuffer(inputStream)
+  private val numRows = readInt
+  private var isLastNAVar: Boolean = false
+
+  def getNumRows: Int = numRows
+
+  def readBoolean: Boolean = {
+    val data = buffer.getZ
+    isLastNAVar = isNA(buffer, data)
+    data
+  }
+
+  def readByte: Byte = {
+    val data = buffer.get1
+    isLastNAVar= isNA(buffer, data)
+    data
+  }
+
+  def readChar: Char = {
+    val data = buffer.get2
+    isLastNAVar = isNA(buffer, data)
+    data
+  }
+
+  def readShort: Short = {
+    val data = buffer.get2s
+    isLastNAVar = isNA(buffer, data)
+    data
+  }
+
+  def readInt: Int = {
+    val data = buffer.getInt
+    isLastNAVar= isNA(buffer, data)
+    data
+  }
+
+  def readLong: Long = {
+    val data = buffer.get8
+    isLastNAVar = isNA(buffer, data)
+    data
+  }
+
+  def readFloat: Float = {
+    val data = buffer.get4f
+    isLastNAVar = isNA(data)
+    data
+  }
+
+  def readDouble: Double = {
+    val data = buffer.get8d
+    isLastNAVar = isNA(data)
+    data
+  }
+
+  def readString: String = {
+    val data = buffer.getStr
+    isLastNAVar = isNA(buffer, data)
+    data
+  }
+
+  def readTimestamp: Timestamp = {
+    val data = new Timestamp(buffer.get8)
+    isLastNAVar = isNA(buffer, data)
+    data
+  }
+
+  /**
+    * This method is used to check if the last received value was marked as NA by H2O backend
+    */
+  def isLastNA: Boolean = isLastNAVar
+
+  override def close(): Unit = {
+    buffer.close
+  }
+}
---
+++ extensions/src/main/scala/ai/h2o/sparkling/extensions/serde/ChunkAutoBufferWriter.scala
@@ -0,0 +1,74 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package ai.h2o.sparkling.extensions.serde
+
+import SerializationUtils._
+
+import java.io.{Closeable, OutputStream}
+import java.util.UUID
+
+import water.fvec.ChunkUtils
+import water.parser.BufferedString
+import water.{AutoBuffer, DKV}
+
+final class ChunkAutoBufferWriter(val outputStream: OutputStream) extends Closeable {
+
+  private val buffer = new AutoBuffer(outputStream, false)
+
+  def writeChunk(frameName: String, chunkId: Int, expectedTypes: Array[Byte], selectedColumnIndices: Array[Int]): Unit = {
+    val frame = DKV.getGet(frameName)
+    val chunks = ChunkUtils.getChunks(frame, chunkId)
+    val numberOfRows = chunks(0)._len
+    sendInt(buffer, numberOfRows)
+    // buffered string to be reused for strings to avoid multiple allocation in the loop
+    val valStr = new BufferedString
+    var rowIdx = 0
+    while (rowIdx < numberOfRows) {
+      var i = 0
+      while (i < selectedColumnIndices.length) {
+        if (chunks(selectedColumnIndices(i)).isNA(rowIdx)) {
+          sendNA(buffer, expectedTypes(i))
+        } else {
+          val chunk = chunks(selectedColumnIndices(i))
+          expectedTypes(i) match {
+            case EXPECTED_BOOL => sendBoolean(buffer, chunk.at8(rowIdx).toByte)
+            case EXPECTED_BYTE => sendByte(buffer, chunk.at8(rowIdx).toByte)
+            case EXPECTED_CHAR => sendChar(buffer, chunk.at8(rowIdx).toChar)
+            case EXPECTED_SHORT => sendShort(buffer, chunk.at8(rowIdx).toShort)
+            case EXPECTED_INT => sendInt(buffer, chunk.at8(rowIdx).toInt)
+            case EXPECTED_FLOAT => sendFloat(buffer, chunk.atd(rowIdx).toFloat)
+            case EXPECTED_LONG => sendLong(buffer, chunk.at8(rowIdx))
+            case EXPECTED_DOUBLE => sendDouble(buffer, chunk.atd(rowIdx))
+            case EXPECTED_TIMESTAMP => sendTimestamp(buffer, chunk.at8(rowIdx))
+            case EXPECTED_STRING =>
+              var str: String = null
+              if (chunk.vec.isCategorical) str = chunk.vec().domain()(chunk.at8(rowIdx).toInt)
+              else if (chunk.vec.isString) str = chunk.atStr(valStr, rowIdx).toString
+              else if (chunk.vec.isUUID) str = new UUID(chunk.at16h(rowIdx), chunk.at16l(rowIdx)).toString
+              else assert(false, "Can never be here")
+              sendString(buffer, str)
+          }
+        }
+        i += 1
+      }
+      rowIdx = rowIdx + 1
+    }
+  }
+
+  override def close(): Unit = buffer.close()
+}
