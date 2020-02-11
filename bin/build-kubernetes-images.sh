c0398aa03b571bbcadf13acec052aa5058141269
Jakub Háva <jakub.hava@gmail.com>
2020-02-11 14:51:34 +1000

    [SW-1928] Improve kubernetes documentation

--- bin/build-kubernetes-images.sh
+++ bin/build-kubernetes-images.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+if [[ "$#" -ne 1 ]] || [[ "$1" != "scala" && "$1" != "python" && "$1" != "r" ]]; then
+  echo "This script expects exactly one argument which specifies for which client the image should be build."
+  echo "The possible values are: scala, r, python"
+  exit -1
+fi
+
+
 # Current dir
 TOPDIR=$(cd "$(dirname "$0")/.." || exit; pwd)
 
@@ -12,24 +19,30 @@ WORKDIR=$(mktemp -d)
 echo "Working directory created: $WORKDIR"
 
 K8DIR="$TOPDIR/kubernetes"
-cp "$K8DIR/Dockerfile-Python" "$WORKDIR"
-cp "$K8DIR/Dockerfile-R" "$WORKDIR"
-cp "$K8DIR/Dockerfile-Scala" "$WORKDIR"
 
-echo "Bulding Docker Image for Sparkling Water(Scala) ..."
-cp "$FAT_JAR_FILE" "$WORKDIR"
-docker build -t "sparkling-water-scala:$VERSION" -f "$WORKDIR/Dockerfile-Scala" "$WORKDIR"
-echo "Done!"
+if [ "$1" = "scala" ]; then
+  cp "$K8DIR/Dockerfile-Scala" "$WORKDIR"
+  echo "Bulding Docker Image for Sparkling Water(Scala) ..."
+  cp "$FAT_JAR_FILE" "$WORKDIR"
+  docker build -t "sparkling-water-scala:$VERSION" -f "$WORKDIR/Dockerfile-Scala" "$WORKDIR"
+  echo "Done!"
+fi
 
-echo "Bulding Docker Image for PySparkling(Python) ..."
-cp "$PY_ZIP_FILE" "$WORKDIR"
-docker build -t "sparkling-water-python:$VERSION" -f "$WORKDIR/Dockerfile-Python" "$WORKDIR"
-echo "Done!"
+if [ "$1" = "python" ]; then
+  cp "$K8DIR/Dockerfile-Python" "$WORKDIR"
+  echo "Bulding Docker Image for PySparkling(Python) ..."
+  cp "$PY_ZIP_FILE" "$WORKDIR"
+  docker build -t "sparkling-water-python:$VERSION" -f "$WORKDIR/Dockerfile-Python" "$WORKDIR"
+  echo "Done!"
+fi
 
-echo "Bulding Docker Image for RSparkling(R) ..."
-cp "$TOPDIR/rsparkling_$VERSION.tar.gz" "$WORKDIR"
-docker build -t "sparkling-water-r:$VERSION" -f "$WORKDIR/Dockerfile-R" "$WORKDIR"
-echo "Done!"
+if [ "$1" = "r" ]; then
+  cp "$K8DIR/Dockerfile-R" "$WORKDIR"
+  echo "Bulding Docker Image for RSparkling(R) ..."
+  cp "$TOPDIR/rsparkling_$VERSION.tar.gz" "$WORKDIR"
+  docker build -t "sparkling-water-r:$VERSION" -f "$WORKDIR/Dockerfile-R" "$WORKDIR"
+  echo "Done!"
+fi
 
 echo "Cleaning up temporary directories"
 rm -rf "$WORKDIR"
