9e37263588a5bbb12f66595e2c81fbf7972faa58
Marek Novotný <marek.novotny@h2o.ai>
2020-02-17 19:45:13 +0100

    specify region

--- benchmarks/src/main/terraform/aws/modules/emr_benchmarks_deployment/main.tf
+++ benchmarks/src/main/terraform/aws/modules/emr_benchmarks_deployment/main.tf
@@ -109,7 +109,7 @@ resource "aws_s3_bucket_object" "run_benchmarks_script" {
     runBenchmarks "yarn" "internal" "${var.benchmarks_driver_memory_gb}G" "${var.benchmarks_executor_memory_gb}G"
   fi
   if ${var.benchmarks_run_yarn_external}; then
-    aws s3 cp ${format("s3://h2o-release/h2o/%s/%s/h2o-%s-hdp2.6.zip", var.h2o_version_name, var.h2o_build, var.h2o_version)} /home/hadoop/h2o.zip
+    aws s3 cp ${format("s3://h2o-release/h2o/%s/%s/h2o-%s-hdp2.6.zip", var.h2o_version_name, var.h2o_build, var.h2o_version)} /home/hadoop/h2o.zip --source-region us-east-1
     unzip -q /home/hadoop/h2o.zip -d /home/hadoop
     export H2O_DRIVER_JAR="/home/hadoop/h2o-${var.h2o_version}-hdp2.6/h2odriver.jar"
     runBenchmarks "yarn" "external" "${var.benchmarks_driver_memory_gb}G" "${var.benchmarks_executor_memory_gb/2}G"
