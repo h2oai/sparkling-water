c050a021d201f5edc9b4377586443e084ce7f747
Marek Novotný <marek.novotny@h2o.ai>
2020-02-17 17:17:17 +0100

    [SW-1958] Property for Passing Extra Jars to External Backend

--- core/src/main/scala/ai/h2o/sparkling/backend/external/ExternalBackendConf.scala
+++ core/src/main/scala/ai/h2o/sparkling/backend/external/ExternalBackendConf.scala
@@ -21,6 +21,8 @@ import ai.h2o.sparkling.backend.shared.SharedBackendConf
 import org.apache.spark.h2o.H2OConf
 import collection.JavaConverters._
 
+import scala.collection.JavaConverters._
+
 /**
  * External backend configuration
  */
