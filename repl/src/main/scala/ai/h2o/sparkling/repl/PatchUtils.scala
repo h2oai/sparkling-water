f3ecb9e6fb86ecc03f9e4668fc335e96e75f1e14
Jakub Háva <jakub.hava@gmail.com>
2020-02-26 07:08:52 +1000

    fix

--- repl/src/main/scala/ai/h2o/sparkling/repl/PatchUtils.scala
+++ repl/src/main/scala/ai/h2o/sparkling/repl/PatchUtils.scala
@@ -59,7 +59,7 @@ private[repl] object PatchUtils {
       try {
         f.set(obj, OUTER_SCOPE_REPL_REGEX)
       } catch {
-        case _: IllegalAccessError => // we have already patched once
+        case _: IllegalArgumentException => // we have already patched once
       }
       true
     }
