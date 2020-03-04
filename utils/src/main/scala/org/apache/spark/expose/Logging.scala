387148edb88758d308b3cbb0ec4448188209173c
Jakub Háva <jakub.hava@gmail.com>
2020-03-04 20:28:01 +1000

    [SW-2040] Fix doc warning: Could not find any member to link for org.apache.spark.internal.Logging (#1914)

--- utils/src/main/scala/org/apache/spark/expose/Logging.scala
+++ utils/src/main/scala/org/apache/spark/expose/Logging.scala
@@ -17,6 +17,6 @@
 package org.apache.spark.expose
 
 /**
-  * The trait is used just to make visible the logic of [[org.apache.spark.internal.Logging]].
+  * The trait is used to make visible the logic of org.apache.spark.internal.Logging.
   */
 trait Logging extends org.apache.spark.internal.Logging with Serializable
