897dff4ae1c314b460227a871dd9aca3ceaa936d
Jakub Háva <jakub.hava@gmail.com>
2020-03-23 12:35:28 +0800

    [SW-1818][FollowUp] Switch PySparkling & RSparkling in internal backend to client-less approach by default (#1973)

--- core/src/main/scala/ai/h2o/sparkling/backend/utils/H2OClientUtils.scala
+++ core/src/main/scala/ai/h2o/sparkling/backend/utils/H2OClientUtils.scala
@@ -17,7 +17,7 @@
 
 package ai.h2o.sparkling.backend.utils
 
-import java.net.NetworkInterface
+import java.net.{InetAddress, NetworkInterface}
 
 import ai.h2o.sparkling.backend.NodeDesc
 import ai.h2o.sparkling.backend.api.RestAPIManager
@@ -133,9 +133,9 @@ object H2OClientUtils extends SharedBackendUtils {
       interface.getInterfaceAddresses.asScala.foreach { address =>
         val ip = address.getAddress.getHostAddress + "/" + address.getNetworkPrefixLength
         val cidr = HostnameGuesser.CIDRBlock.parse(ip)
-        //if (cidr != null && cidr.isInetAddressOnNetwork(InetAddress.getByName(remoteAddress))) {
-        //  return Some(address.getAddress.getHostAddress)
-        //}
+        if (cidr != null && cidr.isInetAddressOnNetwork(InetAddress.getByName(remoteAddress))) {
+          return Some(address.getAddress.getHostAddress)
+        }
       }
     }
     None
