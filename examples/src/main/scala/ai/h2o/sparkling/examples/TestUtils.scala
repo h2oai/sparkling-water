6e6e3cc733310dd5fa10cf2646a95799ffe6c707
Jakub Háva <jakub.hava@gmail.com>
2020-03-24 05:44:11 +0800

    [SW-2109][FollowUp] Ensure AirlinesDemo runs without the client (#1979)

--- examples/src/main/scala/ai/h2o/sparkling/examples/TestUtils.scala
+++ examples/src/main/scala/ai/h2o/sparkling/examples/TestUtils.scala
@@ -20,12 +20,6 @@ import java.io.File
 
 object TestUtils {
   def locate(name: String): String = {
-    val abs = new File("/home/0xdiag/" + name)
-    if (abs.exists()) {
-      abs.getAbsolutePath
-    } else {
-      new File("./examples/" + name).getAbsolutePath
-    }
+    "file://" + new File("./examples/" + name).getAbsolutePath
   }
-
 }
