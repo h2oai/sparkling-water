3bc6871bb62a4022cb48699cb7075252767cd724
Jakub Háva <jakub.hava@gmail.com>
2020-03-24 09:10:03 +0800

    [SW-2113] Make HamOrSpam example use SW API

--- examples/src/scriptsTest/scala/ai/h2o/sparkling/ScriptsTestSuite.scala
+++ examples/src/scriptsTest/scala/ai/h2o/sparkling/ScriptsTestSuite.scala
@@ -24,29 +24,6 @@ class ScriptChicagoCrimeAppSmall extends ScriptsTestHelper {
 }
 
 @RunWith(classOf[JUnitRunner])
-class ScriptHamOrSpam extends ScriptsTestHelper {
-  override protected def beforeAll(): Unit = {
-    sparkConf = defaultConf.setMaster("local[*]")
-      .set("spark.driver.memory", "2G")
-      .set("spark.executor.memory", "2G")
-    super.beforeAll()
-  }
-
-  test("hamOrSpam.script.scala") {
-    val inspections = new ScriptInspections()
-    inspections.addSnippet("val answer1 = isSpam(\"Michal, h2oworld party tonight in MV?\", dlModel, hashingTF, idfModel, h2oContext)")
-    inspections.addTermToCheck("answer1")
-    inspections.addSnippet("val answer2 = isSpam(\"We tried to contact you re your reply to our offer of a Video Handset? 750 anytime any networks mins? UNLIMITED TEXT?\", dlModel, hashingTF, idfModel, h2oContext)")
-    inspections.addTermToCheck("answer2")
-
-    val result = launchScript("hamOrSpam.script.scala", inspections)
-    assert(result.codeExecutionStatus == CodeResults.Success, "Problem during interpreting the script!")
-    assert(result.realTermValues.get("answer1").get == "false", "Value of term \"answer1\" should be false")
-    assert(result.realTermValues.get("answer2").get == "true", "Value of term \"answer2\" should be true")
-  }
-}
-
-@RunWith(classOf[JUnitRunner])
 class ScriptCraigListJobTitles extends ScriptsTestHelper {
   override protected def beforeAll(): Unit = {
     sparkConf = defaultConf.setMaster("local[*]")
@@ -81,91 +58,5 @@ class CityBikeSharing extends ScriptsTestHelper {
       msg = "Exception occurred during the execution. One possible cause could be missing necessary citibike-nyc data files in examples/bigdata/laptop/citibike-nyc/ folder."
     }
     assert(result.codeExecutionStatus == CodeResults.Success, msg)
-  }
-}
-
-@RunWith(classOf[JUnitRunner])
-class ScriptPipelineHamOrSpamGBM extends ScriptsTestHelper {
-  override protected def beforeAll(): Unit = {
-    sparkConf = defaultConf.setMaster("local[*]")
-      .set("spark.driver.memory", "2G")
-      .set("spark.executor.memory", "2G")
-    super.beforeAll()
-  }
-
-  test("Ham or Spam GBM Pipeline") {
-    HamOrSpamTester.test(this, "hamOrSpamMultiAlgo.script.scala", "gbm")
-  }
-}
-
-@RunWith(classOf[JUnitRunner])
-class ScriptPipelineHamOrSpamDL extends ScriptsTestHelper {
-  override protected def beforeAll(): Unit = {
-    sparkConf = defaultConf.setMaster("local[*]")
-      .set("spark.driver.memory", "2G")
-      .set("spark.executor.memory", "2G")
-    super.beforeAll()
-  }
-
-  test("Ham or Spam DeepLearning Pipeline") {
-    HamOrSpamTester.test(this, "hamOrSpamMultiAlgo.script.scala", "dl")
-  }
-}
-
-@RunWith(classOf[JUnitRunner])
-class ScriptPipelineHamOrSpamXGBoost extends ScriptsTestHelper {
-  override protected def beforeAll(): Unit = {
-    sparkConf = defaultConf.setMaster("local[*]")
-      .set("spark.driver.memory", "2G")
-      .set("spark.executor.memory", "2G")
-    super.beforeAll()
-  }
-
-  test("Ham or Spam XGBoost Pipeline") {
-    HamOrSpamTester.test(this, "hamOrSpamMultiAlgo.script.scala", "xgboost")
-  }
-}
-
-@RunWith(classOf[JUnitRunner])
-class ScriptPipelineHamOrSpamGridSearch extends ScriptsTestHelper {
-  override protected def beforeAll(): Unit = {
-    sparkConf = defaultConf.setMaster("local[*]")
-      .set("spark.driver.memory", "2G")
-      .set("spark.executor.memory", "2G")
-    super.beforeAll()
-  }
-
-  test("Ham or Spam Grid Search Pipeline") {
-    HamOrSpamTester.test(this, "hamOrSpamMultiAlgo.script.scala", "grid_gbm")
-  }
-}
-
-@RunWith(classOf[JUnitRunner])
-class ScriptPipelineHamOrSpamAutoML extends ScriptsTestHelper {
-  override protected def beforeAll(): Unit = {
-    sparkConf = defaultConf.setMaster("local[*]")
-      .set("spark.driver.memory", "2G")
-      .set("spark.executor.memory", "2G")
-    super.beforeAll()
-  }
-
-  test("Ham or Spam AutoML Pipeline") {
-    HamOrSpamTester.test(this, "hamOrSpamMultiAlgo.script.scala", "automl")
-  }
-}
-
-object HamOrSpamTester {
-
-  def test(scriptsTestHelper: ScriptsTestHelper, fileName: String, algo: String) {
-    val inspections = new ScriptInspections()
-    inspections.addSnippet("val answer1 = isSpam(\"Michal, h2oworld party tonight in MV?\", loadedModel)")
-    inspections.addTermToCheck("answer1")
-    inspections.addSnippet("val answer2 = isSpam(\"We tried to contact you re your reply to our offer of a Video Handset? 750 anytime any networks mins? UNLIMITED TEXT?\", loadedModel)")
-    inspections.addTermToCheck("answer2")
-
-    val result = scriptsTestHelper.launchScript(fileName, inspections, "val algo=\"" + algo + "\"")
-    assert(result.codeExecutionStatus == CodeResults.Success, "Problem during interpreting the script!")
-    assert(result.realTermValues.get("answer1").get == "false", "Value of term \"answer1\" should be false")
-    assert(result.realTermValues.get("answer2").get == "true", "Value of term \"answer2\" should be true")
   }
 }
