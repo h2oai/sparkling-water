7547c915ad826c2656181ba2040c4f3d47dcc375
Jakub Háva <jakub.hava@gmail.com>
2020-04-03 22:33:28 +0800

    remove extra lines

--- py/examples/tests/pySparklingImportBeforeSparkSessionCreated.py
+++ py/examples/tests/pySparklingImportBeforeSparkSessionCreated.py
@@ -1,13 +1,10 @@
 from pysparkling import *
 from pyspark.sql import SparkSession
 
-# Initiate SparkSession
 spark = SparkSession.builder.appName("App name").getOrCreate()
-
 # Check if Sparkling Water classes are available
-
 jvm = spark.sparkContext._jvm
 package = getattr(jvm.ai.h2o.sparkling.backend, "BuildInfo$")
 module = package.__getattr__("MODULE$")
-# This would fail if the PYSPARK_SUBMIT_ARGS would not contain the SW jar as it would not be able to find the class
+# This would fail if the PYSPARK_SUBMIT_ARGS would not specify jar dependency to SW as it would not be able to find the class
 module.SWVersion()
