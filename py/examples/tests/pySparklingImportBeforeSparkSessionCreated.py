14bb35c4e87363ef92305181a9c129faac173c1e
Jakub Háva <jakub.hava@gmail.com>
2020-04-04 11:57:39 +0200

    [SW-2145][Fix] Fix initialization of Sparkling Water JAR in case we call import pysparkling and sc is not yet running (#2011)

--- py/examples/tests/pySparklingImportBeforeSparkSessionCreated.py
+++ py/examples/tests/pySparklingImportBeforeSparkSessionCreated.py
@@ -4,7 +4,7 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName("App name").getOrCreate()
 # Check if Sparkling Water classes are available
 jvm = spark.sparkContext._jvm
-package = getattr(jvm.ai.h2o.sparkling.backend, "BuildInfo$")
+package = getattr(jvm.org.apache.spark.h2o, "BuildInfo$")
 module = package.__getattr__("MODULE$")
 # This would fail if the PYSPARK_SUBMIT_ARGS would not specify jar dependency to SW as it would not be able to find the class
 module.SWVersion()
