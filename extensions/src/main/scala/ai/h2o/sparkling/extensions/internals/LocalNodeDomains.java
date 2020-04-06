0942c473e83b0760d30c35ce73fd76541b6b81ff
Marek Novotný <marek.novotny@h2o.ai>
2020-04-06 15:31:50 +0200

    [SW-1779][FOLLOWUP] Remove java doc warnings in LocalNodeDomains

--- extensions/src/main/scala/ai/h2o/sparkling/extensions/internals/LocalNodeDomains.java
+++ extensions/src/main/scala/ai/h2o/sparkling/extensions/internals/LocalNodeDomains.java
@@ -60,6 +60,8 @@ public final class LocalNodeDomains {
   /**
    * The method returns domains for all chunks on the H2O node. The first array level identifies
    * chunks, the second columns, the third column values.
+   * @param frameKey A key of a frame containing categorical domains.
+   * @return Domains for all chunks on the H2O node.
    */
   public static synchronized String[][][] getDomains(Key frameKey) {
     return domainsMap.get(frameKey).toArray(new String[0][][]);
