5da8bb89fd76f86916d561d59ce2f409604dc55a
Jakub Háva <jakub.hava@gmail.com>
2020-04-23 14:04:35 +0200

    increase executor mem

--- ci/aws/terraform/modules/jenkins/scripts/init_jenkins.groovy
+++ ci/aws/terraform/modules/jenkins/scripts/init_jenkins.groovy
@@ -230,7 +230,7 @@ def ami = new SlaveTemplate(
         '1', // numExecutors
         'jenkins', // remoteAdmin
         new UnixData('', '', '', '22'), // amiType
-        '', // jvmopts
+        '-Xms2048M', // jvmopts
         false, // stopOnTerminate
         subnetId, // subnetId
         [new EC2Tag('Name', 'SW-Tests-Jenkins-Slave')], //tags
