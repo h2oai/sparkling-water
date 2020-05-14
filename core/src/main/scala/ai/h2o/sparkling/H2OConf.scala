39624beffe25721f928e08d6598f94cda72d3898
Jakub Háva <jakub.hava@gmail.com>
2020-05-14 21:45:02 +0200

    [SW-2229][FollowUp] Deprecate setClientIcedDir & setNodeIcedDir in favor of setIcedDir and clientIcedDir & nodeIcedDir in favor of icedDir (#2106)

--- core/src/main/scala/ai/h2o/sparkling/H2OConf.scala
+++ core/src/main/scala/ai/h2o/sparkling/H2OConf.scala
@@ -120,7 +120,9 @@ object H2OConf extends Logging {
 
   def apply(sparkConf: SparkConf): H2OConf = new H2OConf(sparkConf)
 
-  private val deprecatedOptions = Map[String, String]("spark.ext.h2o.node.iced.dir" -> "spark.ext.h2o.iced.dir")
+  private val deprecatedOptions = Map[String, String](
+    "spark.ext.h2o.node.iced.dir" -> "spark.ext.h2o.iced.dir",
+    "spark.ext.h2o.client.iced.dir" -> "spark.ext.h2o.iced.dir")
 
   private def checkDeprecatedOptions(sparkConf: SparkConf): Unit = {
     deprecatedOptions.foreach {
