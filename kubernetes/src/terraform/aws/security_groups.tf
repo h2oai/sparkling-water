ffad4d2626a34199258284f97950e1e95dc09d0a
Jakub Háva <jakub.hava@gmail.com>
2020-05-18 13:59:34 +0200

    EKS created

--- kubernetes/src/terraform/aws/security_groups.tf
+++ kubernetes/src/terraform/aws/security_groups.tf
@@ -3,13 +3,12 @@ resource "aws_security_group" "worker_group_mgmt_one" {
   vpc_id = module.vpc.vpc_id
 
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
 
     cidr_blocks = [
-      "10.0.0.0/8",
-      "192.168.0.0/16",
+      "0.0.0.0/0",
     ]
   }
 }
