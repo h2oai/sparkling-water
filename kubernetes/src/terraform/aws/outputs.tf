9951ebe737656200f6baa13880790caa9e9a8395
Jakub Háva <jakub.hava@gmail.com>
2020-05-18 13:08:17 +0200

    remove commented code

--- kubernetes/src/terraform/aws/outputs.tf
+++ kubernetes/src/terraform/aws/outputs.tf
@@ -13,11 +13,6 @@ output "kubectl_config" {
   value       = module.eks.kubeconfig
 }
 
-# output "config_map_aws_auth" {
-#   description = "A kubernetes configuration to authenticate to this EKS cluster."
-#   value       = module.eks.config_map_aws_auth
-# }
-
 output "region" {
   description = "AWS region"
   value       = var.region
