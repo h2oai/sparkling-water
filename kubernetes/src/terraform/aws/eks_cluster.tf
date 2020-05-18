f53f2930a756ae1901b1064914f36c255ae40e15
Jakub Háva <jakub.hava@gmail.com>
2020-05-18 12:55:01 +0200

    include kubernetes in doc only for spark 2.3 +

--- kubernetes/src/terraform/aws/eks_cluster.tf
+++ kubernetes/src/terraform/aws/eks_cluster.tf
@@ -1,30 +1,26 @@
 module "eks" {
-  source       = "terraform-aws-modules/eks/aws"
+  source = "terraform-aws-modules/eks/aws"
   cluster_name = local.cluster_name
-  subnets      = module.vpc.private_subnets
-  wait_for_cluster_cmd          = "until curl -k -s $ENDPOINT/healthz >/dev/null; do sleep 4; done"
+  subnets = module.vpc.private_subnets
+  wait_for_cluster_cmd = "until curl -k -s $ENDPOINT/healthz >/dev/null; do sleep 4; done"
   tags = {
     Environment = "training"
-    GithubRepo  = "terraform-aws-eks"
-    GithubOrg   = "terraform-aws-modules"
+    GithubRepo = "terraform-aws-eks"
+    GithubOrg = "terraform-aws-modules"
   }
 
   vpc_id = module.vpc.vpc_id
 
   worker_groups = [
     {
-      name                          = "worker-group-1"
-      instance_type                 = "t2.small"
-      additional_userdata           = "echo foo bar"
-      asg_desired_capacity          = 2
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-    },
-    {
-      name                          = "worker-group-2"
-      instance_type                 = "t2.medium"
-      additional_userdata           = "echo foo bar"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_desired_capacity          = 1
+      name = "worker-group-1"
+      instance_type = "t2.medium"
+      additional_userdata = "echo foo bar"
+      additional_security_group_ids = [
+        aws_security_group.worker_group_mgmt_one.id]
+      asg_max_size = 4
+      asg_min_size = 4
+      asg_desired_capacity = 4
     },
   ]
 }
--- kubernetes/src/terraform/aws/vpc.tf
+++ kubernetes/src/terraform/aws/vpc.tf
@@ -1,11 +1,11 @@
 variable "region" {
-  default     = "us-east-2"
+  default = "us-east-2"
   description = "AWS region"
 }
 
 provider "aws" {
   version = ">= 2.28.1"
-  region  = "us-east-2"
+  region = "us-east-2"
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
@@ -17,21 +17,25 @@ locals {
 }
 
 resource "random_string" "suffix" {
-  length  = 8
+  length = 8
   special = false
 }
 
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
+  source = "terraform-aws-modules/vpc/aws"
   version = "2.6.0"
 
-  name                 = "SWKubernetes-vpc"
-  cidr                 = "10.0.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  name = "SWKubernetes-vpc"
+  cidr = "10.0.0.0/16"
+  azs = data.aws_availability_zones.available.names
+  private_subnets = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"]
+  public_subnets = [
+    "10.0.4.0/24",
+    "10.0.5.0/24"]
+  enable_nat_gateway = true
+  single_nat_gateway = true
   enable_dns_hostnames = true
 
   tags = {
@@ -40,11 +44,11 @@ module "vpc" {
 
   public_subnet_tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = "1"
+    "kubernetes.io/role/elb" = "1"
   }
 
   private_subnet_tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = "1"
+    "kubernetes.io/role/internal-elb" = "1"
   }
 }
