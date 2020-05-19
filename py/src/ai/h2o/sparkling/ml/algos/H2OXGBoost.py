bd79acb7a2278570d543e2490cb23d7653e0ea3a
Marek Novotný <marek.novotny@h2o.ai>
2020-05-19 14:15:48 +0200

    missing comma

--- py/src/ai/h2o/sparkling/ml/algos/H2OXGBoost.py
+++ py/src/ai/h2o/sparkling/ml/algos/H2OXGBoost.py
@@ -101,7 +101,7 @@ class H2OXGBoost(H2OXGBoostParams, H2OTreeBasedSupervisedAlgoBase):
                  foldAssignment="AUTO",
                  tweediePower=1.5,
                  categoricalEncoding="AUTO",
-                 maxCategoricalLevels=10
+                 maxCategoricalLevels=10,
                  **DeprecatedParams):
         Initializer.load_sparkling_jar()
         super(H2OXGBoost, self).__init__()
