65be51c2c467e165c5995887b7e233af3e029d23
Jakub Háva <jakub.hava@gmail.com>
2020-05-19 12:26:16 +0200

    more explicit

--- kubernetes/src/terraform/aws/eks_cluster.tf
+++ kubernetes/src/terraform/aws/eks_cluster.tf
@@ -18,6 +18,14 @@ module "eks" {
   ]
 }
 
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.11"
+}
+
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
 }
