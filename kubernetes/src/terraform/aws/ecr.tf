b04646a62152c377a9e99217599ba7c11f300406
Jakub Háva <jakub.hava@gmail.com>
2020-05-19 10:34:20 +0200

    Upload images

--- kubernetes/src/terraform/aws/ecr.tf
+++ kubernetes/src/terraform/aws/ecr.tf
@@ -1,6 +1,6 @@
 
 resource "aws_ecr_repository" "sw_kubernetes_repo" {
-  name                 = "sw_kubernetes_repo"
+  name                 = "sw_kubernetes_repo/sparkling-water"
   image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
