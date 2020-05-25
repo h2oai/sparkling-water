4a66368ebc82c41162fbc855784087eca7483ca9
Jakub Háva <jakub.hava@gmail.com>
2020-05-25 08:08:50 +0200

    [SW-2255] Deprecate H2OConf and H2OContext in package org.apache.spark (#2122)

--- core/src/main/scala/org/apache/spark/h2o/H2OConf.scala
+++ core/src/main/scala/org/apache/spark/h2o/H2OConf.scala
@@ -17,6 +17,7 @@
 
 package org.apache.spark.h2o
 
+import ai.h2o.sparkling.macros.DeprecatedMethod
 import ai.h2o.sparkling.utils.SparkSessionUtils
 import org.apache.spark.SparkConf
 import org.apache.spark.internal.Logging
@@ -27,12 +28,17 @@ import org.apache.spark.internal.Logging
   */
 class H2OConf(sparkConf: SparkConf) extends ai.h2o.sparkling.H2OConf(sparkConf) {
   this.set("spark.ext.h2o.rest.api.based.client", value = false)
+  logWarning(
+    "The class org.apache.spark.h2o.H2OConf is deprecated and will be removed in the version 3.34." +
+      " Please use ai.h2o.sparkling.H2OConf instead.")
   def this() = this(SparkSessionUtils.active.sparkContext.getConf)
 }
 
 object H2OConf extends Logging {
+  @DeprecatedMethod("ai.h2o.sparkling.H2OConf", "3.34")
   def apply(): H2OConf = new H2OConf()
 
+  @DeprecatedMethod("ai.h2o.sparkling.H2OConf", "3.34")
   def apply(sparkConf: SparkConf): H2OConf = new H2OConf(sparkConf)
 
   private var _sparkConfChecked = false
