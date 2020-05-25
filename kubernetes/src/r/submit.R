f5c00493ffa73590e4e83549498c1778692d7147
Jakub Háva <jakub.hava@gmail.com>
2020-05-25 21:11:17 +0200

    R script

---
+++ kubernetes/src/r/submit.R
@@ -0,0 +1,9 @@
+library(sparklyr)
+
+master = args[1]
+registryId = args[2]
+version = args[3]
+config <- spark_config()
+config[["spark.executor.instances"]] <- 3
+config[["spark.kubernetes.container.image"]] <- paste0(registryId, ".dkr.ecr.us-east-2.amazonaws.com/sw_kubernetes_repo/sparkling-water:r-", version)
+spark_submit(master = master, file = "initTest.R", config = config)
