b59924d89ad0392cdd77a351373b3c08c9b5917e
Jakub Háva <jakub.hava@gmail.com>
2020-05-27 13:33:10 +0200

    [SW-2264] Warn user of upcomming change in grid search in 3.32 (#2133)

--- ml/src/main/scala/ai/h2o/sparkling/ml/algos/H2OGridSearch.scala
+++ ml/src/main/scala/ai/h2o/sparkling/ml/algos/H2OGridSearch.scala
@@ -89,6 +89,9 @@ class H2OGridSearch(override val uid: String)
   }
 
   private def prepareHyperParameters(): String = {
+    logWarning(
+      "Starting from the version 3.32, hyper-parameter names will use the names corresponding to Sparkling Water parameters" +
+        " instead of H2O internal ones.")
     val it = getHyperParameters().entrySet().iterator()
     val checkedHyperParams = new java.util.HashMap[String, Array[AnyRef]]()
     while (it.hasNext) {
