9e2db730c0d9c735d9ec94b25f6e3fab9cfd92ec
Jakub Háva <jakub.hava@gmail.com>
2020-05-28 09:45:21 +0200

    formatting

--- kubernetes/src/terraform/aws/eks_cluster.tf
+++ kubernetes/src/terraform/aws/eks_cluster.tf
@@ -19,11 +19,11 @@ module "eks" {
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
+  host = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
-  version                = "~> 1.11"
+  token = data.aws_eks_cluster_auth.cluster.token
+  load_config_file = false
+  version = "~> 1.11"
 }
 
 data "aws_eks_cluster" "cluster" {
