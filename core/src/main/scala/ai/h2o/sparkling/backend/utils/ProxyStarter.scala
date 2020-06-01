3df182683800b5472208f17eb783ee01c6015b97
Jakub Háva <jakub.hava@gmail.com>
2020-06-01 20:11:15 +0200

    port

--- core/src/main/scala/ai/h2o/sparkling/backend/utils/ProxyStarter.scala
+++ core/src/main/scala/ai/h2o/sparkling/backend/utils/ProxyStarter.scala
@@ -38,7 +38,7 @@ object ProxyStarter extends Logging {
     var port = findFlowProxyBasePort(conf)
     while (true) {
       try {
-        port = findNextFreeFlowPort(conf.clientWebPort, port)
+        port = findNextFreeFlowPort(conf.clientWebPort, port + 1)
         val pool = new QueuedThreadPool()
         pool.setDaemon(true)
         val server = new Server(pool)
@@ -84,7 +84,7 @@ object ProxyStarter extends Logging {
         case _ => 0
       }
     } else {
-      1
+      0
     }
     conf.basePort + numSkipped
   }
