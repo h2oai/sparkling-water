c02bc810eed45177dc91b92446545654792cee48
Jakub Háva <jakub.hava@gmail.com>
2020-06-11 15:26:41 +0200

    Uncomment

--- py/tests/integration/test_integ_both_backends.py
+++ py/tests/integration/test_integ_both_backends.py
@@ -23,19 +23,20 @@ from pyspark.context import SparkContext
 from tests.integration.integ_test_utils import *
 
 
-# def testHamOrSpamPipelineAlgos(integ_spark_conf):
-#     return_code = launch(integ_spark_conf, "examples/HamOrSpamMultiAlgorithmDemo.py")
-#     assert return_code == 0, "Process ended in a wrong way. It ended with return code " + str(return_code)
-#
-#
-# def testChicagoCrime(integ_spark_conf):
-#     return_code = launch(integ_spark_conf, "examples/ChicagoCrimeDemo.py")
-#     assert return_code == 0, "Process ended in a wrong way. It ended with return code " + str(return_code)
-#
-#
-# def testImportPysparklingStandaloneApp(integ_spark_conf):
-#     return_code = launch(integ_spark_conf, "examples/tests/pysparkling_ml_import_overrides_spark_test.py")
-#     assert return_code == 0, "Process ended in a wrong way. It ended with return code " + str(return_code)
+def testHamOrSpamPipelineAlgos(integ_spark_conf):
+    return_code = launch(integ_spark_conf, "examples/HamOrSpamMultiAlgorithmDemo.py")
+    assert return_code == 0, "Process ended in a wrong way. It ended with return code " + str(return_code)
+
+
+def testChicagoCrime(integ_spark_conf):
+    return_code = launch(integ_spark_conf, "examples/ChicagoCrimeDemo.py")
+    assert return_code == 0, "Process ended in a wrong way. It ended with return code " + str(return_code)
+
+
+def testImportPysparklingStandaloneApp(integ_spark_conf):
+    return_code = launch(integ_spark_conf, "examples/tests/pysparkling_ml_import_overrides_spark_test.py")
+    assert return_code == 0, "Process ended in a wrong way. It ended with return code " + str(return_code)
+
 
 def testPy4jGatewayConnection(integ_spark_conf):
     token = "my_super_secret_token"
