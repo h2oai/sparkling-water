c27ec4d13f62e0eedb17cf9033c21970cf46d6cb
Jakub Háva <jakub.hava@gmail.com>
2020-06-12 09:52:49 +0200

    Fix for nightly

--- ci/sparklingWaterPipeline.groovy
+++ ci/sparklingWaterPipeline.groovy
@@ -390,7 +390,7 @@ def publishNightly() {
 
 def publishSparklingWaterDockerImage(String type, version, sparkMajorVersion) {
     sh """
-        ./bin/build-kubernetes-images.sh ${type}
+        H2O_HOME=${env.WORKSPACE}/h2o-3 ./bin/build-kubernetes-images.sh ${type}
         docker tag sparkling-water-${type}:${version} distrace/sparkling-water-${type}:latest-nightly-${sparkMajorVersion}
         docker push distrace/sparkling-water-${type}:latest-nightly-${sparkMajorVersion}
         docker rmi distrace/sparkling-water-${type}:latest-nightly-${sparkMajorVersion}
