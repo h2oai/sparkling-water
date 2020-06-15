15cf0aeabdc328de63997ae4446d7a87a6ba36bc
Jakub Háva <jakub.hava@gmail.com>
2020-06-15 15:22:05 +0200

    [SW-1966][FollowUp] Fix intermittent error coming from SparklyR in our tests

--- r/src/tests/testthat/testMojo.R
+++ r/src/tests/testthat/testMojo.R
@@ -27,7 +27,9 @@ config <- c(config, list(
   "sparklyr.backend.timeout" = 240,
   "sparklyr.log.console" = TRUE,
   "spark.ext.h2o.external.start.mode" = "auto",
-  "spark.ext.h2o.external.disable.version.check" = "true"
+  "spark.ext.h2o.external.disable.version.check" = "true",
+  "sparklyr.gateway.port" = 55555,
+  "sparklyr.connect.timeout" = 60 * 5
 ))
 
 for (i in 1:4) {
