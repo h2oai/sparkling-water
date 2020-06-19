a38731694a757ca78a523dbae2fb680eac551e40
Jakub Háva <jakub.hava@gmail.com>
2020-06-19 11:01:51 +0200

    [SW-2310][FollowUp] Ensure that endpoints on Spark driver respect authentication options (#2191)

--- core/src/test/scala/ai/h2o/sparkling/H2OContextAuthTestSuite.scala
+++ core/src/test/scala/ai/h2o/sparkling/H2OContextAuthTestSuite.scala
@@ -41,6 +41,18 @@ class H2OContextAuthTestSuite extends FunSuite with SharedH2OTestContext {
         .set("spark.ext.h2o.user.name", "admin")
         .set("spark.ext.h2o.password", "admin_password"))
 
+  private def getFlowIp(): String = {
+    val field = hc.getClass.getDeclaredField("flowIp")
+    field.setAccessible(true)
+    field.get(hc).asInstanceOf[String]
+  }
+
+  private def getFlowPort(): Int = {
+    val field = hc.getClass.getDeclaredField("flowPort")
+    field.setAccessible(true)
+    field.get(hc).asInstanceOf[Int]
+  }
+
   private def createLoginConf(): String = {
     val filename = "build/login.conf"
     new PrintWriter(filename) { write("admin:admin_password"); close() }
@@ -48,7 +60,8 @@ class H2OContextAuthTestSuite extends FunSuite with SharedH2OTestContext {
   }
 
   test("H2O endpoint behind Flow Proxy is accessible with provided username and password") {
-    val cloudV3 = RestApiUtils.query[CloudV3](new URI(s"http://${hc.flowIp}:${hc.flowPort}"), "3/Cloud", hc.getConf)
+    val cloudV3 =
+      RestApiUtils.query[CloudV3](new URI(s"http://${getFlowIp()}:${getFlowPort()}"), "3/Cloud", hc.getConf)
     assert(cloudV3 != null)
     assert(cloudV3.cloud_name == hc.getConf.cloudName.get)
   }
@@ -57,13 +70,13 @@ class H2OContextAuthTestSuite extends FunSuite with SharedH2OTestContext {
     import spark.implicits._
     spark.sparkContext.parallelize(1 to 10).toDF().createOrReplaceTempView("table_name")
     val dataFrames =
-      RestApiUtils.query[DataFrames](new URI(s"http://${hc.flowIp}:${hc.flowPort}"), "3/dataframes", hc.getConf)
+      RestApiUtils.query[DataFrames](new URI(s"http://${getFlowIp()}:${getFlowPort()}"), "3/dataframes", hc.getConf)
     assert(dataFrames != null)
     assert(dataFrames.dataframes.head.dataframe_id == "table_name")
   }
 
   test("H2O endpoint behind Flow Proxy is not accessible without provided username and password") {
-    val url = s"http://${hc.flowIp}:${hc.flowPort}/3/Cloud"
+    val url = s"http://${getFlowIp()}:${getFlowPort()}/3/Cloud"
     val thrown = intercept[IOException] {
       Source.fromURL(url)
     }
@@ -71,7 +84,7 @@ class H2OContextAuthTestSuite extends FunSuite with SharedH2OTestContext {
   }
 
   test("SW endpoint behind Flow Proxy is not accessible without provided username and password") {
-    val url = s"http://${hc.flowIp}:${hc.flowPort}/3/dataframes"
+    val url = s"http://${getFlowIp()}:${getFlowPort()}/3/dataframes"
     val thrown = intercept[IOException] {
       Source.fromURL(url)
     }
