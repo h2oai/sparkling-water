72f74df7e0ffd353f7183eb0003cc94cbc5bd16a
Jakub Háva <jakub.hava@gmail.com>
2020-06-26 11:16:07 +0200

    User Fabric8 client

--- core/src/main/scala/ai/h2o/sparkling/backend/external/KubernetesUtils.scala
+++ core/src/main/scala/ai/h2o/sparkling/backend/external/KubernetesUtils.scala
@@ -18,61 +18,44 @@
 package ai.h2o.sparkling.backend.external
 
 import ai.h2o.sparkling.H2OConf
-import io.kubernetes.client.openapi.{ApiClient, Configuration}
-import io.kubernetes.client.openapi.apis.CoreV1Api
-import io.kubernetes.client.openapi.models.{V1APIServiceSpec, V1ObjectMeta, V1Service, V1ServicePort, V1ServiceSpec}
-import io.kubernetes.client.util.Config
+import io.fabric8.kubernetes.client.{DefaultKubernetesClient, KubernetesClient}
 
 import scala.collection.JavaConverters._
 
 trait KubernetesUtils {
 
-  def setClient(): Unit = {
-    val client = Config.defaultClient
-    Configuration.setDefaultApiClient(client)
+  def deleteH2OHeadlessService(client: KubernetesClient, conf: H2OConf): Unit = {
+    client
+      .services()
+      .inNamespace(conf.externalK8sH2OServiceName)
+      .withName(conf.externalK8sH2OServiceName)
+      .delete()
   }
 
-  def deleteH2OHeadlessService(conf: H2OConf): Unit = {
-    val api = new CoreV1Api()
-    import io.kubernetes.client.openapi.models.V1DeleteOptions
-    val v1DeleteOptions = new V1DeleteOptions
-    v1DeleteOptions.setApiVersion("v1")
-    api.deleteNamespacedService(
-      conf.externalK8sH2OServiceName,
-      conf.externalK8sNamespace,
-      null,
-      null,
-      0,
-      true,
-      null,
-      v1DeleteOptions)
+  def installH2OHeadlessService(client: KubernetesClient, conf: H2OConf): Unit = {
+    client
+      .services()
+      .createOrReplaceWithNew()
+      .withApiVersion("v1")
+      .withKind("Service")
+      .withNewMetadata()
+      .withName(conf.externalK8sH2OServiceName)
+      .endMetadata()
+      .withNewSpec()
+      .withType("ClusterIP")
+      .withClusterIP("None")
+      .withSelector(Map("app" -> conf.externalK8sH2OLabel).asJava)
+      .addNewPort()
+      .withProtocol("TCP")
+      .withPort(54321)
+      .endPort()
+      .endSpec()
+      .done()
   }
 
-  def installH2OHeadlessService(conf: H2OConf): Unit = {
-    val api = new CoreV1Api()
-
-    val v1Metadata = new V1ObjectMeta
-    v1Metadata.setName(conf.externalK8sH2OServiceName)
-
-    val v1Spec = new V1ServiceSpec
-    v1Spec.setType("ClusterIP")
-    v1Spec.setClusterIP("None")
-    v1Spec.setSelector(Map("app" -> conf.externalK8sH2OLabel).asJava)
-    val v1Port = new V1ServicePort
-    v1Port.setProtocol("TCP")
-    v1Port.setPort(54321)
-    v1Spec.setPorts(List(v1Port).asJava)
-
-    val v1Service = new V1Service
-    v1Service.setApiVersion("v1")
-    v1Service.setKind("Service")
-    v1Service.setMetadata(v1Metadata)
-    v1Service.setSpec(v1Spec)
-    api.createNamespacedService(conf.externalK8sNamespace, v1Service, null, null, null)
-  }
-
-  def startExternalH2OOnKuberentes(conf: H2OConf): Unit = {
-    deleteH2OHeadlessService(conf)
-    installH2OHeadlessService(conf)
+  def startExternalH2OOnKubernetes(conf: H2OConf): Unit = {
+    val client = new DefaultKubernetesClient
+    deleteH2OHeadlessService(client, conf)
+    installH2OHeadlessService(client, conf)
   }
 }
