6651fe17eaa0ab2f8a37bdc6b14c7853ac2ffbdc
Jakub Háva <jakub.hava@gmail.com>
2020-06-30 18:30:59 +0200

    [SW-2344] Fix mojo test -> H2O added a new parameter and therefore number of parameters does not match now

--- py/tests/unit/with_runtime_sparkling/test_mojo.py
+++ py/tests/unit/with_runtime_sparkling/test_mojo.py
@@ -42,7 +42,7 @@ def testTrainingParams(gbmModel):
     assert params["seed"] == "42"
     assert params["distribution"] == "bernoulli"
     assert params["ntrees"] == "2"
-    assert len(params) == 45
+    assert len(params) == 46
 
 def testModelCategory(gbmModel):
     category = gbmModel.getModelCategory()
