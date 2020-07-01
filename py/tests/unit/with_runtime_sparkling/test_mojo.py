6c97dc127530cf69cdd927662494872048cafa89
Jakub Háva <jakub.hava@gmail.com>
2020-07-01 15:19:22 +0200

    [HOTFIX] Default training params have changed on H2O master branch

--- py/tests/unit/with_runtime_sparkling/test_mojo.py
+++ py/tests/unit/with_runtime_sparkling/test_mojo.py
@@ -42,7 +42,7 @@ def testTrainingParams(gbmModel):
     assert params["seed"] == "42"
     assert params["distribution"] == "bernoulli"
     assert params["ntrees"] == "2"
-    assert len(params) == 46
+    assert len(params) == 44
 
 def testModelCategory(gbmModel):
     category = gbmModel.getModelCategory()
