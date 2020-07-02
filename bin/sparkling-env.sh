c2c35eb1edbd62944e10e76834d977f73a489a41
Jakub Háva <jakub.hava@gmail.com>
2020-07-02 07:35:10 +0200

    reuse code

--- bin/sparkling-env.sh
+++ bin/sparkling-env.sh
@@ -124,14 +124,13 @@ if [ -z $(which java) ]; then
     exit -1
 fi
 
+version=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
 if [ "$SPARK_MAJOR_VERSION" == "2.1" ]; then
-  version=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
    if [[ "$version" < "1.7" ]]; then
       echo "Java 7 or higher is required to run Spark $SPARK_VERSION"
       exit -1
    fi
 else
-  version=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
    if [[ "$version" < "1.8" ]]; then
       echo "Java 8 or higher is required to run Spark $SPARK_VERSION"
       exit -1
