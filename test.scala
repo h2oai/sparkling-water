1237899f46d20b8cdfd2bf248441f4398d6f15e2
Jakub Háva <jakub.hava@gmail.com>
2020-07-13 13:51:40 +0200

    cleanups

--- test.scala
+++
@@ -1,8 +0,0 @@
-import java.net.URI
-import ai.h2o.sparkling._
-import org.apache.spark.SparkFiles
-import org.apache.spark.sql.SparkSession
-import spark.implicits._
-
-
-val hc = H2OContext.getOrCreate()
