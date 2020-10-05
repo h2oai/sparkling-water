63bb369cadac0e01e5d033788dedb6514cbd8474
Marek Novotný <marek.novotny@h2o.ai>
2020-10-05 22:54:56 +0200

    revoke rules on delete

--- kubernetes/src/terraform/aws/security_groups.tf
+++ kubernetes/src/terraform/aws/security_groups.tf
@@ -1,6 +1,8 @@
 resource "aws_security_group" "worker_group_mgmt_one" {
   name_prefix = "worker_group_mgmt_one"
   vpc_id = module.vpc.vpc_id
+  revoke_rules_on_delete = true
+
 
   ingress {
     from_port = 0
