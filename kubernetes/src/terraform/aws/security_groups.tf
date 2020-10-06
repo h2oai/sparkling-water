bfad4e022d16ceeed01f1d86a0bf66e3d6864145
Marek Novotný <marek.novotny@h2o.ai>
2020-10-06 15:38:57 +0200

    Add egress according to the documentation

--- kubernetes/src/terraform/aws/security_groups.tf
+++ kubernetes/src/terraform/aws/security_groups.tf
@@ -1,20 +1,18 @@
 resource "aws_security_group" "worker_group_mgmt_one" {
   name_prefix = "worker_group_mgmt_one"
   vpc_id = module.vpc.vpc_id
-  revoke_rules_on_delete = true
 
-
-  ingress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-
-    cidr_blocks = [
-      "0.0.0.0/0",
-    ]
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  lifecycle {
-    create_before_destroy = true
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
