e7bdf7aad05f60d291133457bfa15942de87f3f5
Marek Novotný <marek.novotny@h2o.ai>
2020-10-23 12:05:20 +0200

    Separate security group rules

--- kubernetes/src/terraform/aws/security_groups.tf
+++ kubernetes/src/terraform/aws/security_groups.tf
@@ -1,18 +1,24 @@
 resource "aws_security_group" "worker_group_mgmt_one" {
   name_prefix = "worker_group_mgmt_one"
   vpc_id = module.vpc.vpc_id
+}
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "worker_group_mgmt_one_ingress" {
+  type = "ingress"
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
 
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.worker_group_mgmt_one.id}"
+}
+
+resource "aws_security_group_rule" "worker_group_mgmt_one_egress" {
+  type = "egress"
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.worker_group_mgmt_one.id}"
 }
