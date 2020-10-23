c3ac286deea652c689d79ce5f4b6fe29a55b87ac
Marek Novotný <marek.novotny@h2o.ai>
2020-10-23 19:14:33 +0200

    Restrict terraform version to 0.12

--- kubernetes/src/terraform/aws/security_groups.tf
+++ kubernetes/src/terraform/aws/security_groups.tf
@@ -1,6 +1,10 @@
 resource "aws_security_group" "worker_group_mgmt_one" {
   name_prefix = "worker_group_mgmt_one"
   vpc_id = module.vpc.vpc_id
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_security_group_rule" "worker_group_mgmt_one_ingress" {
