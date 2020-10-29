80c9430234f2d7e23a2f95d83c75e4ccfc2be50a
Marek Novotný <marek.novotny@h2o.ai>
2020-10-29 21:21:22 +0100

    Fallback when reading mojo parameters

--- scoring/src/main/scala/ai/h2o/sparkling/ml/utils/Utils.scala
+++ scoring/src/main/scala/ai/h2o/sparkling/ml/utils/Utils.scala
@@ -20,13 +20,21 @@ package ai.h2o.sparkling.ml.utils
 import java.io.File
 
 import hex.genmodel.{ModelMojoReader, MojoModel, MojoReaderBackendFactory}
+import org.apache.spark.expose.Logging
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.catalyst.expressions.GenericRow
 
-object Utils {
+object Utils extends Logging {
   def getMojoModel(mojoFile: File): MojoModel = {
-    val reader = MojoReaderBackendFactory.createReaderBackend(mojoFile.getAbsolutePath)
-    ModelMojoReader.readFrom(reader, true)
+    try {
+      val reader = MojoReaderBackendFactory.createReaderBackend(mojoFile.getAbsolutePath)
+      ModelMojoReader.readFrom(reader, true)
+    } catch {
+      case e: Throwable =>
+        logError(s"Reading a mojo model with metadata failed. Trying to load the model without metadata...", e)
+        val reader = MojoReaderBackendFactory.createReaderBackend(mojoFile.getAbsolutePath)
+        ModelMojoReader.readFrom(reader, false)
+    }
   }
 
   def arrayToRow[T](array: Array[T]): Row = new GenericRow(array.map(_.asInstanceOf[Any]))
