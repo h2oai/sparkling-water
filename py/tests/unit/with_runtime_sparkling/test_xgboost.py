6b0d89c6a5731cc1a58104b0b33ca0cead471a4c
Marek Novotný <marek.novotny@h2o.ai>
2020-11-16 13:42:02 +0100

    [SW-2482] Expose Interaction Constraints on H2OXGBoost

--- py/tests/unit/with_runtime_sparkling/test_xgboost.py
+++ py/tests/unit/with_runtime_sparkling/test_xgboost.py
@@ -23,6 +23,7 @@ from pyspark.sql.types import *
 from pysparkling.ml import H2OXGBoost
 
 from tests.unit.with_runtime_sparkling.algo_test_utils import *
+from tests import unit_test_utils
 
 
 def testParamsPassedByConstructor():
@@ -38,3 +39,22 @@ def testFitXgboostWithoutError(prostateDataset):
 
     model = xgboost.fit(prostateDataset)
     model.transform(prostateDataset).repartition(1).collect()
+
+
+def testInteractionConstraintsAffectResult(spark, prostateDataset):
+    [traningDataset, testingDataset] = prostateDataset.randomSplit([0.9, 0.1], 1)
+    featureCols = ["AGE", "RACE", "DPROS", "DCAPS", "PSA", "VOL", "GLEASON"]
+
+    def createInitialXGBoostDefinition():
+        return H2OXGBoost(featuresCols=featureCols, labelCol="CAPSULE", seed=1, splitRatio=0.8)
+
+    referenceXGBoost = createInitialXGBoostDefinition()
+    referenceModel = referenceXGBoost.fit(traningDataset)
+    referenceResult = referenceModel.transform(testingDataset)
+
+    xgboost = createInitialXGBoostDefinition()
+    xgboost.setInteractionConstraints([["DPROS", "DCAPS"], ["PSA", "VOL", "GLEASON"]])
+    model = xgboost.fit(traningDataset)
+    result = model.transform(testingDataset)
+
+    unit_test_utils.assert_data_frames_have_different_values(referenceResult, result)
