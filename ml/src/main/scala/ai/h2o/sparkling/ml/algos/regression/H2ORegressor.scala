f2162ad604b3d3ddb42b3fbc6fd58567f85464fa
Marek Novotný <marek.novotny@h2o.ai>
2021-01-27 14:39:50 +0100

    Fix automl tests

--- ml/src/main/scala/ai/h2o/sparkling/ml/algos/regression/H2ORegressor.scala
+++ ml/src/main/scala/ai/h2o/sparkling/ml/algos/regression/H2ORegressor.scala
@@ -19,16 +19,25 @@ package ai.h2o.sparkling.ml.algos.regression
 
 import ai.h2o.sparkling.H2OFrame
 import ai.h2o.sparkling.ml.algos.H2OAlgoCommonUtils
-import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.{DataFrame, Dataset}
 import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.types.DoubleType
 
 private[sparkling] trait H2ORegressor extends H2OAlgoCommonUtils {
   def getLabelCol(): String
 
-  override private[sparkling] def prepareDatasetForFitting(dataset: Dataset[_]): (H2OFrame, Option[H2OFrame]) = {
+  private def prepareDatasetForRegression(dataset: Dataset[_]): DataFrame = {
     val labelColumnName = getLabelCol()
-    val preparedDataset = dataset.withColumn(labelColumnName, col(labelColumnName).cast(DoubleType))
-    super.prepareDatasetForFitting(preparedDataset)
+    dataset.withColumn(labelColumnName, col(labelColumnName).cast(DoubleType))
+  }
+
+  override private[sparkling] def prepareDatasetForFitting(dataset: Dataset[_]): (H2OFrame, Option[H2OFrame]) = {
+    super.prepareDatasetForFitting(prepareDatasetForRegression(dataset))
+  }
+
+  override private[sparkling] def prepareDatasetForFitting(
+      dataset: Dataset[_],
+      registerFramesForDeletion: Boolean): (H2OFrame, Option[H2OFrame]) = {
+    super.prepareDatasetForFitting(prepareDatasetForRegression(dataset), registerFramesForDeletion)
   }
 }
