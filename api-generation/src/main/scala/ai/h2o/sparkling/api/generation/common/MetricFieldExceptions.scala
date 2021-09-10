d2c5d01b73d4d0c2fb6705b511ec19c298f07206
Marek Novotný <marek.novotny@h2o.ai>
2021-09-10 22:40:33 +0200

    add custom_metric_value to optional

--- api-generation/src/main/scala/ai/h2o/sparkling/api/generation/common/MetricFieldExceptions.scala
+++ api-generation/src/main/scala/ai/h2o/sparkling/api/generation/common/MetricFieldExceptions.scala
@@ -21,5 +21,5 @@ object MetricFieldExceptions {
   def ignored(): Set[String] =
     Set("__meta", "domain", "model", "model_checksum", "frame", "frame_checksum", "model_category", "predictions")
 
-  def optional(): Set[String] = Set("custom_metric_name", "mean_score", "mean_normalized_score")
+  def optional(): Set[String] = Set("custom_metric_name", "custom_metric_value", "mean_score", "mean_normalized_score")
 }
