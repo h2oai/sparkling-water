aefc40d518cde326fb8ee9b0d7d086e6d308d0b6
krasinski <bartosz.krasinski@h2o.ai>
2021-11-19 00:43:08 +0100

    [SW-2634] Display Warnings Coming from ModelBuilders

--- ml/src/main/scala/ai/h2o/sparkling/ml/utils/EstimatorCommonUtils.scala
+++ ml/src/main/scala/ai/h2o/sparkling/ml/utils/EstimatorCommonUtils.scala
@@ -17,7 +17,6 @@
 package ai.h2o.sparkling.ml.utils
 
 import java.io.File
-
 import ai.h2o.sparkling.backend.H2OJob
 import ai.h2o.sparkling.backend.utils.{RestApiUtils, RestCommunication}
 import ai.h2o.sparkling.ml.internals.H2OModel
@@ -25,6 +24,7 @@ import ai.h2o.sparkling.ml.models.{H2OMOJOModel, H2OMOJOSettings}
 import ai.h2o.sparkling.{H2OConf, H2OContext}
 import hex.schemas.ModelBuilderSchema
 import org.apache.spark.expose
+import water.api.schemas3.ValidationMessageV3
 
 trait EstimatorCommonUtils extends RestCommunication {
   protected def trainAndGetDestinationKey(
@@ -42,6 +42,7 @@ trait EstimatorCommonUtils extends RestCommunication {
       encodeParamsAsJson)
     val jobId = modelBuilder.job.key.name
     H2OJob(jobId).waitForFinish()
+    Option(modelBuilder.messages).foreach(printWarnings)
     modelBuilder.job.dest.name
   }
 
@@ -81,5 +82,12 @@ trait EstimatorCommonUtils extends RestCommunication {
       replacement = s"${modelId}_$suffixNumber"
     } while (H2OModel.modelExists(replacement))
     replacement
+  }
+
+  private def printWarnings(messages: Array[ValidationMessageV3]): Unit = {
+    messages
+      .filter(_.message_type == "WARN")
+      .map(warn => s"${warn.message} (field name: ${warn.field_name})")
+      .foreach(logWarning(_))
   }
 }
