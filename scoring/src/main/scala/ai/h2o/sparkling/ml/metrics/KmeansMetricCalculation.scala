bb0e4f6c0c90f51ace3d543711b1532900b5c313
Marek Novotný <marek.novotny@h2o.ai>
2021-11-20 00:53:07 +0100

    Fix clustering metrics

--- scoring/src/main/scala/ai/h2o/sparkling/ml/metrics/KmeansMetricCalculation.scala
+++ scoring/src/main/scala/ai/h2o/sparkling/ml/metrics/KmeansMetricCalculation.scala
@@ -18,60 +18,27 @@
 package ai.h2o.sparkling.ml.metrics
 
 import ai.h2o.sparkling.ml.models.H2OKMeansMOJOModel
-import com.google.gson.{GsonBuilder, JsonObject}
 import hex.ModelMetrics.IndependentMetricBuilder
 import hex.ModelMetricsClustering.IndependentMetricBuilderClustering
+import hex.genmodel.GenModel
 import hex.genmodel.algos.kmeans.KMeansMojoModel
-import hex.genmodel.attributes.ModelJsonReader
 import hex.genmodel.easy.{EasyPredictModelWrapper, RowData}
 
 trait KmeansMetricCalculation {
   self: H2OKMeansMOJOModel =>
 
   override private[sparkling] def makeMetricBuilder(wrapper: EasyPredictModelWrapper): IndependentMetricBuilder[_] = {
-    val k = getK()
     val model = wrapper.m.asInstanceOf[KMeansMojoModel]
-    val nCols = model.nfeatures()
-    val modes = model._modes
-
-    val gson = new GsonBuilder().create().fromJson(getModelDetails(), classOf[JsonObject])
-    if (!gson.has("centers")) {
-      throw new UnsupportedOperationException(
-        "Calculation of metrics is not supported since the MOJO model doesn't have 'centers' field on model output.")
-    }
-    if (!gson.has("centers_std")) {
-      throw new UnsupportedOperationException(
-        "Calculation of metrics is not supported since the MOJO model doesn't have 'centers_std' field on model output.")
-    }
-    val centers = parse2dArray(gson, "centers")
-    val centersStandardized = parse2dArray(gson, "centers_std")
-
-    new IndependentMetricBuilderClustering(nCols, k, centers, centersStandardized, modes)
-  }
-
-  private def parse2dArray(obj: JsonObject, elementName: String): Array[Array[Double]] = {
-    if (obj.get(elementName).isJsonNull) {
-      null
-    } else {
-      val table = ModelJsonReader.readTable(obj, elementName)
-      val result = new Array[Array[Double]](table.rows())
-      var i = 0
-      while (i < table.rows()) {
-        val row = new Array[Double](table.columns() - 1)
-        var j = 1
-        while (j < table.columns()) {
-          row(j-1) = table.getCell(j, i).asInstanceOf[Double]
-          j += 1
-        }
-        result(i) = row
-        i += 1
-      }
-      result
-    }
+    new IndependentMetricBuilderClustering(model.nfeatures(), getK(), model._centers, model._modes)
   }
 
   override private[sparkling] def extractActualValues(rowData: RowData, wrapper: EasyPredictModelWrapper): Array[Float] = {
+    val model = wrapper.m.asInstanceOf[KMeansMojoModel]
     val rawData = new Array[Double](wrapper.m.nfeatures())
-    wrapper.fillRawData(rowData, rawData).map(_.toFloat)
+    wrapper.fillRawData(rowData, rawData)
+    if (model._standardize) {
+      GenModel.Kmeans_preprocessData(rawData,model._means, model._mults, model._modes)
+    }
+    rawData.map(_.toFloat)
   }
 }
