63f2043ccf73c5d8ba0d5da0d73e5a382bc6a918
Marek Novotný <marek.novotny@h2o.ai>
2022-02-18 14:12:26 +0100

    [SW-2691] Increase Memory on Scala Integration Tests

--- ml/src/integTest/scala/ai/h2o/sparkling/ml/IntegrationTestSuite.scala
+++ ml/src/integTest/scala/ai/h2o/sparkling/ml/IntegrationTestSuite.scala
@@ -29,7 +29,7 @@ import scala.concurrent.duration.Duration
 @RunWith(classOf[JUnitRunner])
 class IntegrationTestSuite extends FunSuite with SharedH2OTestContext {
 
-  override def createSparkSession(): SparkSession = sparkSession("local-cluster[2,1,2024]")
+  override def createSparkSession(): SparkSession = sparkSession("local-cluster[2,1,3036]")
 
   test("SchemaUtils: flattenDataFrame should process a complex data frame with more than 200k columns after flattening") {
     val expectedNumberOfColumns = 200000
