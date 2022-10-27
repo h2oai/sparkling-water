b4cb0e082d6eedca64d3648ee3a3fe709cd05fe3
Marek Novotný <marek.novotny@h2o.ai>
2022-10-27 12:42:15 +0200

    [SW-2754] Expose splinesNonNegative parameter on H2OGAM (#2831)
    * [SW-2754] Expose splinesNonNegative parameter on H2OGAM
    
    * Ignore parameter value comparison on MOJO models
    
    (cherry picked from commit 6b6a94f4e49521b4c067bda784a0585c90815f5a)

--- py/tests/unit/with_runtime_sparkling/test_mojo_parameters.py
+++ py/tests/unit/with_runtime_sparkling/test_mojo_parameters.py
@@ -53,9 +53,10 @@ def testGLMParameters(prostateDataset):
 def testGAMParameters(prostateDataset):
     features = ['AGE', 'RACE', 'DPROS', 'DCAPS', 'PSA']
     algorithm = H2OGAM(seed=1, labelCol="CAPSULE", gamCols=[["PSA"], ["AGE"]], numKnots=[5, 5], lambdaValue=[0.5],
-                       featuresCols=features, bs=[1, 1], scale=[0.5, 0.5], splineOrders=[-1, -1])
+                       featuresCols=features, bs=[1, 1], scale=[0.5, 0.5], splineOrders=[-1, -1],
+                       splinesNonNegative=[True, False])
     model = algorithm.fit(prostateDataset)
-    compareParameterValues(algorithm, model, ["getFeaturesCols"])
+    compareParameterValues(algorithm, model, ["getFeaturesCols", "getSplinesNonNegative"])
 
 
 def testDeepLearningParameters(prostateDataset):
