2b20fb2e7aac85e60afaa10d948893a5275b8541
Marek Novotný <marek.novotny@h2o.ai>
2022-12-15 18:43:55 +0100

    Fix typo

--- py-scoring/src/ai/h2o/sparkling/__init__.py
+++ py-scoring/src/ai/h2o/sparkling/__init__.py
@@ -18,4 +18,4 @@
 from ai.h2o.sparkling.Initializer import Initializer
 from ai.h2o.sparkling.BackingJar import BackingJar
 from ai.h2o.sparkling.VersionComponents import VersionComponents
-from ai.h2o.sparkling.H2ODataFrameConvertors import H2ODataFrameConverters
+from ai.h2o.sparkling.H2ODataFrameConverters import H2ODataFrameConverters
