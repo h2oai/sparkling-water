434bad756a4047bf843287a67b49b699947aa338
Marek Novotný <marek.novotny@h2o.ai>
2022-12-15 18:25:20 +0100

    Fix formatting problems

--- py-scoring/src/ai/h2o/sparkling/H2ODataFrameConverters.py
+++ py-scoring/src/ai/h2o/sparkling/H2ODataFrameConverters.py
@@ -18,27 +18,28 @@
 from pyspark.sql import DataFrame, SparkSession
 from py4j.java_gateway import JavaObject
 
+
 class H2ODataFrameConverters(object):
 
-@staticmethod
-def scalaToPythonDataFrame(jdf):
-    if jdf is None:
-        return None
-    elif isinstance(jdf, JavaObject):
-        session = SparkSession.builder.getOrCreate()
-        if hasattr(session, '_wrapped'):
-            sqlContext = session._wrapped
+    @staticmethod
+    def scalaToPythonDataFrame(jdf):
+        if jdf is None:
+            return None
+        elif isinstance(jdf, JavaObject):
+            session = SparkSession.builder.getOrCreate()
+            if hasattr(session, '_wrapped'):
+                sqlContext = session._wrapped
+            else:
+                sqlContext = session  # Spark 3.3+ utilizes SparkSession instead of SQLContext
+            return DataFrame(jdf, sqlContext)
         else:
-            sqlContext = session  # Spark 3.3+ utilizes SparkSession instead of SQLContext
-        return DataFrame(jdf, sqlContext)
-    else:
-        raise TypeError("Invalid type.")
+            raise TypeError("Invalid type.")
 
-@staticmethod
-def scalaDfArrayToPythonDfArray(array):
-    if array is None:
-        return None
-    elif isinstance(array, JavaObject):
-        return [H2ODataFrameConverters.scalaToPythonDataFrame(v) for v in array]
-    else:
-        raise TypeError("Invalid type.")
+    @staticmethod
+    def scalaDfArrayToPythonDfArray(array):
+        if array is None:
+            return None
+        elif isinstance(array, JavaObject):
+            return [H2ODataFrameConverters.scalaToPythonDataFrame(v) for v in array]
+        else:
+            raise TypeError("Invalid type.")
