9a6ddb70e71a73302a6c6068b82edfaa8db58cea
Bartosz Krasinski <bartosz.krasinski@h2o.ai>
2023-03-07 17:40:51 +0100

    [SW-2777] Custom jetty login module POC

---
+++ core/src/integTest/scala/ai/h2o/sparkling/CustomLdapAuthModuleSuite.scala
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ai.h2o.sparkling
+
+import ai.h2o.sparkling.H2OFrame.query
+import org.apache.spark.sql.SparkSession
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import water.api.schemas3.PingV3
+
+import java.io.File
+import java.net.URI
+import java.nio.charset.StandardCharsets
+import java.util.Base64
+
+// POC ONLY, Base64 can be replaced with anything
+@RunWith(classOf[JUnitRunner])
+class CustomLdapAuthModuleSuite extends LdapTestSuiteBase {
+
+  private val LdapAdminPasswordB64 = Base64.getEncoder.encodeToString(LdapAdminPassword.getBytes(StandardCharsets.UTF_8))
+  private val LdapBase64ConnectionConfig =
+    s"""
+       |ldaploginmodule {
+       |    water.webserver.jetty9.LdapBase64LoginModule required
+       |    debug="true"
+       |    useLdaps="false"
+       |    contextFactory="com.sun.jndi.ldap.LdapCtxFactory"
+       |    hostname="localhost"
+       |    port="$LdapPort"
+       |    bindDn="cn=$LdapAdmin,$LdapBaseDn"
+       |    base64BindPassword="$LdapAdminPasswordB64"
+       |    authenticationMethod="simple"
+       |    forceBindingLogin="true"
+       |    userBaseDn="$LdapBaseDn";
+       |};
+       |""".stripMargin
+
+  test("LdapBase64LoginModule should allow the binding") {
+    val conf = new H2OConf()
+      .setUserName(SwClusterOwnerName)
+      .setPassword(SwClusterOwnerPassword)
+    query[PingV3](new URI(hc.flowURL()), "/3/Ping", conf)
+  }
+
+  override def createSparkSession(): SparkSession = {
+    val tmpFile: File = writeTmpFile("-ldap-login.conf", LdapBase64ConnectionConfig)
+    val sparkConf = defaultSparkConf
+      .set("spark.ext.h2o.ldap.login", "true")
+      .set("spark.ext.h2o.user.name", SwClusterOwnerName)
+      .set("spark.ext.h2o.password", SwClusterOwnerPassword)
+      .set("spark.ext.h2o.login.conf", tmpFile.getAbsolutePath)
+    val result = sparkSession("local-cluster[2,1,1024]", sparkConf)
+    result.sparkContext.addFile(classOf[CustomLdapAuthModuleSuite].getClassLoader.getResource("log4j.properties").getPath)
+    result
+  }
+
+}
---
+++ core/src/main/scala/water/webserver/jetty9/LdapBase64LoginModule.scala
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package water.webserver.jetty9
+import java.util
+import java.util.Base64
+import javax.security.auth.Subject
+import javax.security.auth.callback.CallbackHandler
+import scala.collection.JavaConverters._
+import scala.collection.mutable
+
+// POC ONLY, Base64 can be replaced with anything
+class LdapBase64LoginModule extends org.eclipse.jetty.jaas.spi.LdapLoginModule {
+
+  private val Base64PasswordPropertyName = "base64BindPassword"
+  private val BindPasswordPropertyName = "bindPassword"
+
+  override def initialize(
+      subject: Subject,
+      callbackHandler: CallbackHandler,
+      sharedState: util.Map[String, _],
+      javaOptions: util.Map[String, _]): Unit = {
+    val options = collection.mutable.Map(javaOptions.asScala.asInstanceOf[mutable.Map[String, AnyRef]].toSeq: _*)
+    if (options.isDefinedAt(BindPasswordPropertyName)) {
+      throw new RuntimeException(
+        s"$BindPasswordPropertyName option not supported, please use $Base64PasswordPropertyName")
+    }
+
+    options
+      .get(Base64PasswordPropertyName)
+      .map(_.asInstanceOf[String])
+      .foreach { pwd =>
+        options.put(BindPasswordPropertyName, decodeBase64(pwd))
+      }
+    options.remove(Base64PasswordPropertyName)
+    super.initialize(subject, callbackHandler, sharedState, options.asJava)
+  }
+
+  private def decodeBase64(input: String): String = new String(Base64.getDecoder.decode(input))
+}
