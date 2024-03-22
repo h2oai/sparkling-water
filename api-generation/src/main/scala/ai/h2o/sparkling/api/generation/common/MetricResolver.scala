3af804b230bd488ec827dde89b54c044cddfc4a7
krasinski <8573352+krasinski@users.noreply.github.com>
2024-03-22 12:44:11 +0100

    fix metrics generation (#5718)

--- api-generation/src/main/scala/ai/h2o/sparkling/api/generation/common/MetricResolver.scala
+++ api-generation/src/main/scala/ai/h2o/sparkling/api/generation/common/MetricResolver.scala
@@ -21,11 +21,13 @@ import water.api.API
 
 trait MetricResolver {
   def resolveMetrics(substitutionContext: ModelMetricsSubstitutionContext): Seq[Metric] = {
-    val h2oSchemaClass = substitutionContext.h2oSchemaClass
+    val h2oSchemaClass: Class[_] = substitutionContext.h2oSchemaClass
 
     val parameters =
       for (field <- h2oSchemaClass.getDeclaredFields
-           if field.getAnnotation(classOf[API]) != null && !MetricFieldExceptions.ignored().contains(field.getName))
+           if field.getAnnotation(classOf[API]) != null
+           if !MetricFieldExceptions.ignored().contains(field.getName)
+           if !substitutionContext.skipFields.contains(field.getName))
         yield {
           val (swFieldName, swMetricName) = MetricNameConverter.convertFromH2OToSW(field.getName)
           Metric(swFieldName, swMetricName, field.getName, field.getType, field.getAnnotation(classOf[API]).help())
