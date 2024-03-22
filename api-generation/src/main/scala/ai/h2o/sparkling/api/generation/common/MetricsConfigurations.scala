c57e776842f7ad98eb568f60e5877cc5f17dc495
krasinski <8573352+krasinski@users.noreply.github.com>
2024-03-22 12:44:11 +0100

    fix metrics generation (#5718)
    (cherry picked from commit 3af804b230bd488ec827dde89b54c044cddfc4a7)

--- api-generation/src/main/scala/ai/h2o/sparkling/api/generation/common/MetricsConfigurations.scala
+++ api-generation/src/main/scala/ai/h2o/sparkling/api/generation/common/MetricsConfigurations.scala
@@ -22,82 +22,92 @@ import water.api.schemas3._
 
 trait MetricsConfigurations {
   def metricsConfiguration: Seq[ModelMetricsSubstitutionContext] = {
+    val duplicatedGLMMetrics = Seq("loglikelihood", "AIC")
     Seq(
       ModelMetricsSubstitutionContext(
-        "H2OCommonMetrics",
-        classOf[ModelMetricsBaseV3[_, _]],
-        Seq("H2OMetrics"),
-        "The class makes available all metrics that shared across all algorithms, and ML problems." +
+        entityName = "H2OCommonMetrics",
+        h2oSchemaClass = classOf[ModelMetricsBaseV3[_, _]],
+        parentEntities = Seq("H2OMetrics"),
+        classDescription = "The class makes available all metrics that shared across all algorithms, and ML problems." +
           " (classification, regression, dimension reduction)."),
       ModelMetricsSubstitutionContext(
-        "H2OBinomialMetrics",
-        classOf[ModelMetricsBinomialV3[_, _]],
-        Seq("H2OCommonMetrics"),
-        "The class makes available all metrics that shared across all algorithms supporting binomial classification."),
+        entityName = "H2OBinomialMetrics",
+        h2oSchemaClass = classOf[ModelMetricsBinomialV3[_, _]],
+        parentEntities = Seq("H2OCommonMetrics"),
+        classDescription =
+          "The class makes available all metrics that shared across all algorithms supporting binomial classification."),
       ModelMetricsSubstitutionContext(
-        "H2OBinomialGLMMetrics",
-        classOf[ModelMetricsBinomialGLMV3],
-        Seq("H2OBinomialMetrics", "H2OGLMMetrics"),
-        "The class makes available all binomial metrics supported by GLM algorithm."),
+        entityName = "H2OBinomialGLMMetrics",
+        h2oSchemaClass = classOf[ModelMetricsBinomialGLMV3],
+        parentEntities = Seq("H2OBinomialMetrics", "H2OGLMMetrics"),
+        classDescription = "The class makes available all binomial metrics supported by GLM algorithm.",
+        skipFields = duplicatedGLMMetrics),
       ModelMetricsSubstitutionContext(
-        "H2ORegressionMetrics",
-        classOf[ModelMetricsRegressionV3[_, _]],
-        Seq("H2OCommonMetrics"),
-        "The class makes available all metrics that shared across all algorithms supporting regression."),
+        entityName = "H2ORegressionMetrics",
+        h2oSchemaClass = classOf[ModelMetricsRegressionV3[_, _]],
+        parentEntities = Seq("H2OCommonMetrics"),
+        classDescription =
+          "The class makes available all metrics that shared across all algorithms supporting regression."),
       ModelMetricsSubstitutionContext(
-        "H2ORegressionGLMMetrics",
-        classOf[ModelMetricsRegressionGLMV3],
-        Seq("H2ORegressionMetrics", "H2OGLMMetrics"),
-        "The class makes available all regression metrics supported by GLM algorithm."),
+        entityName = "H2ORegressionGLMMetrics",
+        h2oSchemaClass = classOf[ModelMetricsRegressionGLMV3],
+        parentEntities = Seq("H2ORegressionMetrics", "H2OGLMMetrics"),
+        classDescription = "The class makes available all regression metrics supported by GLM algorithm.",
+        skipFields = duplicatedGLMMetrics),
       ModelMetricsSubstitutionContext(
-        "H2ORegressionCoxPHMetrics",
-        classOf[ModelMetricsRegressionCoxPHV3],
-        Seq("H2ORegressionMetrics"),
-        "The class makes available all regression metrics supported by CoxPH algorithm."),
+        entityName = "H2ORegressionCoxPHMetrics",
+        h2oSchemaClass = classOf[ModelMetricsRegressionCoxPHV3],
+        parentEntities = Seq("H2ORegressionMetrics"),
+        classDescription = "The class makes available all regression metrics supported by CoxPH algorithm."),
       ModelMetricsSubstitutionContext(
-        "H2OMultinomialMetrics",
-        classOf[ModelMetricsMultinomialV3[_, _]],
-        Seq("H2OCommonMetrics"),
-        "The class makes available all metrics that shared across all algorithms supporting multinomial classification."),
+        entityName = "H2OMultinomialMetrics",
+        h2oSchemaClass = classOf[ModelMetricsMultinomialV3[_, _]],
+        parentEntities = Seq("H2OCommonMetrics"),
+        classDescription =
+          "The class makes available all metrics that shared across all algorithms supporting multinomial classification."),
       ModelMetricsSubstitutionContext(
-        "H2OMultinomialGLMMetrics",
-        classOf[ModelMetricsMultinomialGLMV3],
-        Seq("H2OMultinomialMetrics", "H2OGLMMetrics"),
-        "The class makes available all multinomial metrics supported by GLM algorithm."),
+        entityName = "H2OMultinomialGLMMetrics",
+        h2oSchemaClass = classOf[ModelMetricsMultinomialGLMV3],
+        parentEntities = Seq("H2OMultinomialMetrics", "H2OGLMMetrics"),
+        classDescription = "The class makes available all multinomial metrics supported by GLM algorithm.",
+        skipFields = duplicatedGLMMetrics),
       ModelMetricsSubstitutionContext(
-        "H2OOrdinalMetrics",
-        classOf[ModelMetricsOrdinalV3[_, _]],
-        Seq("H2OCommonMetrics"),
-        "The class makes available all metrics that shared across all algorithms supporting ordinal regression."),
+        entityName = "H2OOrdinalMetrics",
+        h2oSchemaClass = classOf[ModelMetricsOrdinalV3[_, _]],
+        parentEntities = Seq("H2OCommonMetrics"),
+        classDescription =
+          "The class makes available all metrics that shared across all algorithms supporting ordinal regression."),
       ModelMetricsSubstitutionContext(
-        "H2OOrdinalGLMMetrics",
-        classOf[ModelMetricsOrdinalGLMV3],
-        Seq("H2OOrdinalMetrics", "H2OGLMMetrics"),
-        "The class makes available all ordinal metrics supported by GLM algorithm."),
+        entityName = "H2OOrdinalGLMMetrics",
+        h2oSchemaClass = classOf[ModelMetricsOrdinalGLMV3],
+        parentEntities = Seq("H2OOrdinalMetrics", "H2OGLMMetrics"),
+        classDescription = "The class makes available all ordinal metrics supported by GLM algorithm."),
       ModelMetricsSubstitutionContext(
-        "H2OAnomalyMetrics",
-        classOf[ModelMetricsAnomalyV3],
-        Seq("H2OCommonMetrics"),
-        "The class makes available all metrics that shared across all algorithms supporting anomaly detection."),
+        entityName = "H2OAnomalyMetrics",
+        h2oSchemaClass = classOf[ModelMetricsAnomalyV3],
+        parentEntities = Seq("H2OCommonMetrics"),
+        classDescription =
+          "The class makes available all metrics that shared across all algorithms supporting anomaly detection."),
       ModelMetricsSubstitutionContext(
-        "H2OClusteringMetrics",
-        classOf[ModelMetricsClusteringV3],
-        Seq("H2OCommonMetrics"),
-        "The class makes available all metrics that shared across all algorithms supporting clustering."),
+        entityName = "H2OClusteringMetrics",
+        h2oSchemaClass = classOf[ModelMetricsClusteringV3],
+        parentEntities = Seq("H2OCommonMetrics"),
+        classDescription =
+          "The class makes available all metrics that shared across all algorithms supporting clustering."),
       ModelMetricsSubstitutionContext(
-        "H2OAutoEncoderMetrics",
-        classOf[ModelMetricsAutoEncoderV3],
-        Seq("H2OCommonMetrics"),
-        "The class provides all metrics available for ``H2OAutoEncoder``."),
+        entityName = "H2OAutoEncoderMetrics",
+        h2oSchemaClass = classOf[ModelMetricsAutoEncoderV3],
+        parentEntities = Seq("H2OCommonMetrics"),
+        classDescription = "The class provides all metrics available for ``H2OAutoEncoder``."),
       ModelMetricsSubstitutionContext(
-        "H2OGLRMMetrics",
-        classOf[ModelMetricsGLRMV99],
-        Seq("H2OCommonMetrics"),
-        "The class provides all metrics available for ``H2OGLRM``."),
+        entityName = "H2OGLRMMetrics",
+        h2oSchemaClass = classOf[ModelMetricsGLRMV99],
+        parentEntities = Seq("H2OCommonMetrics"),
+        classDescription = "The class provides all metrics available for ``H2OGLRM``."),
       ModelMetricsSubstitutionContext(
-        "H2OPCAMetrics",
-        classOf[ModelMetricsPCAV3],
-        Seq("H2OCommonMetrics"),
-        "The class provides all metrics available for ``H2OPCA``."))
+        entityName = "H2OPCAMetrics",
+        h2oSchemaClass = classOf[ModelMetricsPCAV3],
+        parentEntities = Seq("H2OCommonMetrics"),
+        classDescription = "The class provides all metrics available for ``H2OPCA``."))
   }
 }
